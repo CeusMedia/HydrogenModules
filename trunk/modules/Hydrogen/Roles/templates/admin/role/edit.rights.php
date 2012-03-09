206f3db29ea005c577c4692a4959d74cf226aecf
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-03-09 03:38:33 +0000

    Hydrogen: Updated roles module.

---
+++ trunk/modules/Hydrogen/Roles/templates/admin/role/edit.rights.php
@@ -0,0 +1,63 @@
+<?php
+$rows	= array();
+foreach( $actions as $controller => $class ){
+	$list	= array();
+	foreach( $class->methods as $action => $method ){
+		$access	= $acl->hasRight( $roleId, $controller, $action );
+		$check	= "";
+		$for	= NULL;
+		$class	= 'red';
+		$id		= 'input-role-right-'.$roleId.'-'.$controller.'-'.$action;
+		switch( $access ){
+			case -1:
+				$id	= NULL;
+				break;
+			case -0:
+				$class	= 'red changable';
+				break;
+			case 1:
+				$class	= 'green changable';
+				break;
+			case 2:
+				$class	= 'green';
+				$id	= NULL;
+				break;
+		}
+		$label	= UI_HTML_Tag::create( 'label', $method->name, array() );
+		$list[]	= UI_HTML_Tag::create( 'li', $check.$label, array( 'class' => $class, 'id' => $id ) );
+	}
+	$list	= UI_HTML_Tag::create( 'ul', join( $list ), array() );
+	$rows[]	= '<tr><td>'.$controller.'</td><td>'.$list.'</td></tr>';
+}
+$tableRights	= '<table><tr><th>Controller</th><th>Aktionen</th><th>Status</th></tr>'.join( $rows ).'</table>';
+
+$script	= '
+$(document).ready(function(){
+	$("#role-edit-rights li.changable").bind("mousedown",function(){
+		var id = $(this).attr("id");
+		var parts = id.split(/-/);
+		var action = parts.pop();
+		var controller = parts.pop();
+		$.ajax({
+			url: "./admin/role/ajaxChangeRight/'.$roleId.'/"+controller+"/"+action,
+			dataType: "json",
+			context: $(this),
+			success: function(data){
+				if(data)
+					$(this).removeClass("red").addClass("green");
+				else
+					$(this).removeClass("green").addClass("red");
+			}
+		});
+	});
+});
+';
+$this->env->getPage()->js->addScript( $script );
+
+return '
+<fieldset id="role-edit-rights">
+	<legend>Zugriffsrechte der Rolle</legend>
+	'.$tableRights.'
+</fieldset>
+';
+?>
