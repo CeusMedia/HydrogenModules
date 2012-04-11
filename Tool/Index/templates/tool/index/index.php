c99a50819060de9dd9caa253df3fe332f0aad318
Christian Würker <kriss@ceusmedia.de>
2012-04-11 00:14:43 +0000

    Modules/Hydrogen: Updated tool index module.

--- Tool/Index/templates/tool/index/index.php
+++ Tool/Index/templates/tool/index/index.php
@@ -1,5 +1,21 @@
 <?php
-$list	= UI_HTML_Tag::create( "ul", implode( "\n", $list ) );
+$tools	= array();
+foreach( $list as $item ){
+	$description	= isset( $labels[$item]['description'] ) ? "<br/>".$labels[$item]['description'] : "";
+	$tags			= "";
+	if( isset( $labels[$item]['tags'] ) ){
+		$tagList	= explode( ",", $labels[$item]['tags'] );
+		foreach( $tagList as $id => $tag )
+			$tagList[$id]	= '<span class="tag">'.$tag.'</span>';
+		$tags	= implode( ", ", $tagList );
+		$tags	= '<br/><span class="tags">Tags: '.$tags.'</span>';
+	}
+	$link	= UI_HTML_Tag::create( "a", $item, array( 'href' => "./tools/".$item."/" ) );
+	$tools[$item]	= UI_HTML_Tag::create( "li", $link.$description.$tags );
+}
+
+
+$list	= UI_HTML_Tag::create( "ul", $tools );
 
 return '
 	<script src="javascripts/tool.index.js"></script>
