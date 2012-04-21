eceae3b28e47d4b949904f4220373a85643feccd
Christian Würker <kriss@ceusmedia.de>
2012-04-21 08:01:36 +0000

    Tools/Hydrogen/Setup: Update.

--- Auth/templates/auth/login.php
+++ Auth/templates/auth/login.php
@@ -1,32 +1,43 @@
 <?php
 
+$w	= (object) $words['login'];
+
 $text	= $this->populateTexts( array( 'top', 'info', 'bottom' ), 'html/auth.login.' );
-$heading	= empty( $words['login']['heading'] ) ? "" : UI_HTML_Tag::create( 'h2', $words['login']['heading'] );
 
 return '
-'.$heading.'
 <div class="auth-login-text-top">'.$text['top'].'</div>
-<div class="auth-login-text-info">'.$text['info'].'</div>
-<div class="auth-login-form">
-	<form name="editUser" action="./auth/login" method="post">
-		<fieldset>
-			<legend>'.$words['login']['legend'].'</legend>
-			<ul class="input">
-				<li>
-					<label for="username">'.$words['login']['labelUsername'].'</label><br/>
-					'.UI_HTML_Elements::Input( 'username', $data['username'], 'm' ).'
-				</li>
-				<li>
-					<label for="password">'.$words['login']['labelPassword'].'</label><br/>
-					'.UI_HTML_Elements::Password( 'password', 'm' ).'
-				</li>
-			</ul>
-			<div class="buttonbar">
-				'.UI_HTML_Elements::Button( 'login', $words['login']['button'], 'button save' ).'
-			</div>
-		</fieldset>
-	</form>
+<div class="column-left-50">
+	<div class="column-left-90">
+		<div class="auth-login-form">
+			<form name="editUser" action="./auth/login" method="post">
+				<fieldset>
+					<legend class="login">'.$w->legend.'</legend>
+					<ul class="input">
+						<li>
+							<div class="column-left-60">
+								<label for="login_username" class="mandatory">'.$w->labelUsername.'</label><br/>
+								'.UI_HTML_Elements::Input( 'login_username', $data['login_username'], 'max mandatory' ).'
+							</div>
+							<div class="column-left-40">
+								<label for="login_password" class="mandatory">'.$w->labelPassword.'</label><br/>
+								'.UI_HTML_Elements::Password( 'login_password', 'max mandatory' ).'
+							</div>
+							<div class="column-clear"></div>
+						</li>
+					</ul>
+					<div class="buttonbar">
+						'.UI_HTML_Elements::Button( 'doLogin', $w->button, 'button save' ).'
+					</div>
+				</fieldset>
+
+			</form>
+		</div>
+	</div>
 </div>
+<div class="column-left-50">
+	<div class="auth-login-text-info">'.$text['info'].'</div>
+</div>
+<div class="column-clear"></div>
 <div class="auth-login-text-bottom">'.$text['bottom'].'</div>
 ';
 ?>
---
+++ Auth/templates/auth/password.php
@@ -0,0 +1,29 @@
+<?php
+$w		= (object) $words['password'];
+$text	= $this->populateTexts( array( 'top', 'info', 'bottom' ), 'html/auth/password.' );
+
+return '
+'.$text['top'].'
+<div class="column-left-50">
+	<div class="column-left-90">
+		<fieldset>
+			<legend>'.$w->legend.'</legend>
+			<form action="./auth/password" method="post">
+				<ul class="input">
+					<li>
+						<label for="input_password_email" class="mandatory">'.$w->labelEmail.'</label><br/>
+						<input type="text" name="password_email" id="input_password_email" class="max mandatory" value="'.$request->get( 'password_email' ).'">
+					</li>
+				</ul>
+				<div class="buttonbar">
+					'.UI_HTML_Elements::Button( 'confirm', $w->buttonSend, 'button save' ).'
+				</div>
+			</form>
+		</fieldset>
+	</div>
+</div>
+<div class="column-left-50">
+	'.$text['info'].'
+</div>
+<div class="column-clear"></div>';
+?>
