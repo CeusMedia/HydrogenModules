ed9c6cbc513d14201a2a213a3d4bcf72c06e5d24
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-04-26 04:18:46 +0000

    Modules/Hydrogen: Updated missions module.

--- trunk/modules/Hydrogen/Work/Mission/js/work.mission.js
+++ trunk/modules/Hydrogen/Work/Mission/js/work.mission.js
@@ -1,9 +1,59 @@
-function showOptionals(elem,speed){
+var FormMissionFilter = {
+	form: null,
+	__init: function(){
+		this.form = $("#form_mission_filter");
+		if(!this.form.size())
+			return false;
+		this.form.find(".optional-trigger").trigger("change");
+		this.form.find("#filter_query").bind("change",function(){
+			this.form.submit();
+		});
+		if(this.form.find("#filter_query").val().length)
+			this.form.find("#reset-button-container").show();
+		this.form.find("#reset-button-trigger").bind("click",this.clearQuery);
+		return true;
+	},
+	clearQuery: function(){
+		if(!FormMissionFilter.form.size())
+			return false;
+		FormMissionFilter.form.find("#filter_query").val("");
+		FormMissionFilter.form.submit();
+		return true;
+	}
+};
+
+
+
+
+function showOptionals(elem){
 	var form = $(elem.form);
 	var name = $(elem).attr("name");
-	var type = name+"-"+$(elem).val();
-	form.find(".optional."+name).not("."+type).hide();
-	form.find(".optional."+type).show();
+	var type = $(elem).attr("type");
+	var value = name+"-"+$(elem).val();
+	if(type == "checkbox"){
+		value = name+"-"+$(elem).prop("checked");
+	}
+	var toHide = form.find(".optional."+name).not("."+value);
+	var toShow = form.find(".optional."+value);
+	if(!$(elem).data("status")){
+		toHide.hide();
+		toShow.show();
+		$(elem).data("status",1)
+		return;
+	}
+	switch($(elem).data('animation')){
+		case 'fade':
+			toHide.fadeOut();
+			toShow.fadeIn();
+			break;
+		case 'slide':
+			toHide.slideUp($(elem).data('speed-hide'));
+			toShow.slideDown($(elem).data('speed-show'));
+			break;
+		default:
+			toHide.hide();
+			toShow.show();
+	}
 }
 
 
