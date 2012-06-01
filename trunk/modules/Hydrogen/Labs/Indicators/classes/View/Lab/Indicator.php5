6dd824328d13c505928bd7c8dcf7675f012ce7c4
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-06-01 18:24:26 +0000

    Modules/Hydrogen: Added new indicator lab module. Updated button lab module definition and lab index template.

---
+++ trunk/modules/Hydrogen/Labs/Indicators/classes/View/Lab/Indicator.php5
@@ -0,0 +1,18 @@
+<?php
+/**
+ *	 Lab Controller.
+ *	@category		CMF.Hydrogen
+ *	@package		Modules.Lab.Controller
+ *	@version		$Id$
+ */
+/**
+ *	Indicator Lab Controller.
+ *	@category		CMF.Hydrogen
+ *	@package		Modules.Lab.Controller
+ *	@version		$Id$
+ */
+class View_Lab_Indicator extends CMF_Hydrogen_View{
+
+	public function index(){}
+}
+?>
--- trunk/modules/Hydrogen/Labs/templates/lab/index.php
+++ trunk/modules/Hydrogen/Labs/templates/lab/index.php
@@ -1,6 +1,56 @@
 <?php
-return '<h2>Labs</h2>
-	
+/*  --  INIT  --  */
+$dbc = $page = $messenger = $script = $style = $content = $dump = NULL;
+if( $dbc = $this->env->get( 'database', FALSE ) )
+	$dbc->beginTransaction();
+ob_start();
+try{
+/*  --  YOUR CODE HERE  --  */
 
-';
+
+
+
+/*
+try{
+	$this->env->get( 'INVALID' );
+}
+catch( Exception $e ){
+	UI_HTML_Exception_Page::display( $e );
+	die;
+}
+
+*/
+
+
+/*  --  YOUR CSS  --  */$style	= <<<EOT
+
+EOT;
+
+/*  --  YOUR JS  --  */$script	= <<<EOT
+$(document).ready(function(){});
+
+EOT;
+
+
+/*  --  HTML OUTPUT  --  */
+$content	= '
+<div id="lab">
+	<h2>Lab</h2>
+	<div class="chamber" id="lab-chamber-main">
+		'.$content.'
+	</div>
+</div>';
+
+
+
+
+
+
+/*  --  DESTRUCTION - STOP CODING HERE  --  */
+	$dump	= ob_get_clean();
+}
+catch( Excepton $e ){$dump	.= UI_HTML_Excepion_View::render( $e );}
+$content	.= $dump ? '<div class="dev-dumpbox">'.$dump.'</div>' : '';
+isset( $dbc ) ? $dbc->rollBack() : NULL;
+return '<style>'.$style.'</style><script>'.$script.'</script>'.$content;
 ?>
