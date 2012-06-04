8cbd3bffdfd81ee72e393d2219a9ae288e3d1cac
Christian Würker <kriss@ceusmedia.de>
2012-06-04 23:21:29 +0000

    Modules/Hydrogen: Updated script to set file permissions.

--- setrights.sh
+++ setrights.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
-sudo chown -R kriss .
-sudo chgrp -R www-data .
-sudo chmod -R 770 .
+#!/bin/bash 
+
+# see http://stackoverflow.com/a/1638397
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=`readlink -f $0`
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
+USER=`whoami`
+GROUP=$1
+if [ $GROUP=='' ]
+  then
+    GROUP='www-data'
+fi
+
+#echo "Path: $SCRIPTPATH"
+#echo "User: $USER"
+#echo "Group: $GROUP"
+
+sudo chown -R $USER $SCRIPTPATH
+sudo chgrp -R $GROUP $SCRIPTPATH
+sudo chmod -R 770 $SCRIPTPATH
 
