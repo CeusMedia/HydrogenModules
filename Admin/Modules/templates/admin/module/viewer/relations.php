586d331c28c7078d73642fc7537cec2b0b9c6fa3
Christian Würker <kriss@ceusmedia.de>
2012-06-26 03:39:47 +0000

    Modules/Hydrogen: Updated modules module and instances module.

--- Admin/Modules/templates/admin/module/viewer/relations.php
+++ Admin/Modules/templates/admin/module/viewer/relations.php
@@ -7,26 +7,62 @@ $url	= './admin/module/viewer/index/';
 
 $relationsNeeded	= '-';
 if( $module->neededModules )
-	$relationsNeeded	= $this->renderRelatedModulesList( $modules, $module->neededModules, $url, 'relations-needed' );
+	$relationsNeeded	= $this->renderRelatedModulesList( $modules, $module->neededModules, $url, 'relations relations-needed' );
 
 $relationsSupported	= '-';
 if( $module->supportedModules )
-	$relationsSupported	= $this->renderRelatedModulesList( $modules, $module->supportedModules, $url, 'relations-supported' );
+	$relationsSupported	= $this->renderRelatedModulesList( $modules, $module->supportedModules, $url, 'relations relations-supported' );
 
-return '
-<div class="column-right-60">
+
+$panelGraphNeeds	= '';
+if( $module->neededModules ){
+	$panelGraphNeeds	= '
+	<h4>Abhängigkeiten</h4>
+	<img src="./admin/module/showRelationGraph/'.$moduleId.'" style="max-width: 100%"/><br/><br/>';
+}
+
+$panelGraphSupports	= '';
+if( $module->supportedModules ){
+	$panelGraphSupports	= '
+	<h4>Unterstützung</h4>
+	<img src="./admin/module/showRelationGraph/'.$moduleId.'/supports" style="max-width: 100%"/><br/><br/>';
+}
+/*
+$panelGraphNeeds	= '';
+if( $module->neededModules ){
+	$panelGraphNeeds	= '
 	<fieldset>
 		<legend>Abhängigkeiten</legend>
-		<img src="./admin/module/showRelationGraph/'.$moduleId.'"/>
-	</fieldset>
-</div>
-<div class="column-left-40">
+		<img src="./admin/module/showRelationGraph/'.$moduleId.'" style="max-width: 100%"/>
+	</fieldset>';
+}
+
+$panelGraphSupports	= '';
+if( $module->supportedModules ){
+	$panelGraphSupports	= '
+	<fieldset>
+		<legend>Unterstützung</legend>
+		<img src="./admin/module/showRelationGraph/'.$moduleId.'/supports" style="max-width: 100%"/>
+	</fieldset>';
+}
+*/
+
+return '
+'.$panelGraphNeeds.'
+'.$panelGraphSupports.'
+<br/>
+<div class="column-left-50">
 	<dl>
 		<dt>'.$w->relationsNeeded.'</dt>
 		<dd>'.$relationsNeeded.'</dd>
+	</dl>
+</div>
+<div class="column-left-50">
+	<dl>
 		<dt>'.$w->relationsSupported.'</dt>
 		<dd>'.$relationsSupported.'</dd>
 	</dl>
-	<div class="clearfix"></div>
-</div>';
+</div>
+<div class="column-clear"></div>
+';
 ?>
