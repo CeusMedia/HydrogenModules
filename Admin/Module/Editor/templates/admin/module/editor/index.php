f37c28813307f0564fb49b875095c004cce99a74
Christian Würker <kriss@ceusmedia.de>
2012-07-06 17:49:33 +0000

    Modules/Hydrogen: Updated module Admin:Modules:*.

--- Admin/Module/Editor/templates/admin/module/editor/index.php
+++ Admin/Module/Editor/templates/admin/module/editor/index.php
@@ -10,12 +10,18 @@ if( !$listSections )
 	$listSections	= '<em>Keine <a href="./admin/module">Module</a> installiert.</em>';
 
 return '
-<h3 class="position">
-	<span>'.$words['index']['heading'].'</span>
-</h3><br/>
-<div class="module-overview">
-	'.$listSections.'
-	<div class="column-clear"></div>
+<div style="position: relative">
+	<div id="search">
+		<input type="text" name="query" id="input_query" placeholder="Suche" autocomplete="off"/>
+		<div id="search-reset"></div>
+	</div>
+	<h3 class="position">
+		<span>'.$words['index']['heading'].'</span>
+	</h3><br/>
+	<div class="module-overview">
+		'.$listSections.'
+		<div class="column-clear"></div>
+	</div>
 </div>';
 
 ?>
