eae5cb9b7658fea56be4bba41e982c6b98f3201b
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-07-06 17:48:56 +0000

    Modules/Hydrogen: Updated module Admin:Modules.

--- trunk/modules/Hydrogen/Admin/Modules/js/site.admin.module.js
+++ trunk/modules/Hydrogen/Admin/Modules/js/site.admin.module.js
@@ -9,8 +9,30 @@ AdminModuleInstaller = {
 	}
 };
 
-
 $(document).ready(function(){
+
+	new InstantFilter('div#search input#input_query',{
+		caseSense: false,
+		skipKeys: [32],
+		durationFadeIn: 600,
+		durationFadeOut: 200,
+		selectorItems: 'div.module',
+		selectorReset: '#search #search-reset',
+		onSearch: function(instance){
+			var o = instance.options;
+			$(o.selectorItems).parent().each(function(){
+				var got = $(this).find(o.selectorItems).not(".hidden").stop(true,true);
+				var dur = got.size() ? o.durationFadeIn : o.durationFadeOut;
+				got.size() ? $(this).fadeIn(dur) : $(this).fadeOut(dur);
+			});
+		},
+		onReset: function(instance){
+			var o = instance.options;
+			var fieldSets = $(o.selectorItems).parent();
+			fieldSets.not(":visible").fadeIn(o.durationFadeIn);
+		}
+	});
+
 	$("button.auto-back").each(function(){
 		$(this).removeAttr("disabled").removeAttr("readonly");
 		$(this).bind("click",function(){
