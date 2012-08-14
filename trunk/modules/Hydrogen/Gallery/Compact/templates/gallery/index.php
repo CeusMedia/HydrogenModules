c7d58287aa7c2d85636d8166136e92fabc7ac048
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-08-14 05:21:52 +0000

    Modules/Hydrogen: Updated module Gallery:Compact.

--- trunk/modules/Hydrogen/Gallery/Compact/templates/gallery/index.php
+++ trunk/modules/Hydrogen/Gallery/Compact/templates/gallery/index.php
@@ -1,5 +1,30 @@
 <?php
 $feedUrl	= View_Helper_Gallery::getFeedUrl( $env );
+function isMobile(){
+	$userAgent	= getEnv( 'HTTP_USER_AGENT' );
+	$patterns	= array(
+		'iPhone'		=> "/(iPad|iPod|iPhone).* (([3]_[2-9](_| ))|([4-9]_[0-9](_| )))/",
+		'Android'		=> "/(Android) ([0-9.]+).*Mobile/ ",
+		'Backberry'		=> "/(BlackBerry).*Version\/([6-9])\./",
+		'PlayBook'		=> "/(PlayBook).*OS ([1-9])\./",
+		'Kindle'		=> "/(Kindle)\/([3-9])/",
+		'KindleFire'	=> "/(Kindle Fire)/",
+		'Windows'		=> "/(Windows Phone OS) ([7-9])/",
+		'Opera'			=> "/(Opera)\/([0-9.]+) /"
+	);
+	foreach( $patterns as $pattern ){
+		$matches	= array();
+		if( preg_match_all( $pattern, $userAgent, $matches ) ){
+			return (object) array(
+				'browser'	=> $matches[1][0],
+				'version'	=> isset( $matches[2][0] ) ? $matches[2][0] : NULL,
+				'matches'	=> $matches
+			);
+		}
+	}
+}
+
+$mobile	= isMobile();
 
 $dateFormat	= $env->getConfig()->get( 'module.gallery_compact.format.date' );
 
@@ -44,19 +69,26 @@ foreach( $files as $file ){
 	$title		= !empty( $info[$data['filename']] ) ? $info[$data['filename']] : $fileName;
 	
 	$attributes	= array(
-		'src'			=> $path.$source.$data['filename'].'.small.'.$data['extension'],
-		'title'			=> htmlentities( utf8_decode( strip_tags( $title ) ) ),
-		'class'			=> 'thumbnail',
+		'src'		=> $path.$source.$data['filename'].'.small.'.$data['extension'],
+		'title'		=> htmlentities( utf8_decode( strip_tags( $title ) ) ),
+		'class'		=> 'thumbnail',
 	);
 	$image		= UI_HTML_Tag::create( 'image', NULL, $attributes );
-	$attributes	= array(
-		'href'			=> $path.$source.$data['filename'].'.medium.'.$data['extension'],
-		'class'			=> 'no-thickbox layer-image',
-		'rel'			=> 'gallery',
-		'target'		=> '_blank',
-		'title'			=> $title,
-		'data-original'	=> $source.$data['filename'].'.'.$data['extension'],
-	);
+	if( $mobile )
+		$attributes	= array(
+			'href'	=> './gallery/info/'.$source.$data['filename'].'.'.$data['extension'],
+			'title'		=> $title,
+		);
+	else
+		$attributes	= array(
+			'href'			=> $path.$source.$data['filename'].'.medium.'.$data['extension'],
+			'class'			=> 'no-thickbox layer-image',
+			'rel'			=> 'gallery',
+			'target'		=> '_blank',
+			'title'			=> $title,
+			'data-original'	=> $source.$data['filename'].'.'.$data['extension'],
+		);
+	
 	$image		= UI_HTML_Tag::create( 'a', $image, $attributes );
 	$list[$fileName]		= UI_HTML_Tag::create( 'div', $image, array( 'class' => 'thumbnail' ) );
 }
@@ -70,7 +102,7 @@ $navigation	= View_Helper_Gallery::renderStepNavigation( $env, $source );
 return '
 <script>
 $(document).ready(function(){
-	Gallery.setupIndex();
+	Gallery.setupIndex('.( (integer) (bool) $mobile ).');
 });
 </script>
 <div id="gallery">
