b5bdbf7706f23757714b994de50e0193dc1a720b
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-08-14 18:13:00 +0000

    Modules/Hydrogen: Added new module Resource:Tracker:Google.

---
+++ trunk/modules/Hydrogen/Resource/Tracker/Google/js/ga.js
@@ -0,0 +1,13 @@
+var _gaq = _gaq || [];
+function initGoogleAnalytics(options){
+	_gaq.push(['_setAccount', options.trackingId]);
+	if(options.anonymizeIP)
+		_gaq.push(['_gat._anonymizeIp']);
+	_gaq.push(['_trackPageview']);
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+}
