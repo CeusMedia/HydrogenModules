c0fa1f6561644da57338126be10d7bccbfd00327
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-08-27 02:00:44 +0000

    Modules/Hydrogen: Updated module Admin:Modules viewer by module reload functionality.

--- trunk/modules/Hydrogen/Admin/Modules/templates/admin/module/viewer/general.php
+++ trunk/modules/Hydrogen/Admin/Modules/templates/admin/module/viewer/general.php
@@ -93,6 +93,7 @@ $buttonInstall		= UI_HTML_Elements::LinkButton( './admin/module/installer/index/
 $disabled			= $module->type == 4 ? 'disabled' : '';
 $buttonEdit			= UI_HTML_Elements::LinkButton( './admin/module/editor/'.$module->id, $w->buttonEdit, 'button edit', NULL, $disabled );
 $buttonUninstall	= UI_HTML_Elements::LinkButton( './admin/module/viewer/uninstall/'.$module->id, $w->buttonRemove, 'button remove', 'Die Modulkopie oder -referenz wird gelöscht. Wirklich?', $disabled );
+$buttonReload		= UI_HTML_Elements::LinkButton( './admin/module/viewer/reload/'.$module->id, $w->buttonReload, 'button icon refresh', !$disabled );
 
 return '
 <fieldset>
@@ -112,8 +113,11 @@ return '
 	<div class="buttonbar">
 		'.$buttonBack.'
 		'.$buttonInstall.'
+		&nbsp;|&nbsp;
 		'.$buttonEdit.'
 		'.$buttonUninstall.'
+		&nbsp;|&nbsp;
+		'.$buttonReload.'
 	</div>
 </fieldset>
 ';
