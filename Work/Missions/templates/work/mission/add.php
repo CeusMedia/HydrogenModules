e410eada87f8bada01ae9e0132119c3bf1133063
Christian Würker <kriss@ceusmedia.de>
2012-09-10 10:24:13 +0000

    Modules/Hydrogen: Updated module Work:Missions.

--- Work/Missions/templates/work/mission/add.php
+++ Work/Missions/templates/work/mission/add.php
@@ -3,13 +3,31 @@
 $w	= (object) $words['add'];
 
 $optType		= UI_HTML_Elements::Options( $words['types'], $mission->type );
-$optPriority	= UI_HTML_Elements::Options( $words['priorities'], $mission->priority );
-$optStatus		= UI_HTML_Elements::Options( $words['states'], $mission->status );
+#$optPriority	= UI_HTML_Elements::Options( $words['priorities'], $mission->priority );
+#$optStatus		= UI_HTML_Elements::Options( $words['states'], $mission->status );
+
+$optPriority	= array();
+foreach( $words['priorities'] as $key => $value )
+	$optPriority[]	= UI_HTML_Elements::Option( (string) $key, $value, $mission->priority == $key, NULL, 'mission priority'.$key );
+$optPriority	= join( $optPriority );
+
+$optStatus	= array();
+foreach( $words['states'] as $key => $value )
+	$optStatus[]	= UI_HTML_Elements::Option( (string) $key, $value, $mission->status == $key, NULL, 'mission status'.$key );
+$optStatus	= join( $optStatus );
 
 $optWorker	= array();
 foreach( $users as $user )
 	$optWorker[$user->userId]	= $user->username;
 $optWorker		= UI_HTML_Elements::Options( $optWorker, $mission->workerId );
+
+if( $useProjects ){
+	$optProject	= array();
+	foreach( $userProjects as $relation )
+		if( $projects[$relation->projectId]->status >= 0 )
+			$optProject[$relation->projectId]	= $projects[$relation->projectId]->title;
+	$optProject	= UI_HTML_Elements::Options( $optProject );
+}
 
 $panelAdd	= '
 <form action="./work/mission/add" method="post">
@@ -20,15 +38,7 @@ $panelAdd	= '
 				<label for="input_content">'.$w->labelContent.'</label><br/>
 				<input type="text" name="content" id="input_content" class="max" value="'.htmlentities( $mission->content, ENT_QUOTES, 'UTF-8' ).'"/>
 			</li>
-<!--			<li class="column-left-20">
-				<label for="input_content">'.$w->labelWorker.'</label><br/>
-				<select name="workerId" id="input_workerId" class="max">'.$optWorker.'</select>
-			</li>-->
 			<li>
-				<div class="column-left-20">
-					<label for="input_type">'.$w->labelType.'</label><br/>
-					<select name="type" id="input_type" class="max" onchange="showOptionals(this)">'.$optType.'</select>
-				</div>
 				<div class="column-left-20">
 					<label for="input_priority">'.$w->labelPriority.'</label><br/>
 					<select name="priority" id="input_priority" class="max">'.$optPriority.'</select>
@@ -37,37 +47,49 @@ $panelAdd	= '
 					<label for="input_status">'.$w->labelStatus.'</label><br/>
 					<select name="status" id="input_status" class="max">'.$optStatus.'</select>
 				</div>
+				<div class="column-left-40">
+					<label for="input_projectId">'.$w->labelProjectId.'</label><br/>
+					<select name="projectId" id="input_projectId" class="max">'.$optProject.'</select>
+				</div>
+				<div class="column-left-20">
+					<label for="input_workerId">'.$w->labelWorker.'</label><br/>
+					<select name="workerId" id="input_workerId" class="max">'.$optWorker.'</select>
+				</div>
+				<div class="column-clear"></div>
+			</li>
+			<li class="">
+				<div class="column-left-20">
+					<label for="input_type">'.$w->labelType.'</label><br/>
+					<select name="type" id="input_type" class="max" onchange="showOptionals(this)">'.$optType.'</select>
+				</div>
 				<div class="column-left-20 optional type type-0">
 					<label for="input_day">'.$w->labelDay.'</label><br/>
 					<input type="text" name="day" id="input_day" class="max" value="'.$mission->dayStart.'" autocomplete="off"/>
 				</div>
-				<div class="column-clear"></div>
-			</li>
-			<li class="optional type type-1">
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="input_dayStart">'.$w->labelDayStart.'</label><br/>
 					<input type="text" name="dayStart" id="input_dayStart" class="max" value="'.$mission->dayStart.'" autocomplete="off"/>
 				</div>
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="dayEnd">'.$w->labelDayEnd.'</label><br/>
 					<input type="text" name="dayEnd" id="input_dayEnd" class="max" value="'.$mission->dayEnd.'" autocomplete="off"/>
 				</div>
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="input_timeStart">'.$w->labelTimeStart.'</label><br/>
 					<input type="text" name="timeStart" id="input_timeStart" class="max" value="'.$mission->timeStart.'" autocomplete="off"/>
 				</div>
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="input_timeEnd">'.$w->labelTimeEnd.'</label><br/>
 					<input type="text" name="timeEnd" id="input_timeEnd" class="max" value="'.$mission->timeEnd.'" autocomplete="off"/>
 				</div>
 				<div class="column-clear"></div>
 			</li>
 			<li>
-				<div class="column-left-50">
+				<div class="column-left-40">
 					<label for="input_location">'.$w->labelLocation.'</label><br/>
 					<input type="text" name="location" id="input_location" class="max" value="'.htmlentities( $mission->location, ENT_QUOTES, 'UTF-8' ).'"/>
 				</div>
-				<div class="column-left-50">
+				<div class="column-left-60">
 					<label for="input_reference">'.$w->labelReference.'</label><br/>
 					<input type="text" name="reference" id="input_reference" class="max" value="'.htmlentities( $mission->reference, ENT_QUOTES, 'UTF-8' ).'"/>
 				</div>
--- Work/Missions/templates/work/mission/edit.php
+++ Work/Missions/templates/work/mission/edit.php
@@ -6,13 +6,53 @@ $priorities		= $words['priorities'];
 unset( $priorities[0] );
 
 $optType		= UI_HTML_Elements::Options( $words['types'], $mission->type );
-$optPriority	= UI_HTML_Elements::Options( $priorities, $mission->priority );
-$optStatus		= UI_HTML_Elements::Options( $words['states'], $mission->status );
+
+$optPriority	= array();
+foreach( $priorities as $key => $value )
+	$optPriority[]	= UI_HTML_Elements::Option( $key, $value, $mission->priority == $key, NULL, 'mission priority'.$key );
+$optPriority	= join( $optPriority, $mission->priority );
+
+$optStatus	= array();
+foreach( $words['states'] as $key => $value )
+	$optStatus[]	= UI_HTML_Elements::Option( $key, $value, $mission->status == $key, NULL, 'mission status'.$key );
+$optStatus	= join( $optStatus, $mission->status );
 
 $optWorker	= array();
 foreach( $users as $user )
 	$optWorker[$user->userId]	= $user->username;
 $optWorker		= UI_HTML_Elements::Options( $optWorker, $mission->workerId );
+
+if( $useProjects ){
+	$optProject	= array();
+	foreach( $userProjects as $relation )
+		if( $projects[$relation->projectId]->status >= 0 )
+			$optProject[$relation->projectId]	= $projects[$relation->projectId]->title;
+	$optProject	= UI_HTML_Elements::Options( $optProject, $mission->projectId );
+	
+}
+
+$panelToIssue	= '';
+if( $useIssues ){
+#	print_m( $wordsIssue['types'] );
+#	die;
+	$panelToIssue	= '
+<form action="./work/mission/convert/'.$mission->missionId.'/issue" method="post">
+	<fieldset>
+		<legend>Zu "Problem" konvertieren</legend>
+		<ul class="input">
+			<li class="column-left-40">
+				<label for="input_title">Titel des Problems</label><br/>
+				<input type="text" name="title" id="input_title" class="max"/>
+			</li>
+			<li class="column-left-20">
+				<label for="input_status">Status</label><br/>
+				<select type="text" name="status" id="input_status" class="max"></select>
+			</li>
+		</ul>
+	</fieldset>
+</form>';
+}
+
 
 $panelEdit	= '
 <form action="./work/mission/edit/'.$mission->missionId.'" method="post">
@@ -23,15 +63,7 @@ $panelEdit	= '
 				<label for="input_content">'.$w->labelContent.'</label><br/>
 				<input type="text" name="content" id="input_content" class="max" value="'.htmlentities( $mission->content, ENT_QUOTES, 'UTF-8' ).'"/>
 			</li>
-<!--			<li class="column-left-20">
-				<label for="input_content">'.$w->labelWorker.'</label><br/>
-				<select name="workerId" id="input_workerId" class="max">'.$optWorker.'</select>
-			</li>-->
 			<li>
-				<div class="column-left-20">
-					<label for="input_type">'.$w->labelType.'</label><br/>
-					<select name="type" id="input_type" class="max" onchange="showOptionals(this)">'.$optType.'</select>
-				</div>
 				<div class="column-left-20">
 					<label for="input_priority">'.$w->labelPriority.'</label><br/>
 					<select name="priority" id="input_priority" class="max">'.$optPriority.'</select>
@@ -40,37 +72,49 @@ $panelEdit	= '
 					<label for="input_status">'.$w->labelStatus.'</label><br/>
 					<select name="status" id="input_status" class="max">'.$optStatus.'</select>
 				</div>
-				<div class="column-left-20 optional type type-0">
-					<label for="input_day">'.$w->labelDay.'</label><br/>
-					<input type="text" name="day" id="input_day" value="'.$mission->dayStart.'" autocomplete="off"/>
+				<div class="column-left-40">
+					<label for="input_projectId">'.$w->labelProjectId.'</label><br/>
+					<select name="projectId" id="input_projectId" class="max">'.$optProject.'</select>
+				</div>
+				<div class="column-left-20">
+					<label for="input_content">'.$w->labelWorker.'</label><br/>
+					<select name="workerId" id="input_workerId" class="max">'.$optWorker.'</select>
 				</div>
 				<div class="column-clear"></div>
 			</li>
-			<li class="optional type type-1">
+			<li class="">
 				<div class="column-left-20">
+					<label for="input_type">'.$w->labelType.'</label><br/>
+					<select name="type" id="input_type" class="max" onchange="showOptionals(this)">'.$optType.'</select>
+				</div>
+				<div class="column-left-20 optional type type-0">
+					<label for="input_day">'.$w->labelDay.'</label><br/>
+					<input type="text" name="day" id="input_day" value="'.$mission->dayStart.'" class="max autocomplete="off"/>
+				</div>
+				<div class="column-left-20 optional type type-1">
 					<label for="input_dayStart">'.$w->labelDayStart.'</label><br/>
 					<input type="text" name="dayStart" id="input_dayStart" class="max" value="'.$mission->dayStart.'" autocomplete="off"/>
 				</div>
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="dayEnd">'.$w->labelDayEnd.'</label><br/>
 					<input type="text" name="dayEnd" id="input_dayEnd" class="max" value="'.$mission->dayEnd.'" autocomplete="off"/>
 				</div>
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="input_timeStart">'.$w->labelTimeStart.'</label><br/>
 					<input type="text" name="timeStart" id="input_timeStart" class="max" value="'.$mission->timeStart.'" autocomplete="off"/>
 				</div>
-				<div class="column-left-20">
+				<div class="column-left-20 optional type type-1">
 					<label for="input_timeEnd">'.$w->labelTimeEnd.'</label><br/>
 					<input type="text" name="timeEnd" id="input_timeEnd" class="max" value="'.$mission->timeEnd.'" autocomplete="off"/>
 				</div>
 				<div class="column-clear"></div>
 			</li>
 			<li>
-				<div class="column-left-50">
+				<div class="column-left-40">
 					<label for="input_location">'.$w->labelLocation.'</label><br/>
 					<input type="text" name="location" id="input_location" class="max" value="'.htmlentities( $mission->location, ENT_QUOTES, 'UTF-8' ).'"/>
 				</div>
-				<div class="column-left-50">
+				<div class="column-left-60">
 					<label for="input_reference">'.$w->labelReference.'</label><br/>
 					<input type="text" name="reference" id="input_reference" class="max" value="'.htmlentities( $mission->reference, ENT_QUOTES, 'UTF-8' ).'"/>
 				</div>
@@ -85,58 +129,44 @@ $panelEdit	= '
 </form>
 ';
 
-$list	= array();
-foreach( $words['states'] as $status => $label ){
-	$attributes	= array(
+//  --  STATES  --  //
+$states	= $words['states'];
+unset( $states[0] );
+foreach( $states as $status => $label )
+	$states[$status]	= UI_HTML_Tag::create( 'button', $label, array(
 		'type'		=> 'button',
 		'onclick'	=> 'document.location.href=\'./work/mission/setStatus/'.$mission->missionId.'/'.urlencode( $status ).'\';',
 		'disabled'	=> $mission->status == $status ? 'disabled' : NULL,
 		'class'		=> 'button',
-		'style'		=> 'width: 120px',
-	);
-	$button	= UI_HTML_Tag::create( 'button', $label, $attributes );
-	$list[]	= $button;
-}
-$states	= join( '<br/>', $list );
+	) );
+$states	= join( $states );
 
-
+//  --  PRIORITIES  --  //
 $priorities		= $words['priorities'];
 unset( $priorities[0] );
-$list	= array();
-foreach( $priorities as $priority => $label ){
-	$attributes	= array(
+foreach( $priorities as $priority => $label )
+	$priorities[$priority]	= UI_HTML_Tag::create( 'button', $label, array(
 		'type'		=> 'button',
 		'onclick'	=> 'document.location.href=\'./work/mission/setPriority/'.$mission->missionId.'/'.$priority.'\';',
 		'disabled'	=> $mission->priority == $priority ? 'disabled' : NULL,
 		'class'		=> 'button',
-		'style'		=> 'width: 120px',
-	);
-	$button	= UI_HTML_Tag::create( 'button', $label, $attributes );
-	$list[]	= $button;
-}
-$priorities	= join( '<br/>', $list );
-
-$panelStatus	= '
-<fieldset>
-	<legend>Priorität und Status</legend>
-	<div class="column-left-50">
-		<h3>Priorität</h3>
-		'.$priorities.'
-	</div>
-	<div class="column-left-50">
-		<h3>Status</h3>
-		'.$states.'
-	</div>
-</fieldset>
-';
+	) );
+$priorities	= join( $priorities );
 
 return '
-<div class="column-right-30">
-	'.$panelStatus.'
-</div>
-<div class="column-left-70">
+<div class="column-left-75">
 	'.$panelEdit.'
+	'.$panelToIssue.'
+	<fieldset>
+		<legend>Status setzen</legend>
+		'.$states.'
+	</fieldset>
+	<fieldset>
+		<legend>Priorität ändern</legend>
+		'.$priorities.'
+	</fieldset>
 </div>
-<div class="column-clear"></div>
-';
+<div class="column-right-25">
+</div>
+<div class="column-clear"></div>';
 ?>
