9a9e9ca72e4780a4da41ce03577da71769fab479
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-09-20 04:15:34 +0000

    Modules/Hydrogen: Updated module Manage:My:User:Invites.

--- trunk/modules/Hydrogen/Manage/My/User/Invite/templates/manage/my/user/invite/index.php
+++ trunk/modules/Hydrogen/Manage/My/User/Invite/templates/manage/my/user/invite/index.php
@@ -3,13 +3,131 @@ $modelUser	= new Model_User( $env );
 $userId		= $session->get( 'userId' );
 $user		= $modelUser->get( $userId );
 
-$list	= array();
-foreach( $invites->codes as $invite ){
-	$url	= './manage/my/user/invite/invite/?code='.$invite->userInviteId;
-	$link	= UI_HTML_Tag::create( 'a', $invite->code, array( 'href' => $url ) );
-	$list[]	= UI_HTML_Tag::create( 'li', $link );
+$buttonInvite	= UI_HTML_Elements::LinkButton( './manage/my/user/invite/invite', 'jemanden einladen', 'button icon add' );
+
+$listInvites	= '<em><small>Noch keine Einladungen verschickt.</em></small><br/>';
+if( $invites->all ){
+	$listInvites	= array();
+	foreach( $invites->all as $invite ){
+		$email		= UI_HTML_Tag::create( 'span', $invite->email );
+		$date		= date( 'd.m.Y H:i', $invite->createdAt );
+		$status		= UI_HTML_Tag::create( 'span', $words['states'][$invite->status] );
+		$buttons	= array();
+		if( $invite->type == 1 ){
+			if( $invite->status == 1 ){
+				$expire	= date( 'd.m.Y', $invite->createdAt + $daysValid * 24 * 60 * 60 );
+				$expire	= sprintf( 'verfällt am %s', $expire );
+				$image	= UI_HTML_Tag::create( 'img', NULL, array( 'src' => 'http://img.int1a.net/famfamfam/silk/error.png' ) );
+				$date	.= '&nbsp;'.UI_HTML_Tag::create( 'span', $image, array( 'title' => $expire ) );
+				$buttons[]	= UI_HTML_Elements::LinkButton( './manage/my/user/invite/cancel/'.$invite->userInviteId, '', 'button tiny remove', NULL, NULL, 'abbrechen' );
+			}
+		}
+		$cells		= array();
+		$cells[]	= UI_HTML_Tag::create( 'td', $date, array( 'class' => 'invite-date' ) );
+		$cells[]	= UI_HTML_Tag::create( 'td', $status, array( 'class' => 'invite-status' ) );
+		$cells[]	= UI_HTML_Tag::create( 'td', $email, array( 'class' => 'invite-email' ) );
+		$cells[]	= UI_HTML_Tag::create( 'td', join( $buttons ), array( 'class' => 'invite-actions' ) );
+		$listInvites[]	= UI_HTML_Tag::create( 'tr', join( $cells ), array( 'class' => 'invite-status status'.$invite->status ) );
+	}
+	$listInvites	= join( $listInvites );
+	$colgroup		= UI_HTML_Elements::ColumnGroup( '140px', '120px', '', '50px' );
+	$tableHeads		= UI_HTML_Elements::TableHeads( array( 'Datum', 'Status', 'E-Mail-Adresse', '' ) );
+	$listInvites	= '
+<style>
+.invite-status.status-2{
+	background-color: #FF9F9F;
+	opacity: 0.75;
+	}
+.invite-status.status-1{
+	background-color: #DFDFDF;
+	opacity: 0.5;
+	}
+.invite-status.status0{
+	background-color: #FFFFFF;
+	}
+.invite-status.status1{
+	background-color: #FFFF7F;
+	}
+.invite-status.status2{
+	background-color: #9FFF9F;
+	}
+td.invite-actions{
+	text-align: right;
+	}
+</style>
+<fieldset>
+	<legend class="icon info">Einladungen</legend>
+	<table>
+		'.$colgroup.'
+		<thead>
+			'.$tableHeads.'
+		</thead>
+		<tbody>
+			'.$listInvites.'
+		</tbody>
+	</table>
+</fieldset>';
 }
-$list	= UI_HTML_Tag::create( 'ul', join( $list ) );
+
+$text	= '
+###Plan###
+**3 Modi:**
+olist>
+- promote - Mail schicken
+- invite - Invite-Code schicken
+- bonus - Coupon-Code schicken
+<olist
+###Fehlt noch###
+list>
+- Integration <cite>Invite-Code</cite> => <cite>Auth</cite>
+- Controller ::cancel
+- Helper für <cite>Manage:My:User</cite>
+- Template für <cite>promote</cite> Modus
+- Locale <cite>en/invite</cite>
+- CSS >>(aus Template <cite>index</cite>)<< auslagern
+- Modus <cite>bonus</cite> 
+- Einlade-Limit pro Monat >>(hardlimit)<<
+- Limit pro Benutzer pro Monat  >>(softlimit)<<
+<list
+';
+
+return '
+<div class="column-left-70">
+	'.$listInvites.'
+	'.$buttonInvite.'
+</div>
+<div class="column-right-30">
+	<fieldset>
+		<legend class="icon info">Informationen</legend>
+		'.View_Helper_ContentConverter::render( $env, $text ).'
+	</fieldset>
+</div>
+<div class="column-clear"></div>
+';
+
+
+/*
+$listInvitesOpen	= '<em><small>Keine.</em></small>';
+if( $invites->open ){
+	$listInvitesOpen	= array();
+	foreach( $invites->open as $invite ){
+		$url	= './manage/my/user/invite/invite/?code='.$invite->userInviteId;
+		$link	= UI_HTML_Tag::create( 'a', $invite->email, array( 'href' => $url ) );
+		$listInvitesOpen[]	= UI_HTML_Tag::create( 'li', $link );
+	}
+	$listInvitesOpen	= UI_HTML_Tag::create( 'ul', join( $listInvitesOpen ) );
+}
+
+$listInvitesDone	= '<em><small>Keine.</em></small>';
+if( $invites->done ){
+	$listInvitesDone	= array();
+	foreach( $invites->done as $invite ){
+		$url	= './manage/my/user/invite/invite/?code='.$invite->userInviteId;
+		$link	= UI_HTML_Tag::create( 'a', $invite->email, array( 'href' => $url ) );
+		$listInvitesDone[]	= UI_HTML_Tag::create( 'li', $link );
+	}
+	$listInvitesDone	= UI_HTML_Tag::create( 'ul', join( $listInvitesDone ) );
+}
 
 return '
 <dl>
@@ -17,8 +135,11 @@ return '
 	<dd>'.$userId.'</dd>
 	<dt>User Name</dt>
 	<dd>'.$user->username.'</dd>
-	<dt>Invite Codes ('.count( $invites->codes ).')</dt>
-	<dd>'.$list.'</dd>
-</dl>
-';
+	<dt>Invite Open</dt>
+	<dd>'.$listInvitesOpen.'</dd>
+	<dt>Invite Done</dt>
+	<dd>'.$listInvitesDone.'</dd>
+</dl>'.$buttonInvite;
+ */
+
 ?>
--- trunk/modules/Hydrogen/Manage/My/User/Invite/templates/manage/my/user/invite/invite.php
+++ trunk/modules/Hydrogen/Manage/My/User/Invite/templates/manage/my/user/invite/invite.php
@@ -3,30 +3,69 @@ $subject	= 'Einladung zu "%s"';
 $subject	= sprintf( $subject, $config->get( 'app.name' ) );
 if( $request->get( 'subject' ) )
 	$subject	= $request->get( 'subject' );
+$message	= 'Hallo!
+
+Ich lade dich hiermit ein, bei "%1$s" teilzunehmen.
+Du bekommst dazu einen meiner exklusiven Einladungs-Codes.
+Klicke den unten stehenden Link, um zur Registrierung zu kommen.
+
+Liebe Grüße,
+%2$s';
+if( $request->get( 'message' ) )
+	$message	= $request->get( 'message' );
+$userName	= $user->username;
+if( !empty( $user->firstname ) && !empty( $user->surname ) )
+	$userName	= $user->firstname.' '.$user->surname;
+else if( !empty( $user->firstname ) )
+	$userName	= $user->firstname;
+$message	= sprintf( $message, $config->get( 'app.name' ), $userName );
+
+if( $env->getModules()->has( 'Manage_Projects' ) ){
+	$modelProject	= new Model_Project( $env );
+	$projects		= $modelProject->getUserProjects( $env->getSession()->get( 'userId' ) );
+	$optProject		= array();
+	foreach( $projects as $project )
+		$optProject[$project->projectId]	= $project->title;
+	$optProject		= UI_HTML_Elements::Options( $optProject, $request->get( 'projectId' ) );
+	
+}
 
 return '
-<form action="./manage/my/user/invite/?code='.$code.'" method="post">
-	<fieldset>
-		<legend>Einladung</legend>
-		<ul class="input">
-			<li>
-				<label for="input_email" class="mandatory">E-Mail-Adresse</label><br/>
-				<input type="text" name="email" id="input_email" class="max mandatory" value="'.htmlentities( $request->get( 'email' ), ENT_COMPAT, 'UTF-8' ).'"/>
-			</li>
-			<li>
-				<label for="input_subject" class="mandatory">Betreff</label><br/>
-				<input type="text" name="subject" id="input_subject" class="max mandatory" value="'.htmlentities( $subject, ENT_COMPAT, 'UTF-8' ).'"/>
-			</li>
-			<li>
-				<label for="input_message" class="mandatory">Nachricht <small>(Link zur Registrierung wird automatisch eingetragen)</small></label><br/>
-				<textarea name="message" id="input_message" class="m-m max mandatory"></textarea>
-			</li>
-		</ul>
-		<div class="buttonbar">
-			'.UI_HTML_Elements::LinkButton( './', 'zurück', 'button cancel' ).'
-			'.UI_HTML_Elements::Button( 'send', 'senden', 'button save' ).'
-		</div>
+<div class="column-left-70">
+	<form action="./manage/my/user/invite/invite" method="post">
+		<fieldset>
+			<legend class="icon edit">Einladung</legend>
+			<ul class="input">
+				<li class="column-left-30">
+					<label for="input_email" class="mandatory">E-Mail-Adresse</label><br/>
+					<input type="text" name="email" id="input_email" class="max mandatory" value="'.htmlentities( $request->get( 'email' ), ENT_COMPAT, 'UTF-8' ).'"/>
+				</li>
+				<li class="column-left-40">
+					<label for="input_subject" class="not-mandatory">Betreff</label><br/>
+					<input type="text" name="subject" id="input_subject" class="max mandatory" value="'.htmlentities( $subject, ENT_COMPAT, 'UTF-8' ).'"/>
+				</li>
+				<li class="column-left-30">
+					<label for="input_projectId" class="">Projekt</label><br/>
+					<select name="projectId" id="input_projectId" class="max">'.$optProject.'</select>
+				</li>
+				<li>
+					<label for="input_message" class="not-mandatory">Nachricht <small>(Link zur Registrierung wird automatisch eingetragen)</small></label><br/>
+					<textarea name="message" id="input_message" class="max mandatory" rows="10">'.$message.'</textarea>
+				</li>
+			</ul>
+			<div class="buttonbar">
+				'.UI_HTML_Elements::LinkButton( './manage/my/user/invite', 'zurück', 'button cancel' ).'
+				'.UI_HTML_Elements::Button( 'send', 'senden', 'button save' ).'
+			</div>
+		</fieldset>
+	</form>
+</div>
+<div class="column-right-30">
+<!--	<fieldset>
+		<legend class="icon info">Informationen</legend>
+		...
 	</fieldset>
-</form>
+--></div>
+<div class="column-clear"></div>
 ';
 ?>
