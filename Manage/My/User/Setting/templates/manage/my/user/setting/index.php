7420b15ec5f498938d67eeac85c9cd0683721152
Christian Würker <kriss@ceusmedia.de>
2012-10-23 01:58:40 +0000

    Modules/Hydrogen: Updated module Manage:My:User:Settings.

--- Manage/My/User/Setting/templates/manage/my/user/setting/index.php
+++ Manage/My/User/Setting/templates/manage/my/user/setting/index.php
@@ -1,48 +1,11 @@
 <?php
 
-$helper	= new View_Helper_MyUserConfig( $env );
-$table	= $helper->renderTable( $settings, 60, 40 );
+$helper	= new View_Helper_UserModuleSettings( $env );
+$panel	= $helper->renderPanel();
 
-if( !$table )
-	return '';
-return '
-<style>
-tr.changed {
-	background-color: #EFEFEF; 
-	}
-legend.config {
-	background-image: url(http://img.int1a.net/famfamfam/silk/wrench.png);
-	}
-table td {
-	vertical-align: text-bottom;
-	}
-table td.label,
-table td.input {
-	border-top: 1px solid #DFDFDF;
-	}
-table td.label {
-	font-size: 0.9em;
-	}
-table td.input {
-/*	text-align: right;
-*/	font-size: 1em;
-	}
-table span.button-reset {
-	float: right;
-	}
-span.suffix {
-	margin-left: 0.5em;
-	}
-</style>
-<div class="column-left-70">
-	<fieldset>
-		<legend class="icon config">Einstellungen</legend>
-		<form name="" action="./manage/my/user/setting/update" method="post">
-			'.$table.'
-			<div class="buttonbar">
-				'.UI_HTML_Elements::Button( 'save', 'speichern', 'button save' ).'
-			</div>
-		</form>
-	</fieldset>
-</div>';
+if( $panel )
+	return $panel;
+
+$words	= (object) $words['index'];
+return $words->noSettings;
 ?>
