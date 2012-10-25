5254c468cff07258cf0240ee1fa6c21f483b21e5
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-10-25 13:49:56 +0000

    Modules/Hydrogen: Updated module Index to look for HTML files and respond with HTTP code 404 on dispatch failure.

--- trunk/modules/Hydrogen/Index/templates/index/index.php
+++ trunk/modules/Hydrogen/Index/templates/index/index.php
@@ -1,17 +1,10 @@
 <?php
-$userId         = $env->getSession()->get( 'userId' );
-
-if( $userId ){
-        $helper = new View_Helper_MissionCalendar( $env );
-        return $helper->render();
-}
 if( $view->hasContent( 'index', 'index', 'html/' ) ){
 	$content	= $view->loadContent( 'index', 'index', 'html/' );
 	if( $env->getModules()->has( 'UI_Helper_Content' ) )
 		$content	= View_Helper_ContentConverter::render( $env, $content );
 	return $content;
 }
-
 return '
 Hello World!
 ';
