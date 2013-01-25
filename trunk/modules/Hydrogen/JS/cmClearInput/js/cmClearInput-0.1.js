6be79e8c133745ddd21578497ce8d3e3da5edf1f
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2013-01-25 00:55:11 +0000

    Modules/Hydrogen: Updated module JS:cmClearInput.

--- trunk/modules/Hydrogen/JS/cmClearInput/js/cmClearInput-0.1.js
+++ trunk/modules/Hydrogen/JS/cmClearInput/js/cmClearInput-0.1.js
@@ -6,30 +6,58 @@
  *	@author		Christian Würker <christian.wuerker@ceusmedia.de>
  *	@copyright	2013 Christian Würker <christian.wuerker@ceusmedia.de> (http://ceusmedia.de)
  *	@license	LGPL/CC
+ *	@option		offsetTop			vertical offset (downwards from top border of input field)
+ *	@option		offsetRight			horizontal offset (to the left from right border of input field)
  */
 (function($){
 	jQuery.fn.cmClearInput = function(method){
 
-		var methods = {																				//  methods callable using constructor
+		var methods = {															//  methods callable using constructor
 			init: function(options){
-				var settings = jQuery.extend({														//  options and defaults
+				var settings = jQuery.extend({									//  options and defaults
+					offsetTop: 5,												//  vertical offset (downwards from top border of input field)
+					offsetRight: 6												//  horizontal offset (to the left from right border of input field)
 				}, options);
 
-				return this.each(function(){			//
-					var input = $(this);
-					var trigger = $("<span></span>").addClass("cmClearInput-trigger");
-					trigger.insertAfter(input);
-					positionTrigger(input, trigger);
-					input.bind("change.updateClearTrigger",{trigger: trigger}, function(event){
-						updateTriggerVisibility($(this), event.data.trigger);
-					}).trigger("change.updateClearTrigger");
-					trigger.bind("click", {input: input}, function(event){
-						event.data.input.val("");
-						event.data.input.trigger("keyup.*");
-						event.data.input.trigger("change.*");
-					});
+				return this.each(function(){									//  apply plugin to matching elements
+					setupTrigger($(this), settings);							//  set up trigger, its events and input field events
 				});
 			}
+		}
+
+		function setupTrigger(input, settings){
+			var trigger = $("<span></span>").addClass("cmClearInput-trigger");	//  create trigger
+			var data = {input: input, trigger: trigger};						//  collect data for events
+			positionTrigger(input, trigger.insertAfter(input), settings);		//  inject and position trigger
+			input.bind("change.updateClearTrigger", data, function(event){		//  bind custom event on input field ...
+				updateTriggerVisibility($(this), event.data.trigger);			//  ... to update trigger on change
+			}).trigger("change.updateClearTrigger");							//  call this event initially
+			trigger.bind("click", data, function(event){						//  bind click event on trigger
+				event.data.input.val("");										//  clear input field
+				event.data.input.trigger("keyup.*");							//  trigger bound key events
+				event.data.input.trigger("change.*");							//  trigger bound change events
+			});
+		}
+
+		function positionTrigger(input, trigger, settings){
+			input.parent().css("position", "relative");							//  position container relative for absolute positioned trigger
+			var left = input.outerWidth();										//  horizontal position is outer width of input field ...
+			left -= parseInt(input.css("border-right"));						//  ... minus right border ...
+			left -= parseInt(input.css("margin-right"));						//  ... minus right margin ...
+			left -= trigger.width() + settings.offsetRight;						//  ... minus trigger width and offset ...
+			left = (left / input.parent().width() * 100) + "%";					//  ... relative to container around input field
+			var top = input.position().top + parseInt(input.css("margin-top"));	//  vertical space above input field ...
+			top += parseInt(input.css("border-top")) + settings.offsetTop;		//  ... including border and trigger offset
+			trigger.css({left: left, top: top});								//  position trigger
+		}
+
+		function updateTriggerVisibility(input, trigger){
+			var isVisible = trigger.is(":visible");								//  store trigger visibility
+			var shouldBe = input.val().length > 0;								//  note if input field has a value
+			if(!isVisible && shouldBe)											//  input field has value but trigger is hidden
+				trigger.show();													//  show trigger
+			else if(isVisible && !shouldBe)										//  input field is empty but trigger is visible
+				trigger.hide();													//  hide trigger
 		}
 
 		// Method calling logic
@@ -37,28 +65,6 @@
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));			//  call method with arguments
 		else if(typeof(method) === 'object' || ! method)											//  options object or nothing given
 			return methods.init.apply(this, arguments);												//  call init method with arguments
-		$.error( 'Method ' +  method + ' does not exist on cmSelectBox' );							//  report invalid method call
-
-		function positionTrigger(input, trigger){
-			input.parent().css("position", "relative");
-			var left = input.outerWidth();
-			left -= parseInt(input.css("border-right")) + parseInt(input.css("margin-right"));
-			left -= trigger.width() + 5;
-			var top = input.position().top + 6;
-			top += parseInt(input.css("margin-top")) + parseInt(input.css("border-top"));
-			trigger.css({
-				left: (left / input.parent().width() * 100) + "%",
-				top: top
-			});
-		}
-
-		function updateTriggerVisibility(input, trigger){
-			var isVisible = trigger.is(":visible");
-			var shouldBe = input.val().length > 0;
-			if(!isVisible && shouldBe)
-				trigger.show();
-			else if(isVisible && !shouldBe)
-				trigger.hide();
-		}
+		$.error( 'Method ' +  method + ' does not exist on cmClearInput' );							//  report invalid method call
 	};
 })( jQuery );
