9e09f8cf2be3096f270bf14803eddf22d4d1bd4a
Christian Würker <kriss@ceusmedia.de>
2013-02-23 19:08:36 +0000

    Modules/Hydrogen: Updated module Manage:Projects to use module UI:Bootstrap.

--- Manage/Projects/templates/manage/project/index.php
+++ Manage/Projects/templates/manage/project/index.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 
 $filterStatus	= $session->get( 'filter_manage_project_status' );
 if( !is_array( $filterStatus ) )
@@ -26,29 +26,34 @@ $iconDown   = UI_HTML_Elements::Image( 'http://img.int1a.net/famfamfam/silk/arro
 $disabled   = $filterDirection == 'ASC';
 $buttonUp   = UI_HTML_Elements::LinkButton( './manage/project/filter/?direction=ASC', $iconUp, 'tiny', NULL, $disabled );
 $buttonDown = UI_HTML_Elements::LinkButton( './manage/project/filter/?direction=DESC', $iconDown, 'tiny', NULL, !$disabled );
+$buttonUp	= UI_HTML_Elements::LinkButton( './manage/project/filter/?direction=ASC', '<i class="icon-arrow-up"></i>', 'btn btn-mini', NULL, $disabled );
+$buttonDown	= UI_HTML_Elements::LinkButton( './manage/project/filter/?direction=DESC', '<i class="icon-arrow-down"></i>', 'btn btn-mini', NULL, !$disabled );
 
 $buttonFilter	= UI_HTML_Elements::Button( 'filter', $words['filter']['buttonFilter'], 'button filter add' );
 $buttonReset	= UI_HTML_Elements::LinkButton( './manage/project/filter/reset', $words['filter']['buttonReset'], 'button filter reset remove' );
+$buttonFilter	= UI_HTML_Elements::Button( 'filter', '<i class="icon-search icon-white"></i> '.$words['filter']['buttonFilter'], 'btn btn-small btn-primary' );
+$buttonReset	= UI_HTML_Elements::LinkButton( './manage/project/filter/reset', '<i class="icon-zoom-out icon-white"></i> '.$words['filter']['buttonReset'], 'btn btn-small btn-inverse' );
 
 $panelFilter	= '
 	<form name="" action="./manage/project/filter" method="post">
 		<fieldset>
 			<legend class="icon filter">'.$words['filter']['legend'].'</legend>
-			<ul class="input">
-				<li>
-					<label for="input_status">Status</label><br/>
-					<select name="status[]" multiple id="input_status" class="max row-6">'.$optStatus.'</select>
-				</li>
-				<li>
-					<label for="input_status">Sortierung</label><br/>
-					<div class="column-left-60">
-						<select name="order" id="input_order" class="max" onchange="this.form.submit()">'.$optOrder.'</select>
-					</div>
-					<div class="column-left-40">
-						'.$buttonUp.$buttonDown.'
-					</div>
-				</li>
-			</ul>
+			<div class="row-fluid">
+				<div class="span12">
+					<label for="input_status">Status</label>
+					<select name="status[]" multiple id="input_status" class="span12 -max row-6">'.$optStatus.'</select>
+				</div>
+			</div>
+			<div class="row-fluid">
+				<div class="span8">
+					<label for="input_status">Sortierung</label>
+					<select name="order" id="input_order" class="span12 -max" onchange="this.form.submit()">'.$optOrder.'</select>
+				</div>
+				<div class="span4">
+					<label>&nbsp;</label>
+					'.$buttonUp.$buttonDown.'
+				</div>
+			</div>
 			<div class="buttonbar">
 				'.$buttonFilter.'
 				'.$buttonReset.'
@@ -69,12 +74,13 @@ foreach( $projects as $project ){
 	$users		= array();
 	foreach( $project->users as $projectUser )
 		$users[]	= $projectUser->username;
-	$descLines	= explode( "\n", trim( $project->description ) );
+/*	$descLines	= explode( "\n", trim( $project->description ) );
 	$desc		= trim( array_shift( $descLines ) );
 	$desc		= Alg_Text_Trimmer::trim( $desc, 50 );
 	if( count( $descLines ) )
 		$desc	.= '&nbsp;<small><em>(...)</em></small>';
-	$graph		= $indicator->build( $project->status + 4, 9, 80 );
+*/	$desc		= trim( $project->description );
+	$graph		= $indicator->build( $project->status + 2, 5, 80 );
 	$status		= $words['states'][$project->status];
 	$cells[]	= UI_HTML_Tag::create( 'td', '<small>'.$status.'</small><br/>'.$graph );
 	$cells[]	= UI_HTML_Tag::create( 'td', $link.'<br/>'.$desc );
@@ -89,6 +95,7 @@ $colgroup	= UI_HTML_Elements::ColumnGroup( array( '10%', '35%', '25%', '15%', '1
 $list		= UI_HTML_Tag::create( 'table', $colgroup.$heads.join( $rows ) );
 
 $buttonAdd	= UI_HTML_Elements::LinkButton( './manage/project/add', $words['index']['buttonAdd'], 'button add' );
+$buttonAdd	= UI_HTML_Elements::LinkButton( './manage/project/add', '<i class="icon-plus icon-white"></i> '.$words['index']['buttonAdd'], 'btn btn-info' );
 if( !$canAdd )
 	$buttonAdd	= UI_HTML_Elements::LinkButton( './manage/project/add', $words['index']['buttonAdd'], 'button add disabled', NULL, TRUE );
 $panelList	= '
