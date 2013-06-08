e425a337eb99df4a93f48325e6a2c7e66bf97662
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2013-06-08 13:14:01 +0000

    Modules/Hydrogen: Updated module Manage:Pages.

--- trunk/modules/Hydrogen/Manage/Pages/templates/manage/page/edit.meta.php
+++ trunk/modules/Hydrogen/Manage/Pages/templates/manage/page/edit.meta.php
@@ -3,19 +3,22 @@
 return '
 <div class="row-fluid">
 	<div class="span6">
-		<div class="row-fluid">
-			<div class="span12">
-				<label for="input_keywords">Schlagwörter <small class="muted">(kommagetrennt)</small></label>
-				<input class="span12" type="text" name="keywords" id="input_keywords" value="'.htmlentities( $page->keywords, ENT_QUOTES, 'UTF-8' ).'"/>
-			</div>
-		</div>
+		<h4>Werte für diese Seite</h4>
+		<p><small class="muted">Wenn keine Werte gespeichert wurden, werden die Standartwerte benutzt.</small></p>
 		<div class="row-fluid">
 			<div class="span12">
 				<label for="input_description">Beschreibung</label>
-				<textarea class="span12" rows="3" name="description" id="input_description">'.htmlentities( $page->description, ENT_QUOTES, 'UTF-8' ).'</textarea>
+				<textarea class="span12" rows="2" name="description" id="input_description">'.htmlentities( $page->description, ENT_QUOTES, 'UTF-8' ).'</textarea>
 			</div>
 		</div>
 		<div class="row-fluid">
+			<div class="span12">
+				<label for="input_keywords">Schlagwörter <small class="muted">(kommagetrennt)</small></label>
+<!--				<input class="span12" type="text" name="keywords" id="input_keywords" value="'.htmlentities( $page->keywords, ENT_QUOTES, 'UTF-8' ).'"/>-->
+				<textarea class="span12" rows="2" name="keywords" id="input_keywords">'.htmlentities( $page->keywords, ENT_QUOTES, 'UTF-8' ).'</textarea>
+			</div>
+		</div>
+<!--		<div class="row-fluid">
 			<div class="span6">
 				<label for="input_author">Autor</label>
 				<input class="span12" type="text" name="author" id="input_author" value="'.htmlentities( ""/*$page->author*/, ENT_QUOTES, 'UTF-8' ).'"/>
@@ -24,19 +27,20 @@ return '
 				<label for="input_publisher">Herausgeber</label>
 				<input class="span12" type="text" name="publisher" id="input_publisher" value="'.htmlentities( ""/*$page->publisher*/, ENT_QUOTES, 'UTF-8' ).'"/>
 			</div>
-		</div>
+		</div>-->
 	</div>
 	<div class="span6">
-		<h4>Standard-Werte</h4>
+		<h4>Standardwerte</h4>
+		<div><small class="muted">Diese Werte wurden im Meta-Modul der Website definiert.</small></div>
 		<dl class="dl-horizontal">
 			<dt>Beschreibung</dt>
 			<dd>'.$meta['description'].'</dd>
 			<dt>Schlüsselwörter</dt>
 			<dd>'.$meta['keywords'].'</dd>
-			<dt>Autor</dt>
+<!--			<dt>Autor</dt>
 			<dd>'.$meta['author'].'</dd>
 			<dt>Herausgeber</dt>
-			<dd>'.$meta['publisher'].'</dd>
+			<dd>'.$meta['publisher'].'</dd>-->
 		</dl>
 	</div>
 </div>';
