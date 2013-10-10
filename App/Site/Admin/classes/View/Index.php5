fbfcc3aa34735d952cbbbf31c3ab28c1f1193295
Christian Würker <kriss@ceusmedia.de>
2013-10-10 03:20:17 +0000

    Modules/Hydrogen: Updated module App:Site:Admin.

---
+++ App/Site/Admin/classes/View/Index.php5
@@ -0,0 +1,6 @@
+<?php
+class View_Index extends CMF_Hydrogen_View{
+	public function index(){
+	}
+}
+?>
---
+++ App/Site/Admin/templates/index/index.php
@@ -0,0 +1,18 @@
+<?php
+
+//  --  LOAD STATIC HTML CONTENT FILE  --  //
+if( !empty( $path ) ){
+	if( $view->hasContentFile( 'html/'.$path.'.html' ) )
+		return $view->loadContentFile( 'html/index.html' );
+}
+if( $view->hasContentFile( 'html/index.html' ) ){
+	return $view->loadContentFile( 'html/index.html' );
+}
+
+//  --  OR RETURN TEMPLATE CONTENT --  //
+return '
+<h2>Hello World!</h2>
+It seems you just have installed the (rather empty) <cite>Hydrogen</cite> module <cite>App:Site:Admin</cite>.<br/>
+You can change this page by create a localized HTML files in "locales/[LANGUAGE]/html/index.html".<br/>
+';
+?>
