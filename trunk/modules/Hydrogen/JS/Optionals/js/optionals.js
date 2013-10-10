e335ef861718dc7c55ae6c38112dc4d2d2272178
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2013-10-10 10:53:51 +0000

    Modules/Hydrogen: Added new module JS:Optionals.

---
+++ trunk/modules/Hydrogen/JS/Optionals/js/optionals.js
@@ -0,0 +1,51 @@
+
+var FormOptionals = {
+    init: function () {
+        $("select.has-optionals").bind("change", FormOptionals.showOptionals);
+        $("select").each(function () {
+           var name = $(this).attr("name");
+           if($(this).children("option").size()){
+               var first = $(this).children("option").eq(0).attr("value");
+               var key = name + "-" + first;
+               if($(this).closest("form").find(".optional."+key).size()){
+                   $(this).bind("change", FormOptionals.showOptionals);
+               }
+           }
+        });
+    },
+    showOptionals: function (elem) {
+        var form = $(elem.form);
+        var name = $(elem).attr("name");
+        var type = $(elem).attr("type");
+        var value = name+"-" + $(elem).val();
+        if (type === "checkbox") {
+            value = name + "-" + $(elem).prop("checked");
+        }
+        var toHide = form.find(".optional." + name).not("." + value);
+        var toShow = form.find(".optional." + value);
+        if (!$(elem).data("status")) {
+            toHide.hide();
+            toShow.show();
+            $(elem).data("status", 1);
+            return;
+        }
+        switch ($(elem).data('animation')) {
+        case 'fade':
+            toHide.fadeOut();
+            toShow.fadeIn();
+            break;
+        case 'slide':
+            toHide.slideUp($(elem).data('speed-hide'));
+            toShow.slideDown($(elem).data('speed-show'));
+            break;
+        default:
+            toHide.hide();
+            toShow.show();
+        }
+    }
+    
+};
+
+function showOptionals (elem) {
+    FormOptionals.showOptionals(elem);
+}
