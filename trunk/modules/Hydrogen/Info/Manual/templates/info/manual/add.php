be27fa597b27a51fd363ef2ef9a93f0cac5d9508
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2013-12-16 12:02:11 +0000

    Modules/Hydrogen: Updated module Info:Manual.

--- trunk/modules/Hydrogen/Info/Manual/templates/info/manual/add.php
+++ trunk/modules/Hydrogen/Info/Manual/templates/info/manual/add.php
@@ -1,5 +1,5 @@
 <?php
-$list	= '<div><em class="muted">Keine Dokumente</em></div><br/>';
+$list	= '<div><em class="muted">'.$words['list']['empty'].'</em></div><br/>';
 if( $files ){
 	$list	= array();
 	foreach( $order as $entry ){
@@ -10,35 +10,44 @@ if( $files ){
 	$list	= UI_HTML_Tag::create( 'ul', $list, array( 'class' => 'nav nav-pills nav-stacked' ) );
 }
 
+$buttonAdd		= "";
+$buttonReload	= "";
+if( $moduleConfig->get( 'editor' ) ){
+	$icon			= UI_HTML_Tag::create( 'i', '', array( 'class' => 'icon-plus icon-white' ) );
+	$buttonAdd		= UI_HTML_Tag::create( 'a', $icon.' '.$words['list']['buttonAdd'], array( 'href' => './info/manual/add', 'class' => "btn btn-small btn-info" ) );
+	$icon			= UI_HTML_Tag::create( 'i', '', array( 'class' => 'icon-refresh' ) );
+	$buttonReload	= UI_HTML_Tag::create( 'a', $icon.' '.$words['list']['buttonReload'], array( 'href' => './info/manual/reload', 'class' => "btn btn-small" ) );
+}
+
 return '
 <div class="row-fluid">
 	<div class="span3">
-		<h3>Dokumente</h3>
+		<h3>'.$words['list']['heading'].'</h3>
 		'.$list.'
-		<a href="./info/manual/add" class="btn btn-small btn-primary"><i class="icon-plus icon-white"></i> neues Dokument</a>
+		'.$buttonAdd.'
+		'.$buttonReload.'
 	</div>
 	<div class="span9">
-		<h3>Editor</h3>
+		<h3>'.$words['edit']['heading'].'</h3>
 		<form action="./info/manual/add" method="post">
 			<div class="row-fluid">
 				<div class="span12">
-					<label for="input_content">Inhalt</label>
+					<label for="input_content">'.$words['edit']['labelContent'].'</label>
 					<textarea class="span12 CodeMirror" name="content" id="input_content" rows="'.$moduleConfig->get( 'editor.rows' ).'">'.$content.'</textarea>
 				</div>
 			</div>
 			<div class="row-fluid">
 				<div class="span4">
-					<label for="input_filename">Dateiname</label>
+					<label for="input_filename">'.$words['edit']['labelFilename'].'</label>
 					<input type="text" name="filename" class="span12" value="'.htmlentities( $filename, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
 				</div>
 				<div class="span4">
 				</div>
 			</div>
 			<div class="buttonbar">
-				<button type="submit" name="save" class="btn btn-small btn-success"><i class="icon-ok icon-white"></i> speichern</button>
+				<button type="submit" name="save" class="btn btn-small btn-success"><i class="icon-ok icon-white"></i> '.$words['edit']['buttonSave'].'</button>
 			</div>
 		</form>
 	</div>
 </div>';
-
 ?>
--- trunk/modules/Hydrogen/Info/Manual/templates/info/manual/edit.php
+++ trunk/modules/Hydrogen/Info/Manual/templates/info/manual/edit.php
@@ -1,5 +1,5 @@
 <?php
-$list	= '<div><em class="muted">Keine Dokumente</em></div><br/>';
+$list	= '<div><em class="muted">'.$words['list']['empty'].'</em></div><br/>';
 if( $files ){
 	$list	= array();
 	foreach( $order as $entry ){
@@ -11,42 +11,52 @@ if( $files ){
 	$list	= UI_HTML_Tag::create( 'ul', $list, array( 'class' => 'nav nav-pills nav-stacked' ) );
 }
 
+$buttonAdd	= "";
+$buttonReload	= "";
+if( $moduleConfig->get( 'editor' ) ){
+	$icon			= UI_HTML_Tag::create( 'i', '', array( 'class' => 'icon-plus icon-white' ) );
+	$buttonAdd		= UI_HTML_Tag::create( 'a', $icon.' '.$words['list']['buttonAdd'], array( 'href' => './info/manual/add', 'class' => "btn btn-small btn-primary" ) );
+	$icon			= UI_HTML_Tag::create( 'i', '', array( 'class' => 'icon-refresh' ) );
+	$buttonReload	= UI_HTML_Tag::create( 'a', $icon.' '.$words['list']['buttonReload'], array( 'href' => './info/manual/reload', 'class' => "btn btn-small" ) );
+}
+
 return '
 <div class="row-fluid">
 	<div class="span3">
-		<h3>Dokumente</h3>
+		<h3>'.$words['list']['heading'].'</h3>
 		'.$list.'
-		<a href="./info/manual/add" class="btn btn-small btn-primary"><i class="icon-plus icon-white"></i> neues Dokument</a>
+		'.$buttonAdd.'
+		'.$buttonReload.'
 	</div>
 	<div class="span9">
-		<h3><span class="muted">Editor: </span> '.htmlentities( $file, ENT_QUOTES, 'UTF-8' ).'</h3>
+		<h3><span class="muted">'.$words['edit']['heading'].' </span> '.htmlentities( $file, ENT_QUOTES, 'UTF-8' ).'</h3>
 		<form action="./info/manual/edit/'.base64_encode( $file ).'" method="post">
 			<div class="row-fluid">
 				<div class="span12">
-					<label for="input_content">Inhalt</label>
+					<label for="input_content">'.$words['edit']['labelContent'].'</label>
 					<textarea class="span12 CodeMirror" name="content" id="input_content" rows="'.$moduleConfig->get( 'editor.rows' ).'">'.$content.'</textarea>
 				</div>
 			</div>
 			<div class="row-fluid">
 				<div class="span4">
-					<label for="input_filename">Dateiname</label>
+					<label for="input_filename">'.$words['edit']['labelFilename'].'</label>
 					<input type="text" name="filename" class="span12" value="'.htmlentities( $file, ENT_QUOTES, 'UTF-8' ).'"/>
 				</div>
 				<div class="span4">
 					<label>&nbsp;</label>
 					<label class="checkbox">
 						<input type="checkbox" name="backup"/>
-						vorherige Version sichern
+						'.$words['edit']['labelBackup'].'
 					</label>
 				</div>
 			</div>
 			<div class="buttonbar">
-				<a href="./info/manual/view/'.$view->urlencode( $file ).'" class="btn btn-small"><i class="icon-arrow-left"></i> view</a>
-				<button type="submit" name="save" class="btn btn-small btn-success"><i class="icon-ok icon-white"></i> save</button>
-				<a href="./info/manual/remove/'.base64_encode( $file ).'" class="btn btn-small btn-danger" onclick="return confirm(\'Wirklich ?\');"><i class="icon-remove icon-white"></i> entfernen</a>
+				<a href="./info/manual/view/'.$view->urlencode( $file ).'" class="btn btn-small"><i class="icon-arrow-left"></i> '.$words['edit']['buttonView'].'</a>
+				<button type="submit" name="save" class="btn btn-small btn-success"><i class="icon-ok icon-white"></i> '.$words['edit']['buttonSave'].'</button>
+				<a href="./info/manual/remove/'.base64_encode( $file ).'" class="btn btn-small btn-danger" onclick="return confirm(\''. addslashes( $words['edit']['buttonRemoveConfirm'] ).'\');"><i class="icon-remove icon-white"></i> '.$words['edit']['buttonRemove'].'</a>
 				&nbsp;&nbsp;|&nbsp;&nbsp;
-				<a href="./info/manual/moveUp/'.base64_encode( $file ).'" class="btn btn-small"><i class="icon-arrow-up"></i> hoch</a>
-				<a href="./info/manual/moveDown/'.base64_encode( $file ).'" class="btn btn-small"><i class="icon-arrow-down"></i> runter</a>
+				<a href="./info/manual/moveUp/'.base64_encode( $file ).'" class="btn btn-small"><i class="icon-arrow-up"></i> '.$words['edit']['buttonMoveUp'].'</a>
+				<a href="./info/manual/moveDown/'.base64_encode( $file ).'" class="btn btn-small"><i class="icon-arrow-down"></i> '.$words['edit']['buttonMoveDown'].'</a>
 			</div>
 		</form>
 	</div>
