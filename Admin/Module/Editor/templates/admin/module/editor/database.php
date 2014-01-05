92a312ac8036471106243ef00c40f8c14b89f52b
Christian Würker <kriss@ceusmedia.de>
2014-01-05 22:44:55 +0000

    Modules/Hydrogen: Updated module Admin:Module:Editor for change in SQL handling.

--- Admin/Module/Editor/templates/admin/module/editor/database.php
+++ Admin/Module/Editor/templates/admin/module/editor/database.php
@@ -4,19 +4,31 @@ $w			= (object) $words['tab-database'];
 $wf			= (object) $words['tab-database-add'];
 
 $count	= 0;
-$sql	= '<br/><div>'.$w->listNone.'</div><br/>';
+$list	= '<br/><div>'.$w->listNone.'</div><br/>';
 if( $module->sql ){
-	$sql	= array();
-	foreach( $module->sql as $type => $content ){
+	$list	= array();
+	foreach( $module->sql as $type => $sql ){
+		if( !strlen( trim( $sql->sql ) ) )
+			continue;
 		$count++;
-		$code	= UI_HTML_Tag::create( 'xmp', trim( $content ) );
-		$sql[]	= UI_HTML_Tag::create( 'dt', $type ).UI_HTML_Tag::create( 'dd', $code, array( 'class' => 'sql' ) );
+		$code		= UI_HTML_Tag::create( 'xmp', trim( $sql->sql ) );
+		$url		= './admin/module/editor/removeSql/'.$moduleId;
+
+		$versions	= $sql->event === 'update' ? '<br/>v'.$sql->from.' &rArr; v'.$sql->to : '';
+		$remove		= UI_HTML_Elements::LinkButton( $url, 'entfernen', 'button icon remove', 'Wirklich?' );
+		$label		= ucFirst( $sql->event ).$versions.'<br/>DBMS: '.$sql->type.'<br/>'.$remove;
+		$list[]		= UI_HTML_Tag::create( 'dt', $label );
+		$list[]		= UI_HTML_Tag::create( 'dd', $code, array( 'class' => 'sql' ) );
 	}
-	$sql	= UI_HTML_Tag::create( 'dl', join( $sql ) );
+	$list	= UI_HTML_Tag::create( 'dl', join( $list ), array( 'class' => 'database' ) );
 }
 
+$types	= array( '*' => $words['tab-database']['allTypes'] ) + $words['database-types'];
+
 $optEvent	= UI_HTML_Elements::Options( $words['database-events'] );
-$optType	= UI_HTML_Elements::Options( $words['database-types'] );
+$optType	= UI_HTML_Elements::Options( $types );
+
+
 
 $panelAdd	= '
 	<form action="./admin/module/editor/addSql/'.$moduleId.'?tab=database" method="post">
@@ -25,13 +37,21 @@ $panelAdd	= '
 			<ul class="input">
 				<li class="column-left-60">
 					<label for="input_event">'.$wf->labelEvent.'</label><br/>
-					<select name="event" id="input_event" class="max">'.$optEvent.'</select>
+					<select name="event" id="input_event" class="max" onchange="showOptionals(this);">'.$optEvent.'</select>
 				</li>
 				<li class="column-left-40">
 					<label for="input_type">'.$wf->labelType.'</label><br/>
 					<select name="type" id="input_type" class="max">'.$optType.'</select>
 				</li>
-				<li>
+				<li class="column-clear column-left-30 optional event-update">
+					<label for="input_version_from">'.$wf->labelVersionFrom.'</label><br/>
+					<input type="text" name="version_from" id="input_version_from" class="s numeric"/>
+				</li>
+				<li class="column-left-30 optional event-update">
+					<label for="input_version_to">'.$wf->labelVersionTo.'</label><br/>
+					<input type="text" name="version_to" id="input_version_to" class="s numeric"/>
+				</li>
+				<li class="column-clear">
 					<label for="input_ddl">'.$wf->labelDDL.'</label><br/>
 					<textarea name="ddl" id="input_ddl" class="max" rows="10"></textarea>
 				</li>
@@ -41,6 +61,11 @@ $panelAdd	= '
 			</div>
 		</fieldset>
 	</form>
+	<script>
+$(document).ready(function(){
+	showOptionals($("#input_event").get(0));	
+});
+	</script>
 ';
 
 return '
@@ -52,7 +77,7 @@ dd.sql {
 
 </style>
 <div class="column-left-70">
-	'.$sql.'
+	'.$list.'
 </div>
 <div class="column-left-30">
 	'.$panelAdd.'
