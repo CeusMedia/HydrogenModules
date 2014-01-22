891d95002df775b6067c5d2429537c0e3dbff58a
Christian Würker <kriss@ceusmedia.de>
2014-01-22 10:26:52 +0000

    Modules/Hydrogen: Updated module UI:DevLayers.

--- UI/DevLayers/js/UI.DevLayers.js
+++ UI/DevLayers/js/UI.DevLayers.js
@@ -2,8 +2,8 @@ if(typeof UI === "undefined")
 	UI = {};
 UI.DevLayers = {
 	init: function(){ 
+		$("#dev-layers").on("click", function(){$(this).fadeOut(function(){$(this).hide()});});
 		$("#dev-layers .dev-layer").each(function(){
-			$(this).bind("click",function(){$(this).parent().fadeOut(function(){$(this).hide()});});
 			var id = $(this).attr("id");
 			$("#dev-layer-"+id+"-trigger").bind("click", {id: id}, function(event){
 				UI.DevLayers.show(event.data.id);
