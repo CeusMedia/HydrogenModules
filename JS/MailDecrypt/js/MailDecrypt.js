94e3c4201e511d9f5e924e07d7cb52c36b8fa394
Christian Würker <kriss@ceusmedia.de>
2014-02-13 03:46:22 +0000

    Modules/Hydrogen: Updated module JS:MailDecrypt.

--- JS/MailDecrypt/js/MailDecrypt.js
+++ JS/MailDecrypt/js/MailDecrypt.js
@@ -6,31 +6,5 @@ var MailDecrypt = function(){
 		link.html($(this).html().length ? $(this).html() : addr);				//  set link content
 		$(this).replaceWith(link);												//  replace span by link
 	});
-//	MailDecryptDeprecated("body");
-}
-
-var MailDecryptDeprecated = function(selector,pattern){
-	if(typeof pattern == "undefined")											//  no special regex pattern defined
-		pattern = /\[mail:([\S]+)#([^@]+)(@.+)?\]/g;							//  use default regex pattern
-	var attr, i, matches, string, user, host, repl;								//  declare variables
-	var element = $(selector);
-	if(!$(selector).size())
-		return;
-	string = $(selector).html();												//  get content of selected node
-	if(string.match(pattern)){													//  content matches regex pattern
-		matches = pattern.exec(string);											//  get all matches
-		if(matches.length > 2){													//  at least user and host found
-			if(typeof matches[3] != "undefined"){								//  also found attribute map
-				attr = matches[3].substr(1).split("@");							//  
-				for(i=0; i<attr.length; i++)									//  
-					attr[i] = ' '+attr[i].replace(/:/,'="')+'"';				//  
-				attr = attr.join("");											//  
-			}
-			user = matches[1].replace(/<.+?>/g,'').replace(/&.+?;/g,'');		//  get clean user
-			host = matches[2].replace(/<.+?>/g,'').replace(/&.+?;/g,'');		//  get clean host
-			repl = '<a href="mailto:'+user+'@'+host+'"'+attr+'>$1@$2</a>';		//  
-			string = string.replace(pattern, repl);								//  replace node conten by mail link
-			$(selector).html(string);											//  realize new linked content in node
-		}
-	}
 };
+
