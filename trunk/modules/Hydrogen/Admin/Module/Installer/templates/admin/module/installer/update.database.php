c3871c382810c5c1bcbd920e381d120213a9bbdf
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2014-02-24 00:24:25 +0000

    Modules/Hydrogen: Updated module Admin:Module:Installer.

--- trunk/modules/Hydrogen/Admin/Module/Installer/templates/admin/module/installer/update.database.php
+++ trunk/modules/Hydrogen/Admin/Module/Installer/templates/admin/module/installer/update.database.php
@@ -1,22 +1,27 @@
 <?php
 $fieldset	= '';
 
+function fixVersionBug( $version ){
+	return preg_replace( "/-pl?([0-9])/", ".0.\\1", $version );
+}
+
 $remote		= $env->getRemote();
 if( $remote->getModules() && $remote->getModules()->has( 'Resource_Database' ) ){
 	$driver			= $remote->getConfig()->get( 'module.resource_database.access.driver' );
 	$versionFrom	= $moduleLocal->versionInstalled;
 	$versionTo		= $moduleSource->versionAvailable;
+
 	$sqlKey			= 'update:'.$versionFrom.'->'.$versionTo.'@';
 
 	$list	= array();
 	foreach( $moduleSource->sql as $key => $sql ){
 		if( $sql->event !== "update" )
 			continue;
-		if( version_compare( $sql->from, $versionFrom ) < 0 )
+		if( version_compare( fixVersionBug( $sql->from ), fixVersionBug( $versionFrom ) ) < 0 )
 			continue;
-		if( version_compare( $versionTo, $sql->to ) < 0 )
+		if( version_compare( fixVersionBug( $versionTo ), fixVersionBug( $sql->to ) ) < 0 )
 			continue;
-		
+
 		$versions	= $sql->event === 'update' ? '<br/>v'.$sql->from.' &rArr; v'.$sql->to : '';
 		$label		= ucFirst( $sql->event ).$versions.'<br/>DBMS: '.$sql->type;
 		$list[]		= UI_HTML_Tag::create( 'dt', $label );
