7503b415ae7c79ddda29cc8f20ef1604ec536ec3
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2014-02-27 19:43:05 +0000

    Modules/Hydrogen: Updated module Manage:Shop.

--- trunk/modules/Hydrogen/Manage/Shop/templates/manage/shop/order/filter.php
+++ trunk/modules/Hydrogen/Manage/Shop/templates/manage/shop/order/filter.php
@@ -1,4 +1,6 @@
 <?php
+$w	= (object) $words['filter'];
+
 $filterCustomer	= !empty( $filters['customer'] ) ? $filters['customer'] : "";
 
 $optStatus	= array(/* '' => '- alle -' */);
@@ -6,37 +8,33 @@ foreach( $words['states'] as $key => $value )
 	$optStatus[$key]	= $value;
 $optStatus	= UI_HTML_Elements::Options( $optStatus, $filters['status'] );
 
-$optOrder	= array(
-	'created:ASC'		=> 'Erstellung aufsteigend',
-	'created:DESC'		=> 'Erstellung absteigend',
-	'edited:ASC'		=> 'Änderung aufsteigend',
-	'edited:DESC'		=> 'Änderung absteigend',
-);
+$optOrder	= $words['filter-orders'];
 $optOrder	= UI_HTML_Elements::Options( $optOrder, $filters['order'] );
 
 return '
+<h4>'.$w->heading.'</h4>
 <form action="./manage/shop/order/filter" method="post">
 	<div class="row-fluid">
 		<div class="span12">
-			<label for="input_customer">Kunde</label>
+			<label for="input_customer">'.$w->labelCustomer.'</label>
 			<input class="span12" type="text" name="customer" id="input_customer" value="'.$filterCustomer.'"/>
 		</div>
 	</div>
 	<div class="row-fluid">
 		<div class="span12">
-			<label for="input_status">Status</label>
+			<label for="input_status">'.$w->labelStatus.'</label>
 			<select class="span12" name="status[]" id="input_status" multiple="multiple" size="13">'.$optStatus.'</select>
 		</div>
 	</div>
 	<div class="row-fluid">
 		<div class="span12">
-			<label for="input_order">Sortierung nach Spalte</label>
+			<label for="input_order">'.$w->labelOrder.'</label>
 			<select class="span12" name="order" id="input_order">'.$optOrder.'</select>
 		</div>
 	</div>
 	<div class="buttonbar">
 		<button type="submit" class="btn btn-small btn-info"><i class="icon-search icon-white"></i> filtern</button>
-		<a href="./manage/shop/order/filter/reset" class="btn btn-small btn-inverse"><i class="icon-remove-circle icon-white"></i> zurücksetzen</a>
+		<a href="./manage/shop/order/filter/reset" class="btn btn-small btn-inverse"><i class="icon-remove-circle icon-white"></i> '.$w->buttonReset.'</a>
 	</div>
 </form>
 ';
