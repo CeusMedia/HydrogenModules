e4666774f7b06aa01fdc1b9d2cea63ca20627bc6
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2014-03-16 12:58:50 +0000

    Modules/Hydrogen: Updated module Resource:Authentication.

--- trunk/modules/Hydrogen/Resource/Authentication/js/Auth.js
+++ trunk/modules/Hydrogen/Resource/Authentication/js/Auth.js
@@ -1,43 +1,52 @@
 var Auth = {
 
-	oneMinute: 60 * 1000,
+	oneMinute: 60,
+	autoLogout: {
+		seconds: 0,
+		interval: null,
+		left: 0,
+		url: './auth/logout'
+	},
+	config: [],
+
+	init: function(){
+		Auth.config	= settings.Resource_Authentication;
+		Auth.initAutoLogout();
+		Auth.initSessionRefresh();
+	},
 
 	initAutoLogout: function(){
-		var config	= settings.Resource_Authentication;
-		var minutes = parseInt(config.logout_auto_minutes);
-		if(!config.logout_auto || !minutes)
+		var minutes = parseInt(Auth.config.logout_auto_minutes);
+		if(!Auth.config.logout_auto || !minutes)
 			return;
-		duration = minutes * this.oneMinute;
+		Auth.autoLogout.seconds = minutes * 60;
 		$.ajax({
 			url: './auth/ajaxIsAuthenticated',
 			dataType: 'json',
 			success: function(status){
 				if(!status)
 					return;
-//				counter = minutes * 60;
-//				window.setInterval(function(){counter--;console.log(counter)},1000);
-				window.setInterval(
-					function(){
-						var url = './auth/logout';
-						if(config.logout_auto_forward_controller){
-							url += '/'+config.logout_auto_forward_controller;
-							if(config.logout_auto_forward_action)
-								url += '/'+config.logout_auto_forward_action;
-						}
-						url += '?autoLogout';
-						document.location.href = url;
-					}, duration
-				);
+				if(Auth.config.logout_auto_forward_controller){
+					Auth.autoLogout.url += '/'+Auth.config.logout_auto_forward_controller;
+					if(Auth.config.logout_auto_forward_action)
+						Auth.autoLogout.url += '/'+Auth.config.logout_auto_forward_action;
+				}
+				Auth.autoLogout.url += '?autoLogout';
+				Auth.refreshAutoLogoutInterval();
+				$("body").on('click', function(){
+					Auth.refreshAutoLogoutInterval();
+				});
+				Auth.autoLogout.left = minutes * 60;
+				Auth.updateAutoLogoutTimer();
+				window.setInterval(Auth.updateAutoLogoutTimer, 1000);
 			}
 		});
 	},
 
 	initSessionRefresh:function(){
-		var config	= settings.Resource_Authentication;
-		var minutes = parseInt(config.session_refresh_minutes);
-		if(!config.session_refresh || !minutes)
+		var minutes = parseInt(Auth.config.session_refresh_minutes);
+		if(!Auth.config.session_refresh || !minutes)
 			return;
-		duration = minutes * this.oneMinute;
 		window.setInterval(
 			function(){
 				$.ajax({
@@ -45,9 +54,29 @@ var Auth = {
 					dataType: 'html',
 					type: 'POST'
 				});
-			}, duration
+			}, minutes * 60 * 1000
 		);
+	},
+
+	refreshAutoLogoutInterval: function(){
+		if(Auth.autoLogout.interval)
+			window.clearInterval(Auth.autoLogout.interval);
+		Auth.autoLogout.left = Auth.autoLogout.seconds;
+		Auth.autoLogout.interval = window.setInterval(function(){
+			document.location.href = Auth.autoLogout.url;
+		}, Auth.autoLogout.seconds * 1000);
+	},
+
+	updateAutoLogoutTimer: function(){
+		if(Auth.autoLogout.left > 0){
+			var minutes = Math.floor(Auth.autoLogout.left / 60);
+			var seconds = Auth.autoLogout.left - minutes * 60;
+			if((""+seconds).length == 1)
+				seconds = "0"+seconds;
+			$("#auth-auto-logout-timer").html(minutes+":"+seconds);
+			Auth.autoLogout.left--;
+		}
 	}
 };
-Auth.initSessionRefresh();
-Auth.initAutoLogout();
+
+Auth.init();
