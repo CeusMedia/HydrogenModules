732d2b11edf1fdef544e3cf1e758d2d647517e34
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2014-04-17 17:11:39 +0000

    Modules/Hydrogen: Updated module Admin:Modules.

--- trunk/modules/Hydrogen/Admin/Modules/js/site.admin.module.js
+++ trunk/modules/Hydrogen/Admin/Modules/js/site.admin.module.js
@@ -1,27 +1,37 @@
+var ModuleAdminModule = {
+	init: function(selector, options){
+	//	console.log( "calling InstantFilter" );
+		var options	= $.extend({
+			caseSense: false,
+			skipKeys: [],
+			durationFadeIn: 600,
+			durationFadeOut: 200,
+			selectorGroups: 'fieldset',
+			selectorItems: 'div.module',
+			selectorReset: '#search #search-reset',
+			autoFocus: true,
+			onSearch: function(instance){
+				var o = instance.options;
+				$(o.selectorItems).parent(o.selectorGroups).each(function(){
+					var got = $(this).find(o.selectorItems).filter(".found").stop(true,true);
+					var dur = got.size() ? o.durationFadeIn : o.durationFadeOut;
+					got.size() ? $(this).fadeIn(dur) : $(this).fadeOut(dur);
+				});
+			},
+			onReset: function(instance){
+				var o = instance.options;
+				var fieldSets = $(o.selectorItems).parent(o.selectorGroups);
+				fieldSets.not(":visible").fadeIn(o.durationFadeIn);
+			}
+		}, options);
+		new InstantFilter(selector,options);	
+	}
+};
 
 $(document).ready(function(){
-	new InstantFilter('div#search input#input_query',{
-		caseSense: false,
-		skipKeys: [32],
-		durationFadeIn: 600,
-		durationFadeOut: 200,
-		selectorItems: 'div.module',
-		selectorReset: '#search #search-reset',
-		autoFocus: true,
-		onSearch: function(instance){
-			var o = instance.options;
-			$(o.selectorItems).parent().each(function(){
-				var got = $(this).find(o.selectorItems).not(".hidden").stop(true,true);
-				var dur = got.size() ? o.durationFadeIn : o.durationFadeOut;
-				got.size() ? $(this).fadeIn(dur) : $(this).fadeOut(dur);
-			});
-		},
-		onReset: function(instance){
-			var o = instance.options;
-			var fieldSets = $(o.selectorItems).parent();
-			fieldSets.not(":visible").fadeIn(o.durationFadeIn);
-		}
-	});
+	var selectorModuleFilter = 'div#search input#input_query';
+	if($(selectorModuleFilter).size())
+		ModuleAdminModule.init(selectorModuleFilter);
 
 	$("button.auto-back").each(function(){
 		$(this).removeAttr("disabled").removeAttr("readonly");
@@ -36,8 +46,8 @@ $(document).ready(function(){
 		$(this).hide();
 	});
 
-	$("button.legend-form-trigger").bind("click",function(){
-		$(this).hide();
+	$("button.legend-form-trigger").bind("click",function(){				//  @todo	kriss: is this used or deprecated ?
+		$(this).hide();														//  @todo	kriss: can be combined with next line
 		$(this).parent().parent().children().show();
 	});
 });
