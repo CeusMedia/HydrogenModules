a08c36efbe6562f4f1976481d8bdd05262cee1fb
Christian Würker <kriss@ceusmedia.de>
2014-05-11 18:50:17 +0000

    Modules/Hydrogen: Updated module UI:JS:Messenger.

--- UI/JS/Messenger/js/UI.Messenger.js
+++ UI/JS/Messenger/js/UI.Messenger.js
@@ -5,69 +5,70 @@ UI.Messenger	= {
 	timeSlideUp: "slow",
 	timeRemove: 5000,
 	__init: function(){
-		if(!this.status){
-			var value;
-			if(typeof config !== "undefined"){
-				this.timeSlideDown	= settings.UI_JS_Messenger.slideDown;
-				this.timeSlideUp	= settings.UI_JS_Messenger.slideUp;
-				this.timeRemove		= settings.UI_JS_Messenger.autoRemove;
-
-				if(this.timeRemove){
-					$(document).ready(function(){
-						$("#layout-messenger>ul>li").each(function(){
-							UI.Messenger.autoRemoveMessage(this);
-						});
-					});	
+		if(!UI.Messenger.status){
+			if(typeof settings !== "undefined"){
+				if(typeof settings.UI_JS_Messenger !== "undefined"){
+					UI.Messenger.timeSlideDown	= settings.UI_JS_Messenger.slideDown;
+					UI.Messenger.timeSlideUp	= settings.UI_JS_Messenger.slideUp;
+					UI.Messenger.timeRemove		= settings.UI_JS_Messenger.autoRemove;
 				}
 			}
-			this.status = 2;
+
+			if(UI.Messenger.timeRemove){
+				$(document).ready(function(){
+					$("#layout-messenger>ul>li").each(function(){
+						UI.Messenger.autoRemoveMessage(this);
+					});
+				});	
+			}
+			UI.Messenger.status = 2;
 		}
 	},
 	autoRemoveMessage: function(item){
-		var messageId	= 'message-'+Math.round(Math.random()*1000000);
-		var callback	= 'UI.Messenger.hideMessage("'+messageId+'")';
-		if(!this.timeRemove)
+		var messageId = 'message-'+Math.round(Math.random()*1000000);
+		var callback = 'UI.Messenger.hideMessage("'+messageId+'")';
+		if(!UI.Messenger.timeRemove)
 			return;
 		$(item).attr("id",messageId);
-		window.setTimeout(callback, this.timeRemove);
+		window.setTimeout(callback, UI.Messenger.timeRemove);
 	},
 	discardMessage: function(item){
 		var id;
 		if(!(id = item.attr("id")))
 			item.attr("id", (id = "tmp-msg-remove"));
-		this.hideMessage(id);
+		UI.Messenger.hideMessage(id);
 	},
-	hideMessage:	function(messageId){
-		this.__init();
+	hideMessage: function(messageId){
+		UI.Messenger.__init();
 		$("#"+messageId+" div.button").hide();
-		$("#"+messageId).slideUp(this.timeSlideUp,function(){
+		$("#"+messageId).slideUp(UI.Messenger.timeSlideUp, function(){
 			$(this).remove();
 			if(!$("#layout-messenger>ul>li").size())
 				$("#layout-messenger>ul").remove();
 		})
 	},
-	noteSuccess:	function(message){
+	noteSuccess: function(message){
 		return UI.Messenger.renderMessage(message,'success');
 	},
-	noteNotice:		function(message){
+	noteNotice: function(message){
 		return UI.Messenger.renderMessage(message,'notice');
 	},
-	noteError:		function(message){
+	noteError: function(message){
 		return UI.Messenger.renderMessage(message,'error');
 	},
-	noteFailure:	function(message){
+	noteFailure: function(message){
 		return UI.Messenger.renderMessage(message,'failure');
 	},
-	renderMessage:	function(message,typeClass){
-		this.__init();
-		container	= $("#layout-messenger");
+	renderMessage: function(message,typeClass){
+		UI.Messenger.__init();
+		container = $("#layout-messenger");
 		if(!$("ul",container).size())
 			container.prepend($("<ul></ul>"));
-		list		= $("#layout-messenger>ul");
-		item		= $("<li></li>").addClass(typeClass).html(message);
+		list = $("#layout-messenger>ul");
+		item = $("<li></li>").addClass(typeClass).html(message);
 		list.append(item.hide());
-		item.slideDown(this.timeSlideDown);
-		this.autoRemoveMessage(item);
+		item.slideDown(UI.Messenger.timeSlideDown);
+		UI.Messenger.autoRemoveMessage(item);
 	}
 };
 UI.Messenger.__init();
