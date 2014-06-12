03fbf474dffb524ec596635f51acbdfaed033406
Christian Würker <kriss@ceusmedia.de>
2014-06-12 10:22:12 +0000

    Modules/Hydrogen: Updated module Index.

--- Index/templates/index/index.php
+++ Index/templates/index/index.php
@@ -1,6 +1,15 @@
 <?php
+if( $user ){
+	if( $view->hasContent( 'index', 'index.inside', 'html/' ) ){
+		$content	= $view->loadContent( 'index', 'index.inside',  array( 'user' => $user ) );
+		if( $env->getModules()->has( 'UI_Helper_Content' ) )
+			$content	= View_Helper_ContentConverter::render( $env, $content );
+		return $content;
+	}
+}
+
 if( $view->hasContent( 'index', 'index', 'html/' ) ){
-	$content	= $view->loadContent( 'index', 'index', 'html/' );
+	$content	= $view->loadContent( 'index', 'index' );
 	if( $env->getModules()->has( 'UI_Helper_Content' ) )
 		$content	= View_Helper_ContentConverter::render( $env, $content );
 	return $content;
