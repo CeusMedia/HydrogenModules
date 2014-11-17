798abf84751151c0176729e3b6c04be0b0ffe6ea
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2014-11-17 09:57:44 +0000

    Modules/Hydrogen: Updated module Manage:Customers.

--- trunk/modules/Hydrogen/Manage/Customer/templates/manage/customer/edit.php
+++ trunk/modules/Hydrogen/Manage/Customer/templates/manage/customer/edit.php
@@ -34,13 +34,13 @@ return '
 					<div class="row-fluid">
 						<div class="span12">
 							<label for="input_title">'.$w->labelTitle.'</label>
-							<input type="text" id="input_title" name="title" class="span12" value="'.htmlentities( $customer->title ).'"/>
+							<input type="text" id="input_title" name="title" class="span12" value="'.htmlentities( $customer->title, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
 						<div class="span12">
 							<label for="input_url">'.$w->labelUrl.'</label>
-							<input type="text" id="input_url" name="url" class="span12" value="'.htmlentities( $customer->url ).'"/>
+							<input type="text" id="input_url" name="url" class="span12" value="'.htmlentities( $customer->url, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
@@ -56,7 +56,7 @@ return '
 					<div class="row-fluid">
 						<div class="span12">
 							<label for="input_description">'.$w->labelDescription.'</label>
-							<textarea id="input_description" name="description" class="span12 CodeMirror-auto" rows="6">'.htmlentities( $customer->description ).'</textarea>
+							<textarea id="input_description" name="description" class="span12 CodeMirror-auto" rows="6">'.htmlentities( $customer->description, ENT_QUOTES, 'UTF-8' ).'</textarea>
 						</div>
 					</div>
 				</div>
@@ -64,43 +64,43 @@ return '
 					<div class="row-fluid">
 						<div class="span12">
 							<label for="input_contact">'.$w->labelContact.'</label>
-							<input type="text" id="input_contact" name="contact" class="span12" value="'.htmlentities( $customer->contact ).'"/>
+							<input type="text" id="input_contact" name="contact" class="span12" value="'.htmlentities( $customer->contact, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
 						<div class="span12">
 							<label for="input_email">'.$w->labelEmail.'</label>
-							<input type="text" id="input_email" name="email" class="span12" value="'.htmlentities( $customer->email ).'"/>
+							<input type="text" id="input_email" name="email" class="span12" value="'.htmlentities( $customer->email, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
 						<div class="span6">
 							<label for="input_phone">'.$w->labelPhone.'</label>
-							<input type="text" id="input_phone" name="phone" class="span12" value="'.htmlentities( $customer->phone ).'"/>
+							<input type="text" id="input_phone" name="phone" class="span12" value="'.htmlentities( $customer->phone, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 						<div class="span6">
 							<label for="input_fax">'.$w->labelFax.'</label>
-							<input type="text" id="input_fax" name="fax" class="span12" value="'.htmlentities( $customer->fax ).'"/>
+							<input type="text" id="input_fax" name="fax" class="span12" value="'.htmlentities( $customer->fax, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
 						<div class="span10">
 							<label for="input_street">'.$w->labelStreet.'</label>
-							<input type="text" id="input_street" name="street" class="span12" value="'.htmlentities( $customer->street ).'"/>
+							<input type="text" id="input_street" name="street" class="span12" value="'.htmlentities( $customer->street, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 						<div class="span2">
 							<label for="input_nr">'.$w->labelNr.'</label>
-							<input type="text" id="input_nr" name="nr" class="span12" value="'.htmlentities( $customer->nr ).'"/>
+							<input type="text" id="input_nr" name="nr" class="span12" value="'.htmlentities( $customer->nr, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
 						<div class="span9">
 							<label for="input_city">'.$w->labelCity.'</label>
-							<input type="text" id="input_city" name="city" class="span12" value="'.htmlentities( $customer->city ).'"/>
+							<input type="text" id="input_city" name="city" class="span12" value="'.htmlentities( $customer->city, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 						<div class="span3">
 							<label for="input_postcode">'.$w->labelPostcode.'</label>
-							<input type="text" id="input_postcode" name="postcode" class="span12" value="'.htmlentities( $customer->postcode ).'"/>
+							<input type="text" id="input_postcode" name="postcode" class="span12" value="'.htmlentities( $customer->postcode, ENT_QUOTES, 'UTF-8' ).'"/>
 						</div>
 					</div>
 					<div class="row-fluid">
