5c129d32b9eb0413a73f9d97223f34085d6bc29a
Christian Würker <kriss@ceusmedia.de>
2014-11-25 02:11:22 +0000

    Modules/Hydrogen: Updated module Admin:Module:Installer.

--- Admin/Module/Installer/templates/admin/module/installer/update.database.php
+++ Admin/Module/Installer/templates/admin/module/installer/update.database.php
@@ -18,23 +18,52 @@ if( $remote->getModules() && $remote->getModules()->has( 'Resource_Database' ) )
 	$versionTarget	= fixVersionBug( $versionTo );
 	foreach( $moduleSource->sql as $key => $sql ){
 		$versionStep	= fixVersionBug( $sql->version );
-		if( $sql->event === "update" ){																				//  sql part is an update
-			if( version_compare( $versionStep, $versionCurrent, '>' ) ){											//  sql part is newer than current version
-				if( version_compare( $versionStep, $versionTarget, '<=' ) ){										//  sql part is older or related to new version
-					$facts		= array( 'Version: '.$sql->version, 'DBMS: '.$sql->type );							//  collect facts
-					$list[]		= UI_HTML_Tag::create( 'dt', join( '<br/>', $facts ) );								//  create definition term
-					$list[]		= UI_HTML_Tag::create( 'dd', UI_HTML_Tag::create( 'xmp', trim( $sql->sql ) ) );		//  create definition description
+		if( $sql->event === "update" ){																//  sql part is an update
+			if( version_compare( $versionStep, $versionCurrent, '>' ) ){							//  sql part is newer than current version
+				if( version_compare( $versionStep, $versionTarget, '<=' ) ){						//  sql part is older or related to new version
+					$facts		= array( 'Version: '.$sql->version, 'DBMS: '.$sql->type );			//  collect facts
+					$facts		= UI_HTML_Tag::create( 'b', join( ' | ', $facts ) );				//  render facts
+					$mode		= $sql->type === 'mysql' ? 'text/x-mysql' : 'text/x-sql';			//  decide SQL dialect by SQL update type
+					$code		= htmlentities( trim( $sql->sql ), ENT_QUOTES, 'UTF-8' );			//  escape SQL content
+					$code		= UI_HTML_Tag::create( 'textarea', $code, array(					//  render textarea for CodeMirror
+						'class'							=> 'CodeMirror-auto',						//  apply automatic CodeMirror
+						'data-codemirror-read-only'		=> 'nocursor',								//  CodeMirror: set readonly
+						'data-codemirror-mode'			=> $mode,									//  CodeMirror: set mode to SQL dialect
+						'data-codemirror-height'		=> 'auto',									//  CodeMirror: adjust height to content
+						'data-codemirror-line-wrapping'	=> 'true',									//  CodeMirror: enable to wrap long lines
+					) );
+					$cell		= UI_HTML_Tag::create( 'td', $facts.$code );						//  render table cell
+					$list[]		= UI_HTML_Tag::create( 'tr', $cell );								//  append table row
 				}
 			}
 		}
 	}
 
 	if( $list ){
-		$list		= UI_HTML_Tag::create( 'dl', join( $list ), array( 'class' => 'database' ) );
+		$list		= UI_HTML_Tag::create( 'table', join( $list ), array( 'class' => 'database' ) );
 		$legend		= UI_HTML_Tag::create( 'legend', "Datenbank", array( 'class' => 'database' ) );
-		$text		= UI_HTML_Tag::create( 'p', $words['update']['textDatabase'] );
-		$fieldset	= UI_HTML_Tag::create( 'fieldset', $legend.$text.'<br/>'.$list );
+		$text		= UI_HTML_Tag::create( 'small', $words['update']['textDatabase'], array( 'class' => 'muted' ) );
+		$fieldset	= UI_HTML_Tag::create( 'fieldset', $legend.$text.$list );
 	}
 }
-return $fieldset;
+return $fieldset.'
+<style>
+table.database {
+	border: none !important;
+	table-layout: fixed;
+	}
+table.database td {
+	border: none !important;
+	}
+table.database .CodeMirror {
+	height: auto;
+	}
+table.database .CodeMirror-scroll {
+	overflow-y: hidden;
+	overflow-x: auto;
+	}
+</style>
+<script>
+
+</script>';
 ?>
--- Admin/Module/Installer/templates/admin/module/installer/update.php
+++ Admin/Module/Installer/templates/admin/module/installer/update.php
@@ -6,10 +6,13 @@ $attributes		= array( 'type' => 'button', 'class' => 'button cancel auto-back', 
 $buttonBack		= UI_HTML_Tag::create( 'button', '<span>'.$w->buttonBack.'</span>', $attributes );
 $buttonUpdate	= UI_HTML_Elements::Button( 'doUpdate', $w->buttonUpdate, 'button update' );
 
+$panelChanges	= $view->loadTemplateFile( 'admin/module/installer/update.changes.php' );
 $panelDatabase	= $view->loadTemplateFile( 'admin/module/installer/update.database.php' );
+$panelLocal		= $view->loadTemplateFile( 'admin/module/installer/update.local.php' );
 $panelInfo		= $view->loadTemplateFile( 'admin/module/installer/update.info.php' );
 $tableConfig	= $view->loadTemplateFile( 'admin/module/installer/update.config.php' );
 $panelFiles		= $view->loadTemplateFile( 'admin/module/installer/update.files.php' );
+$panelDev		= $view->loadTemplateFile( 'admin/module/installer/update.dev.php' );
 
 $panelType	= '
 	<h4>Installationstyp</h4>
@@ -28,9 +31,10 @@ return '
 	<span>'.$words['view']['heading'].'</span>
 	<cite>'.$moduleLocal->title.'</cite>
 </h3>
+<br/>
 <div class="column-left-70">
 	<form action="'.$urlForm.'" method="post">
-		<br/>
+		'.$panelChanges.'
 		'.$panelFiles.'
 		'.$tableConfig.'
 		'.$panelDatabase.'
@@ -46,6 +50,11 @@ return '
 </div>
 <div class="column-right-30">
 	'.$panelInfo.'
+	'.$panelLocal.'
+</div>
+<br/>
+<div class="column-clear" id="panel-dev-modules">
+	'.$panelDev.'
 </div>
 <script>
 $(document).ready(function(){
@@ -68,6 +77,14 @@ table.module-update-files tr.status-changed {background-color: #FFDFDF}
 table.module-update-files tr.status-linked {background-color: #EFEFEF; opacity: 0.75}
 table.module-update-files tr.status-foreign {background-color: #DFDFFF}
 table.module-update-files tr.status-refered {background-color: #DFDFFF; opacity: 0.75}
+
+#panel-dev-modules {
+	opacity: 0.5;
+	}
+#panel-dev-modules:hover {
+	opacity: 1;
+	}
+
 </style>
 <div class="column-clear"></div>';
 ?>
