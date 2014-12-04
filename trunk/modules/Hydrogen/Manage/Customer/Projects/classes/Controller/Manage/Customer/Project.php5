148efa3bb71bb0fe80b645abcdcec169db924c9c
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2014-12-04 12:27:26 +0000

    Modules/Hydrogen: Updated module Manage:Customer:Projects.

--- trunk/modules/Hydrogen/Manage/Customer/Projects/classes/Controller/Manage/Customer/Project.php5
+++ trunk/modules/Hydrogen/Manage/Customer/Projects/classes/Controller/Manage/Customer/Project.php5
@@ -13,9 +13,41 @@ class Controller_Manage_Customer_Project extends CMF_Hydrogen_Controller{
 		$this->logic	= Logic_CustomerProject::getInstance( $this->env );
 	}
 
+	public function add( $customerId ){
+		$request	= $this->env->getRequest();
+		$projectId	= $request->get( 'projectId' );
+		$type		= $request->get( 'type' );
+		$customer	= $this->modelCustomer->get( $customerId );
+		if( !$customer ){
+			$this->messenger->noteError( 'Invalid customer ID.' );
+			$this->restart( NULL, TRUE );
+		}
+		$this->logic->add( $customerId, $projectId, $type );
+		$this->messenger->noteSuccess( 'Relation added.' );
+		$this->restart( $customerId, TRUE );
+	}
+
 	public function index( $customerId ){
-		$this->addData( 'projects', $this->logic->getProjects( $customerId ) );
+
+		$relations	= $this->logic->getProjects( $customerId );
+
+		$logic		= new Logic_Project( $this->env );
+		$list		= array();
+		$projects	= $logic->getProjects( array( 'status' => array( 1, 2, 3, 4, 5 ) ) );
+		foreach( $projects as $project ){
+			if( !array_key_exists( $project->projectId, $relations ) )
+				$list[]	= $project;
+		}
+		$this->addData( 'relations', $relations );
 		$this->addData( 'customerId', $customerId );
 		$this->addData( 'customer', $this->modelCustomer->get( $customerId ) );
+		$this->addData( 'projects', $list );
+	}
+
+	public function remove( $customerId, $projectId ){
+		if( $this->logic->remove( $customerId, $projectId ) )
+			$this->messenger->noteSuccess( 'Relation removed.' );
+		$this->restart( $customerId, TRUE );
 	}
 }
+?>
--- trunk/modules/Hydrogen/Manage/Customer/Projects/templates/manage/customer/project/index.php
+++ trunk/modules/Hydrogen/Manage/Customer/Projects/templates/manage/customer/project/index.php
@@ -6,16 +6,26 @@
 $tabs		= View_Manage_Customer::renderTabs( $env, $customerId, 'project/'.$customerId );
 
 $table		= '';
-if( $projects ){
+if( $relations ){
 	$rows	= array();
-	foreach( $projects as $project ){
-		$url	= './manage/project/'.$project->projectId;
-		$link	= UI_HTML_Tag::create( 'a', $project->title, array( 'href' => $url ) );
+	foreach( $relations as $relation ){
+//print_m( $relation );
+//die;
+		$url	= './manage/project/'.$relation->projectId;
+		$link	= UI_HTML_Tag::create( 'a', $relation->project->title, array( 'href' => $url ) );
+		$urlRemove		= './manage/customer/project/remove/'.$relation->customerId.'/'.$relation->projectId;
+		$iconRemove		= '<i class="icon-remove icon-white"></i>';
+		$buttonRemove	= UI_HTML_Tag::create( 'a', $iconRemove, array(
+			'class'		=> 'btn btn-small btn-danger',
+			'href'		=> $urlRemove,
+			'title'		=> 'entfernen',
+		) );
 		$rows[]	= UI_HTML_Tag::create( 'tr', array(
-			UI_HTML_Tag::create( 'td', $link )
+			UI_HTML_Tag::create( 'td', $link ),
+			UI_HTML_Tag::create( 'td', $buttonRemove )
 		) );
 	}
-	$colgroup	= UI_HTML_Elements::ColumnGroup( "100%" ); 
+	$colgroup	= UI_HTML_Elements::ColumnGroup( "100%" );
 	$thead	= UI_HTML_Tag::create( 'thead', UI_HTML_Elements::TableHeads( array(
 		'Titel'
 	) ) );
@@ -23,9 +33,60 @@ if( $projects ){
 	$table	= UI_HTML_Tag::create( 'table', $colgroup.$thead.$tbody, array( 'class' => 'table table-striped' ) );
 }
 
+$buttonAdd	= UI_HTML_Tag::create( 'a', '<i class="icon-plus icon-white"></i> hinzufügen', array( 'href' => './manage/customer/project/add', 'class' => 'btn btn-success' ) );
+
+
+/*
+$list	= array();
+foreach( $projects as $project ){
+	
+	$list[]	= UI_HTML_Tag::create( 'li', $project->title, array( 'class' => 'autocut' ) );
+}
+$list	= UI_HTML_Tag::create( 'ul', $list );
+*/
+
+$optProject	= array( '' => '' );
+foreach( $projects as $project )
+	$optProject[$project->projectId]	= $project->title;
+$optProject	= UI_HTML_Elements::Options( $optProject );
+
+$optType	= UI_HTML_Elements::Options( $words['types'] );
+
 return '
 <h3><span class="muted">Kunde</span> '.$customer->title.'</h3>
 '.$tabs.'
-'.$table.'
-
+<div class="row-fluid">
+	<div class="span8">
+		<div class="content-panel">
+			<div class="content-panel-inner">
+				<h4>...</h4>
+				'.$table.'
+			</div>
+		</div>
+	</div>
+	<div class="span4">
+		<div class="content-panel">
+			<div class="content-panel-inner">
+				<h4>Zuweisen</h4>
+				<form action="./manage/customer/project/add/'.$customerId.'" method="post">
+					<div class="row-fluid">
+						<div class="span12">
+							<label for="input_projectId">Projekt</label>
+							<select id="input_projectId" name="projectId" onchange="this.form.submit()">'.$optProject.'</select>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span12">
+							<label for="input_type">Art der Verbindung</label>
+							<select id="input_type" name="type">'.$optType.'</select>
+						</div>
+					</div>
+					<div class="buttonbar">
+						'.$buttonAdd.'
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
 ';
