1d12a8b5f22781cb505ed78ceb45685505a7ab8a
Christian Würker <kriss@ceusmedia.de>
2015-09-14 12:39:06 +0200

    Prepare main script for new Git sources.

--- App/Site/index.php
+++ App/Site/index.php
@@ -1,20 +1,11 @@
 <?php
 $errorReporting		= E_ALL;										//  enable full reporting
 $displayErrors		= TRUE;											//  enable error display
-
-$pathLibraries		= "";											//  path to libraries
-
-$versionCMC			= 'trunk';										//  branch path of cmClasses
-$versionCMF			= 'trunk';										//  branch path of cmFrameworks
-
 $pathClasses		= "classes/";
 $configFile			= "config/config.ini";							//  set an alternative config file
 $classRouter		= "CMF_Hydrogen_Environment_Router_Recursive";	//  set an alternative router class
 $defaultTimezone	= "Europe/Berlin";								//  default time zone
 
-//  -------------------------------  //
-//  --  NO NEED TO CHANGE BELOW  --  //
-//  -------------------------------  //
 if( isset( $errorReporting ) )
 	error_reporting( $errorReporting );
 if( isset( $displayErrors ) )
@@ -22,9 +13,28 @@ if( isset( $displayErrors ) )
 if( $defaultTimezone )
 	date_default_timezone_set( $defaultTimezone );					//  set default time zone
 
-$path	= isset( $pathLibraries ) ? $pathLibraries : "";			//  realize library path
-require_once $path.'cmClasses/'.$versionCMC.'/autoload.php5';		//  load cmClasses
-require_once $path.'cmFrameworks/'.$versionCMF.'/autoload.php5';	//  load cmFrameworks
+if( include_once "vendor/autoload.php" ){
+	require_once "vendor/ceus-media/common/compat.php";
+	Loader::registerNew( 'php5', NULL, $pathClasses );				//  register autoloader for project classes
+
+}
+else{
+	$pathLibraries		= "";											//  path to libraries
+
+	$versionCMC			= 'trunk';										//  branch path of cmClasses
+	$versionCMF			= 'trunk';										//  branch path of cmFrameworks
+
+	$path	= isset( $pathLibraries ) ? $pathLibraries : "";			//  realize library path
+	require_once $path.'cmClasses/'.$versionCMC.'/autoload.php5';		//  load cmClasses
+	require_once $path.'cmFrameworks/'.$versionCMF.'/autoload.php5';	//  load cmFrameworks
+
+	CMC_Loader::registerNew( 'php5', NULL, $pathClasses );			//  register autoloader for project classes
+}
+
+
+//  -------------------------------  //
+//  --  NO NEED TO CHANGE BELOW  --  //
+//  -------------------------------  //
 
 if( !empty( $configFile ) )											//  an alternative config file has been set
 	CMF_Hydrogen_Environment_Web::$configFile	= $configFile;		//  set alternative config file in environment
@@ -32,7 +42,6 @@ if( !empty( $classRouter ) )										//  an alternative router class has been s
 	CMF_Hydrogen_Environment_Web::$classRouter	= $classRouter;		//  set alternative router class in environment
 
 try{
-	CMC_Loader::registerNew( 'php5', NULL, $pathClasses );			//  register autoloader for project classes
 	$app	= new CMF_Hydrogen_Application_Web_Site();				//  create default web site application instance
 	$app->run();													//  and run it
 }
