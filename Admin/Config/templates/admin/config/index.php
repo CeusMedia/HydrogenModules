e4b5cbe47baeef2702306d9bd3d129f3bd738eb1
Christian Würker <kriss@ceusmedia.de>
2015-11-09 16:06:48 +0100

    Fixed module configuration.

--- Admin/Config/templates/admin/config/index.php
+++ Admin/Config/templates/admin/config/index.php
@@ -16,42 +16,68 @@ $iconLock		= new UI_HTML_Tag( 'i', '', array( 'class' => 'icon-lock', 'title' =>
 $iconUnlock		= new UI_HTML_Tag( 'i', '', array( 'class' => 'icon-unlock', 'title' => 'unprotected' ) );
 $iconUser		= new UI_HTML_Tag( 'i', '', array( 'class' => 'icon-user', 'title' => 'configurable by user' ) );
 
-$rows	= array();
+//print_m( $config );die;
+
+$list	= array();
 foreach( $config as $moduleId => $module ){
 	if( !$module->config )
 		continue;
-	$rows[]	= new UI_HTML_Tag( 'tr', new UI_HTML_Tag( 'th', $module->title, array( 'colspan' => 5 ) ) );
+	$rows	= array();
 	foreach( $module->config as $item ){
-
-		if( is_bool( $item->value ) )
-			$item->value	= $item->value ? '<em>TRUE</em>' : '<em>FALSE</em>';
-		$value	= new UI_HTML_Tag( 'span', $item->value, array(
-			'class'		=> 'item-value',
-			'data-id'	=> 'input-'.$moduleId.'-'.$item->key,
-		) );
+		$isNumeric		= in_array( $item->type, array( "integer", "float" ) ) || preg_match( "/^[0-9\.]+$/", $item->value );
 		if( $item->values ){
 			$values		= array_combine( $item->values, $item->values );
 			$options	= UI_HTML_Elements::Options( $values, $item->value );
+			$class		= $isNumeric ? "span3" : "span12";
 			$input		= new UI_HTML_Tag( 'select', $options, array(
 				'name'	=> $moduleId.'|'.$item->key,
-				'class'	=> 'span12',
+				'class'	=> $class,
 			) );
 		}
 		else if( $item->type === "boolean" ){
-			$values		= array_combine( array( 'yes', 'no' ), array( 'yes', 'no' ) );
+/*			$values		= array_combine( array( 'yes', 'no' ), array( 'yes', 'no' ) );
 			$options	= UI_HTML_Elements::Options( $values, $item->value );
 			$input		= new UI_HTML_Tag( 'select', $options, array(
 				'name'	=> $moduleId.'|'.$item->key,
 				'id'	=> 'input-'.$moduleId.'-'.$item->key,
 				'class'	=> 'span12',
-			) );
+			) );*/
+			$inputYes	= UI_HTML_Tag::create( 'input', NULL, array(
+				'name'		=> $moduleId.'|'.$item->key,
+				'type'		=> 'radio',
+				'value'		=> 'yes',
+				'checked'	=> !!$item->value ? 'checked' : NULL,
+			) ).'&nbsp;yes';
+			$inputNo	= UI_HTML_Tag::create( 'input', NULL, array(
+				'name'		=> $moduleId.'|'.$item->key,
+				'type'		=> 'radio',
+				'value'		=> 'no',
+				'checked'	=> !$item->value ? 'checked' : NULL,
+			) ).'&nbsp;no';
+			$inputYes		= UI_HTML_Tag::create( 'label', $inputYes, array( 'class' => 'checkbox inline' ) );
+			$inputNo		= UI_HTML_Tag::create( 'label', $inputNo, array( 'class' => 'checkbox inline' ) );
+			$input			= $inputYes.$inputNo;
+
 		}
 		else{
-			$input		= new UI_HTML_Tag( 'input', NULL, array(
-				'type'	=> 'text',
-				'name'	=> $moduleId.'|'.$item->key,
-				'class'	=> 'span12',
-			) );
+			if( preg_match( "/,/", $item->value ) ){
+				$value		= str_replace( ",", "\n", htmlentities( $item->value, ENT_QUOTES, 'UTF-8' ) );
+				$input		= new UI_HTML_Tag( 'textarea', $value, array(
+					'name'			=> $moduleId.'|'.$item->key,
+					'class'			=> 'span6',
+					'rows'			=> count( explode( ",", $item->value ) ),
+				) );
+			}
+			else{
+				$class		= $isNumeric ? "span3" : "span12";
+				$input		= new UI_HTML_Tag( 'input', NULL, array(
+					'type'			=> 'text',
+					'name'			=> $moduleId.'|'.$item->key,
+					'class'			=> $class,
+					'value'			=> htmlentities( $item->value, ENT_QUOTES, 'UTF-8' ),
+					'placeholder'	=> $item->title,
+				) );
+			}
 		}
 
 		$protection	= $iconUnlock;
@@ -61,19 +87,34 @@ foreach( $config as $moduleId => $module ){
 			$protection	= $iconLock;
 
 		$key	= $item->mandatory ? '<b>'.$item->key.'</b>' : $item->key;
-
+		$key	= $item->title ? '<abbr title="'.$item->title.'">'.$key.'</abbr>' : $key;
+		$type	= '<small class="muted">'.$item->type.'</small>';
 		$rows[]	= new UI_HTML_Tag( 'tr', array(
 			new UI_HTML_Tag( 'td', $protection ),
-			new UI_HTML_Tag( 'td', $item->type ),
-			new UI_HTML_Tag( 'td', $key ),
-			new UI_HTML_Tag( 'td', $value ),
-			new UI_HTML_Tag( 'td', $item->title ),
+			new UI_HTML_Tag( 'td', $key, array( 'class' => 'autocut' ) ),
+			new UI_HTML_Tag( 'td', $type ),
+			new UI_HTML_Tag( 'td', $input ),
 		) );
 	}
+	$cols	= UI_HTML_Elements::ColumnGroup( "2.5%", "37%", "7.5%", "53%" );
+	$tbody	= new UI_HTML_Tag( 'tbody', $rows );
+	$table	= new UI_HTML_Tag( 'table', $cols.$tbody, array( 'class' => 'table table-condensed table-striped', 'style' => 'table-layout: fixed' ) );
+	$list[]	= new UI_HTML_Tag( 'h4', $module->title ).$table;
 }
-$cols	= UI_HTML_Elements::ColumnGroup( "30px", "60px" );
-$tbody	= new UI_HTML_Tag( 'tbody', $rows );
-$table	= new UI_HTML_Tag( 'table', $cols.$tbody, array( 'class' => 'table' ) );
 
 return '<h3>'.$words['index']['heading'].'</h3>
-'.$table.'';
+<div class="row-fluid">
+	<div class="span12">
+		'.join( $list ).'
+	</div>
+</div>
+<style>
+body.moduleAdminConfig table input,
+body.moduleAdminConfig table select {
+	margin: 0px;
+	}
+body.moduleAdminConfig table input,
+body.moduleAdminConfig table select {
+	}
+</style>
+';
