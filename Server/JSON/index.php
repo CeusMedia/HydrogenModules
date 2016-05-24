ce0d57413c14ab2a15ae5002fb7fc4516396006a
kriss0r <kriss@ceusmedia.de>
2016-05-24 22:57:26 +0200

    Adjust JSON server index script to work with composer.

--- Server/JSON/index.php
+++ Server/JSON/index.php
@@ -1,15 +1,9 @@
 <?php
-$pathLibs		= "";													//  path to libraries
-$versionCMC		= "trunk";												//  path to cmClasses
-$versionCMF		= "trunk";												//  path to cmFrameworks
-$versionCMM		= "trunk";												//  path to cmModules
 $timezone		= "Europe/Berlin";										//  default time zone
 
-require_once $pathLibs.'cmClasses/'.$versionCMC.'/autoload.php5';		//  load cmClasses
-require_once $pathLibs.'cmFrameworks/'.$versionCMF.'/autoload.php5';	//  load cmFrameworks
-require_once $pathLibs.'cmModules/'.$versionCMM.'/autoload.php5';		//  load cmModules
+require_once 'vendor/autoload.php';
 
-CMC_Loader::registerNew( 'php5', NULL, 'classes/' );					//  register new autoloader
+Loader::registerNew( 'php5', NULL, 'classes/' );						//  register new autoloader
 
 if( $timezone )
 	date_default_timezone_set( $timezone );								//  set default time zone
