895b0239832ea1b285548bedbd642b36a15e0d60
kriss0r <kriss@ceusmedia.de>
2016-11-16 18:51:31 +0100

    Extend export statuses.

--- Work/Mail/Check/templates/work/mail/check/export.php
+++ Work/Mail/Check/templates/work/mail/check/export.php
@@ -2,16 +2,22 @@
 $optType	= array( 'CSV' => 'CSV' );
 $optType	= UI_HTML_Elements::Options( $optType, key( $optType ) );
 
-$optStatus	= array(
-	-1	=> 'ungültig',
-	2	=> 'gültig',
-);
-$optStatus	= UI_HTML_Elements::Options( $optStatus, array( -1, 2 ) );
-
 $optGroup	= array();
 foreach( $groups as $group )
 	$optGroup[$group->mailGroupId]	= $group->title;
 $optGroup	= UI_HTML_Elements::Options( $optGroup, $groups ? $groups[0]->mailGroupId : NULL );
+
+$statuses	= array(
+	-2	=> 'nicht erreichbar',
+	-1	=> 'abgelehnt',
+	2	=> 'erreichbar',
+);
+
+$optStatus	= array();
+foreach( $statuses as $key => $label )
+	$optStatus[$key]	= $label;
+$optStatus	= UI_HTML_Elements::Options( $optStatus, array( 2 ) );
+
 
 $tabs	= View_Work_Mail_Check::renderTabs( $env, 'export' );
 
@@ -33,14 +39,9 @@ return $tabs.'
 						</div>
 					</div>
 					<div class="row-fluid">
-						<label>Exportiere nur</label>
 						<div class="span12">
-							<label for="input_status_1" class="radio">
-								<input type="radio" name="status" id="input_status_1" value="2" checked="checked"/>&nbsp;gültig
-							</label>
-							<label for="input_status_0" class="radio">
-								<input type="radio" name="status" id="input_status_0" value="-1"/>&nbsp;ungültig
-							</label>
+							<label for="input_status">Zustand</label>
+							<select name="status[]" id="input_status" class="span12" multiple="multiple" size="3">'.$optStatus.'</select>
 						</div>
 					</div>
 					<div class="buttonbar">
