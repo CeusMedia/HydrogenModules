6fff051cd9ca0387519193fd49e9119dea4e9faf
Christian Würker <christian.wuerker@ceusmedia.de>
2017-10-26 04:33:12 +0200

    Center login if info block is empty.

--- Resource/Authentication/Backend/Local/templates/auth/local/login.php
+++ Resource/Authentication/Backend/Local/templates/auth/local/login.php
@@ -1,21 +1,23 @@
 <?php
 
+$env->getPage()->js->addScriptOnReady('Auth.Login.init();');
+
 $panelLogin	= $view->loadTemplateFile( 'auth/local/login.form.php' );
 
 extract( $view->populateTexts( array( 'top', 'info', 'bottom' ), 'html/auth/local/login/', array( 'from' => $from ) ) );
 
 $tabs	= View_Auth::renderTabs( $env, 'auth/local/login' );
 
-$env->getPage()->js->addScriptOnReady('Auth.Login.init();');
+if( strlen( trim( strip_tags( $textInfo ) ) ) ){
+	return $tabs.$textTop.
+		HTML::DivClass( "row-fluid", array(
+			HTML::DivClass( "span4", $panelLogin ),
+			HTML::DivClass( "span8", $textInfo ),
+		) ).$textBottom;
+}
 
-return $tabs.$textTop.
-HTML::DivClass( "row-fluid", array(
-	HTML::DivClass( "span4",
-		$panelLogin
-	),
-	HTML::DivClass( "span8",
-		$textInfo
-	)
-) ).
-$textBottom;
+return '<br/></br/><br/><br/><br/><br/>'.$tabs.$textTop.
+	HTML::DivClass( "row-fluid", array(
+		HTML::DivClass( "span4 offset4", $panelLogin )
+	) ).$textBottom;
 ?>
