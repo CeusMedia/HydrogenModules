ad5ae601facc905fde4c668a7addf9e492082e9e
Christian Würker <kriss@ceusmedia.de>
2017-12-27 03:48:41 +0100

    Small improvements.

--- Shop/templates/shop/cart.php
+++ Shop/templates/shop/cart.php
@@ -2,23 +2,22 @@
 $w		= (object) $words['cart'];
 
 $tablePositions	= '<p><em class="muted">'.$w->empty.'</em></p>';
+$buttonbar		= '';
+
 if( count( $positions ) ){
-	$helperCart	= new View_Helper_Shop_CartPositions( $env );
+	$helperCart		= new View_Helper_Shop_CartPositions( $env );
 	$helperCart->setPositions( $positions );
 	$helperCart->setChangeable( TRUE );
-	$helperCart->setOutput( View_Helper_Shop_CartPositions::OUTPUT_HTML );
-	$tablePositionsDesktop	= UI_HTML_Tag::create( 'div', $helperCart->render(), array( 'class' => 'hidden-phone' ) );
-	$helperCart->setOutput( View_Helper_Shop_CartPositions::OUTPUT_HTML_LIST );
-	$tablePositionsPhone	= UI_HTML_Tag::create( 'div', $helperCart->render(), array( 'class' => 'visible-phone' ) );
-	$tablePositions			= $tablePositionsDesktop.$tablePositionsPhone;
+	$tablePositions	= $helperCart->render();
+	$buttonbar		= UI_HTML_Tag::create( 'div', array(
+		new \CeusMedia\Bootstrap\LinkButton( './shop/customer', $w->buttonToCustomer, 'btn-success not-pull-right', 'fa fa-fw fa-arrow-right', !$positions )
+	), array( 'class' => 'buttonbar well well-small' ) );
 }
 
 $tabContent	= '
 	<h3>'.$w->heading.'</h3>
 	'.$tablePositions.'
-	<div class="buttonbar well well-small">
-		'.new \CeusMedia\Bootstrap\LinkButton( './shop/customer', $w->buttonToCustomer, 'btn-success not-pull-right', 'fa fa-fw fa-arrow-right', !$positions ).'
-	</div>
+	'.$buttonbar.'
 ';
 
 $helperTabs		= new View_Helper_Shop_Tabs( $env );
