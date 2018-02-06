a92aeddc88efe8f535a847d21425b1f7498bc131
Christian Würker <kriss@ceusmedia.de>
2018-02-06 02:12:58 +0100

    Handle hidden required inputs earlier.

--- JS/Form/Optionals/js/FormOptionals.js
+++ JS/Form/Optionals/js/FormOptionals.js
@@ -1,42 +1,42 @@
 
 var FormOptionals = {
-    init: function () {
-        $(":input.has-optionals").bind("change change-update", function () {
-            FormOptionals.showOptionals(this);
+	init: function () {
+		$(":input.has-optionals").bind("change change-update", function () {
+			FormOptionals.showOptionals(this);
 		}).trigger("change-update");
-    },
-    showOptionals: function (elem) {
-        var form = $(elem.form);
-        var name = $(elem).attr("name");
-        var type = $(elem).attr("type");
-        var value = name+"-" + $(elem).val();
-        if (type === "checkbox") {
-            value = name + "-" + $(elem).prop("checked");
-        }
-        var toHide = form.find(".optional." + name).not("." + value);
-        var toShow = form.find(".optional." + value);
-        if (!$(elem).data("status")) {
-            toHide.hide();
-            toShow.show();
-            $(elem).data("status", 1);
-            return;
-        }
-        switch ($(elem).data('animation')) {
-        case 'fade':
-            toHide.fadeOut();
-            toShow.fadeIn();
-            break;
-        case 'slide':
-            toHide.slideUp($(elem).data('speed-hide'));
-            toShow.slideDown($(elem).data('speed-show'));
-            break;
-        default:
-            toHide.hide();
-            toShow.show();
-        }
+	},
+	showOptionals: function (elem) {
+		var form = $(elem.form);
+		var name = $(elem).attr("name");
+		var type = $(elem).attr("type");
+		var value = name+"-" + $(elem).val();
+		if (type === "checkbox") {
+			value = name + "-" + $(elem).prop("checked");
+		}
+		var toHide = form.find(".optional." + name).not("." + value);
+		var toShow = form.find(".optional." + value);
 		FormOptionals.disableRequired(toHide);
 		FormOptionals.enableRequired(toShow);
-    },
+		if (!$(elem).data("status")) {
+			toHide.hide();
+			toShow.show();
+			$(elem).data("status", 1);
+			return;
+		}
+		switch ($(elem).data('animation')) {
+		case 'fade':
+			toHide.fadeOut();
+			toShow.fadeIn();
+			break;
+		case 'slide':
+			toHide.slideUp($(elem).data('speed-hide'));
+			toShow.slideDown($(elem).data('speed-show'));
+			break;
+		default:
+			toHide.hide();
+			toShow.show();
+		}
+	},
 	disableRequired: function(container){
 		container.find(":input").each(function(){
 			var elem = jQuery(this);
@@ -59,5 +59,5 @@ var FormOptionals = {
 
 /* @deprecated by hook
 function showOptionals (elem) {
-    FormOptionals.showOptionals(elem);
+	FormOptionals.showOptionals(elem);
 }*/
