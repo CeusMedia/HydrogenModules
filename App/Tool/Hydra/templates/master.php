171db9e9533d8973de49f44ed901f51a066c653d
Christian Würker <kriss@ceusmedia.de>
2018-03-28 01:41:00 +0200

    Replace CDN resource.

--- App/Tool/Hydra/templates/master.php
+++ App/Tool/Hydra/templates/master.php
@@ -115,7 +115,7 @@ $env->clock->profiler->tick( 'interface: master' );
 $pathJsLib		= $config->get( 'path.scripts.lib' );
 $pathCssLib		= $config->get( 'path.styles.lib' );
 
-$page->addFavouriteIcon( 'http://cdn.int1a.net/img/famfamfam/silk/brick.png' );
+$page->addFavouriteIcon( 'http://cdn.ceusmedia.de/img/famfamfam/silk/brick.png' );
 
 $page->css->primer->addUrl( $pathCssLib.'xmp.formats.css' );
 $page->css->primer->addUrl( $pathCssLib.'layout.column.css' );
