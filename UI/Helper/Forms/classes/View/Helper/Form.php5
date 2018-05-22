3a419b704d6d9ccb8a4f15f85b94cfd06d8c8f15
Christian Würker <kriss@ceusmedia.de>
2018-05-22 20:30:43 +0200

    Add script and style.

--- UI/Helper/Forms/classes/View/Helper/Form.php5
+++ UI/Helper/Forms/classes/View/Helper/Form.php5
@@ -1,16 +1,119 @@
 <?php
 class View_Helper_Form/* extends CMF_Hydrogen_View_Helper*/{
 
+	protected $emv;
+	protected $blocks				= array();
+	protected $form;
+	protected $formId;
+	protected $modelBlock;
+	protected $modelForm;
+	protected $returnCode;
+
 	public function __construct( $env ){
 		$this->env	= $env;
+		$this->modelForm	= new Model_Form( $this->env );
+		$this->modelBlock	= new Model_Form_Block( $this->env );
+		foreach( $this->modelBlock->getAll() as $item )
+			$this->blocks[$item->identifier]	= $item;
+		$this->returnCode	= (int) $this->env->getRequest()->get( 'rc' );
+	}
+
+	public function renderStatic( $env, $formId ){
+		$helper	= new View_Helper_Form( $env );
+		return $helper->setId( $formId )->render();
+	}
+
+	public function setId( $formId ){
+		$form	= $this->modelForm->get( $formId );
+		if( !$form )
+			throw new RangeException( 'Invalid form ID given: '.$formId );
+		$this->form		= $form;
+		$this->formId	= $formId;
+		return $this;
 	}
 
 	public function render(){
-
+		$resultMessages		= $this->renderResultMessages();
+		$content			= $resultMessages;
+		if( !$this->returnCode )
+			$content		.= $this->renderForm();
+		while( preg_match( '/\[block_(\S+)\]/su', $content ) ){
+			$identifier		= preg_replace( '/.*\[block_(\S+)\].*/su', "\\1", $content );
+			$replace		= isset( $this->blocks[$identifier] ) ? $this->blocks[$identifier]->content : '';
+			$pattern		= '/'.preg_quote( '[block_'.$identifier.']' ).'/su';
+			$content		= preg_replace( $pattern, $replace, $content, 1 );
+		}
+		if( !$this->returnCode ){
+			$form		= $this->modelForm->get( $this->formId );
+			$clientUrl	= $this->env->getConfig()->get( 'app.url' );
+			$devMode	= $form->status < Model_Form::STATUS_ACTIVATED ? 'true' : 'false';
+			$scripts	= join( '', array(
+				UI_HTML_Tag::create( 'script', 'Forms.init("'.$clientUrl.'", '.$devMode.').apply("form-'.$this->formId.'");' ),
+	//			UI_HTML_Tag::create( 'script', 'FormOptionals.init();' ),
+			) );
+			$content	.= $scripts;
+		}
+		return $content;
 	}
 
-	public function renderStatic( $env ){
-		$helper	= new View_Helper_Form( $env );
-		return $helper->render();
+	protected function renderForm(){
+		$form		= $this->modelForm->get( $this->formId );
+		$button		= UI_HTML_Tag::create( 'div', array(
+			UI_HTML_Tag::create( 'button', 'abschicken', array( 'type' => 'submit', 'name' => 'send', 'class' => 'cmsmasters_button' ) ),
+		), array( 'class' => 'cmforms-row' ) );
+		if( substr_count( $form->content, '[block_row_button]' ) )
+			$button	= '';
+		return UI_HTML_Tag::create( 'form', array(
+			UI_HTML_Tag::create( 'input', NULL, array( 'type' => 'hidden', 'name' => 'formId', 'value' => $this->formId ) ),
+			$form->content,
+			$button,
+		), array(
+			'id'		=> 'form-'.$this->formId,
+			'action' 	=> '#',//'./?action=fill&id='.$this->formId,
+			'method' 	=> 'post',
+			'class'		=> 'cmforms',
+			'onsubmit'	=> 'Forms.sendForm(this); return false;',
+		) );
+	}
+
+	protected function renderResultMessages(){
+		$messageCode	= '';
+		$messageError	= '';
+		$messageSuccess	= '';
+		$blocks			= ADT_List_Dictionary::create( $this->blocks )->getAll( 'message_' );
+		if( $this->returnCode === 2 && isset( $blocks['result_confirmed'] ) )
+			$messageCode	=  UI_HTML_Tag::create( 'div', $blocks['result_confirmed']->content, array(
+				'class'	=> 'form-message-code',
+			) );
+		else if( $this->returnCode === 3 && isset( $blocks['result_confirmed_already'] ) )
+			$messageCode	=  UI_HTML_Tag::create( 'div', $blocks['result_confirmed_already']->content, array(
+				'class'	=> 'form-message-code',
+			) );
+		if( isset( $blocks['error'] ) ){
+			$messageError	= UI_HTML_Tag::create( 'div', $blocks['error']->content, array(
+				'class'	=> 'form-message-error',
+				'style'	=> 'display: none',
+			) );
+		}
+		if( isset( $blocks['success'] ) ){
+			$messageSuccess	= UI_HTML_Tag::create( 'div', $blocks['success']->content, array(
+				'class'	=> 'form-message-success',
+				'style'	=> 'display: none',
+			) );
+		}
+		$form		= $this->modelForm->get( $this->formId );
+		if( $form->type == Model_Form::TYPE_CONFIRM ){
+			if( isset( $blocks['success_confirm'] ) ){
+				$messageSuccess	= UI_HTML_Tag::create( 'div', $blocks['success_confirm']->content, array(
+					'class'	=> 'form-message-success',
+					'style'	=> 'display: none',
+				) );
+			}
+		}
+		return join( '', array(
+			$messageCode,
+			$messageError,
+			$messageSuccess,
+		) );
 	}
 }
---
+++ UI/Helper/Forms/js/module.ui.helper.forms.js
@@ -0,0 +1,202 @@
+/**
+ * Ceus Media Forms
+ * Wordpress plugin for external forms.
+ * Author		Christian Würker <christian.wuerker@ceusmedia.de>
+ * Version		1.0
+ */
+var Forms = {
+	mode: 'live',
+	status: 0,
+	urlServer: null,
+	parameters: {},
+
+	apply: function(formId){
+		if(typeof Forms.parameters['preseed'] !== "undefined")
+			Forms.prefillData(formId);
+		if( Forms.mode === 'dev' )
+			Forms.initCollapsableRows(formId);
+		FormOptionals.init("#"+formId);
+	},
+
+	collectFormData: function(form){
+		var data = {};
+		form.find(':input').each(function(){
+			if(this.nodeName === "BUTTON")
+				return;
+			var node = jQuery(this);
+			var item = {
+				id: this.id,
+				type: this.nodeName.toLowerCase(),
+				name: this.name,
+				label: null,
+				value: node.val(),
+				valueLabel: null,
+			};
+			var relatedLabel = jQuery("label[for='input_"+item.name+"']");
+			if(relatedLabel.size())
+				item.label = relatedLabel.get(0).innerText;
+
+			if(this.nodeName === "SELECT"){
+				item.type = "select";
+				var value = node.val();
+				var options = node.children('option');
+				for(i=0; i<options.size(); i++){
+					if(options.eq(i).val() == value){
+						item.valueLabel = options.eq(i).html();
+						break;
+					}
+				}
+			}
+			if(this.nodeName === "INPUT"){
+				var inputType = node.attr('type');
+
+				if(inputType === "date"){
+					item.type = "date";
+				}
+				if(inputType === "checkbox"){
+					item.type = "checkbox";
+					item.value = node.is(":checked") ? 'ja' : 'nein';
+					item.valueLabel = node.is(":checked") ? 'ja' : 'nein';
+				}
+				else if(inputType === "radio"){
+					if(typeof data[this.name] !== "undefined"){
+						item = data[this.name];
+					}
+					else{
+						item.type = "radio";
+						item.id = 'input_'+this.name;
+//						item.value = '['+this.name+']';
+						item.valueLabel	= "";
+						if(relatedLabel.size())
+							item.label = relatedLabel.get(0).innerText;
+					}
+					if(node.is(":checked")){
+						item.value = this.value;
+						item.valueLabel	= node.parent().get(0).innerText;
+						var radioSpan = jQuery("span#input_"+this.name+"-"+this.value);
+						if(radioSpan.size())
+							item.valueLabel	= radioSpan.get(0).innerText;
+					}
+				}
+				else{
+					item.type = 'text';
+				}
+			}
+			data[item.name]	= item;
+		});
+		return data;
+	},
+
+	handleResponse: function(response){
+//		console.log(response);
+		var form = jQuery("#form-"+response.data.formId);
+		if(response.status == "ok"){
+			form.slideUp(500, function(){
+				form.parent().find(".form-message-error").slideUp(150);
+				form.parent().find(".form-message-success").slideDown(500);
+			});
+		}
+		else if(response.status == "error"){
+			form.parent().find(".form-message-error-title").html(response.data.error);
+			form.slideUp(500, function(){
+				form.parent().find(".form-message-success").slideUp(150);
+				form.parent().find(".form-message-error").slideDown(500);
+			});
+/*			form.css({opacity: 1});
+			form.find(':input').removeProp('disabled');
+			form.find('button').each(function(){
+				jQuery(this).removeProp('disabled');
+			});*/
+		}
+	},
+
+	init: function(urlServer, devMode, formId){
+		if( typeof devMode !== "undefined" )
+			Forms.mode = devMode ? 'dev' : 'live';
+		Forms.urlServer = urlServer;
+		Forms.status = 1;
+
+		//  COLLECT URL REQUEST PARAMETERS
+		if(!Object.keys(Forms.parameters).length){
+			var parameter;
+			var parameters = decodeURIComponent(window.location.search.substring(1)).split('&');
+			for( i=0; i<parameters.length; i++){
+				parameter = parameters[i].split('=');
+				parameter[1] = typeof parameter[1] === "undefined" ? true : parameter[1];
+				Forms.parameters[parameter[0]] = parameter[1];
+			}
+		}
+		return Forms;
+	},
+
+	initCollapsableRows: function(formId){
+		jQuery("#"+formId+" .cmforms-row").prepend(jQuery('<span></span>')
+			.attr({class: 'trigger-close'}).html('Zeile verbergen')
+			.bind('click', function(elem){jQuery(elem.target).parent().slideUp()}));
+	},
+
+	prefillData: function(formId){
+		jQuery("#"+formId+" :input").each(function(){
+			if(this.nodeName == "INPUT" || this.nodeName == "SELECT"){
+				if(typeof Forms.parameters[this.name] !== "undefined"){
+					jQuery(this).val(Forms.parameters[this.name]);
+				}
+			}
+		});
+	},
+
+	sendForm: function (elem){
+		if(Forms.status != 1){
+			alert('Das Formular wurde nicht initiiert.');
+			return false;
+		}
+		try{
+			var form = jQuery(elem);
+			form.css({opacity: 1});
+			form.find('button').each(function(){jQuery(this).prop('disabled', 'disabled')});
+			Forms.validateFormData(form);
+			if(Forms.errors.length){
+				alert('Das Formular wurde nicht korrekt ausgefüllt.' );
+//				console.log(Forms.errors);
+			}
+			else{
+				var data = Forms.collectFormData(form);
+//				console.log(data);
+				jQuery.ajax({
+					url: Forms.urlServer+'?action=fill_receive',
+					method: 'POST',
+					data: {inputs: data},
+					dataType: 'json',
+					success: Forms.handleResponse,
+					error: function(a,b){
+						console.log(a);
+						console.log(b);
+					}
+				});
+			}
+		}
+		catch (e){
+			alert('Bei der Verarbeitung des Formulars ist ein Fehler aufgetreten.' );
+			console.log(e);
+		}
+		return false;
+	},
+
+	validateFormData: function(form){
+		Forms.errors	= [];
+		form.find(':input').each(function(){
+			if(this.nodeName === "BUTTON")
+				return;
+			var input = jQuery(this);
+			if(input.attr('required')){
+				if(!input.val().length){
+					Forms.errors.push({
+						id: this.id,
+						rule: 'required',
+					});
+					return;
+				}
+			}
+		});
+	}
+};
