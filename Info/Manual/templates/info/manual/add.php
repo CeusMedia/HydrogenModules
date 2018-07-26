7d778f0a53bd45534c22cc278bb5c3b93459057a
Christian Würker <kriss@ceusmedia.de>
2018-07-26 03:23:40 +0200

    Improve.

--- Info/Manual/templates/info/manual/add.php
+++ Info/Manual/templates/info/manual/add.php
@@ -43,36 +43,40 @@ return '
 		'.$buttonReload.'
 	</div>
 	<div class="span9">
-		<h3>'.$words['add']['heading'].'</h3>
-		<form action="./info/manual/add" method="post">
-			<div class="row-fluid">
-				<div class="span4">
-					<label for="input_title">'.$words['add']['labelTitle'].'</label>
-					<input type="text" name="title" id="input_title" class="span12" value="'.htmlentities( $title, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
-				</div>
-				<div class="span2">
-					<label for="input_format">'.$words['add']['labelFormat'].'</label>
-					<select name="format" id="input_format" class="span12">'.$optFormat.'</select>
-				</div>
-				<div class="span2">
-					<label for="input_version">'.$words['add']['labelVersion'].'</label>
-					<input type="number" min="1" name="version" id="input_version" class="span12" value="'.htmlentities( max( 1, $version ), ENT_QUOTES, 'UTF-8' ).'" required="required"/>
-				</div>
-				<div class="span2">
-					<label for="input_rank">'.$words['add']['labelRank'].'</label>
-					<input type="number" min="1" name="rank" id="input_rank" class="span12" value="'.htmlentities( max( 1, $rank ), ENT_QUOTES, 'UTF-8' ).'" required="required"/>
-				</div>
+		<div class="content-panel">
+			<h3>'.$words['add']['heading'].'</h3>
+			<div class="content-panel-inner">
+				<form action="./info/manual/add" method="post">
+					<div class="row-fluid">
+						<div class="span4">
+							<label for="input_title">'.$words['add']['labelTitle'].'</label>
+							<input type="text" name="title" id="input_title" class="span12" value="'.htmlentities( $title, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
+						</div>
+						<div class="span2">
+							<label for="input_format">'.$words['add']['labelFormat'].'</label>
+							<select name="format" id="input_format" class="span12">'.$optFormat.'</select>
+						</div>
+						<div class="span2">
+							<label for="input_version">'.$words['add']['labelVersion'].'</label>
+							<input type="number" min="1" name="version" id="input_version" class="span12" value="'.htmlentities( max( 1, $version ), ENT_QUOTES, 'UTF-8' ).'" required="required"/>
+						</div>
+						<div class="span2">
+							<label for="input_rank">'.$words['add']['labelRank'].'</label>
+							<input type="number" min="1" name="rank" id="input_rank" class="span12" value="'.htmlentities( max( 1, $rank ), ENT_QUOTES, 'UTF-8' ).'" required="required"/>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span12">
+							<label for="input_content">'.$words['add']['labelContent'].'</label>
+							<textarea class="span12 CodeMirror-auto" name="content" id="input_content" rows="'.$moduleConfig->get( 'editor.rows' ).'">'.$content.'</textarea>
+						</div>
+					</div>
+					<div class="buttonbar">
+						<button type="submit" name="save" class="btn btn-small btn-success"><i class="icon-ok icon-white"></i> '.$words['add']['buttonSave'].'</button>
+					</div>
+				</form>
 			</div>
-			<div class="row-fluid">
-				<div class="span12">
-					<label for="input_content">'.$words['add']['labelContent'].'</label>
-					<textarea class="span12 CodeMirror-auto" name="content" id="input_content" rows="'.$moduleConfig->get( 'editor.rows' ).'">'.$content.'</textarea>
-				</div>
-			</div>
-			<div class="buttonbar">
-				<button type="submit" name="save" class="btn btn-small btn-success"><i class="icon-ok icon-white"></i> '.$words['add']['buttonSave'].'</button>
-			</div>
-		</form>
+		</div>
 	</div>
 </div>';
 ?>
--- Info/Manual/templates/info/manual/edit.php
+++ Info/Manual/templates/info/manual/edit.php
@@ -15,7 +15,7 @@ $iconRemove		= UI_HTML_Tag::create( 'i', '', array( 'class' => "icon-remove icon
 $iconUp			= UI_HTML_Tag::create( 'i', '', array( 'class' => "icon-arrow-up" ) );
 $iconDown		= UI_HTML_Tag::create( 'i', '', array( 'class' => "icon-arrow-down" ) );
 
-if( 0 && $env->getModules()->has( 'UI_Font_FontAwesome' ) ){
+if( $env->getModules()->has( 'UI_Font_FontAwesome' ) ){
 	$iconCancel		= UI_HTML_Tag::create( 'i', '', array( 'class' => "fa fa-fw fa-arrow-left" ) );
 	$iconPreview	= UI_HTML_Tag::create( 'i', '', array( 'class' => "fa fa-fw fa-eye" ) );
 	$iconSave		= UI_HTML_Tag::create( 'i', '', array( 'class' => "fa fa-fw fa-check" ) );
@@ -45,45 +45,49 @@ return '
 		'.$buttonReload.'
 	</div>
 	<div class="span9">
-		<h3><span class="muted">'.$words['edit']['heading'].' </span> '.htmlentities( $file, ENT_QUOTES, 'UTF-8' ).'</h3>
-		<form action="./info/manual/edit/'.$page->manualPageId.'" method="post">
-			<div class="row-fluid">
-				<div class="span12">
-					<label for="input_content">'.$words['edit']['labelContent'].'</label>
-					<textarea class="span12 CodeMirror-auto ace-auto" name="content" id="input_content" rows="'.$moduleConfig->get( 'editor.rows' ).'">'.$content.'</textarea>
-				</div>
+		<div class="content-panel">
+			<h3><span class="muted">'.$words['edit']['heading'].' </span> '.htmlentities( $file, ENT_QUOTES, 'UTF-8' ).'</h3>
+			<div class="content-panel-inner">
+				<form action="./info/manual/edit/'.$page->manualPageId.'" method="post">
+					<div class="row-fluid">
+						<div class="span12">
+							<label for="input_content">'.$words['edit']['labelContent'].'</label>
+							<textarea class="span12 CodeMirror-auto ace-auto" name="content" id="input_content" rows="'.$moduleConfig->get( 'editor.rows' ).'">'.$content.'</textarea>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span4">
+							<label for="input_title">'.$words['edit']['labelTitle'].'</label>
+							<input type="text" name="title" id="input_title" class="span12" value="'.htmlentities( $page->title, ENT_QUOTES, 'UTF-8' ).'"/>
+						</div>
+		<!--				<div class="span2">
+							<label for="input_version">'.$words['add']['labelVersion'].'</label>
+							<input type="number" min="1" name="version" id="input_version" class="span12" value="'.htmlentities( $page->version, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
+						</div>
+						<div class="span2">
+							<label for="input_rank">'.$words['add']['labelRank'].'</label>
+							<input type="number" min="1" name="rank" id="input_rank" class="span12" value="'.htmlentities( $page->rank, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
+						</div>-->
+		<!--				<div class="span4">
+							<label>&nbsp;</label>
+							<label class="checkbox">
+								<input type="checkbox" name="backup"/>
+								'.$words['edit']['labelBackup'].'
+							</label>
+						</div>-->
+					</div>
+					<div class="buttonbar">
+						<a href="./info/manual/page/'.$pageId.'-'.$view->urlencode( $file ).'" class="btn">'.$iconCancel.' '.$words['edit']['buttonCancel'].'</a>
+						<button type="submit" name="save" class="btn btn-success">'.$iconSave.' '.$words['edit']['buttonSave'].'</button>
+						<a href="./info/manual/page/'.$pageId.'-'.$view->urlencode( $file ).'" class="btn">'.$iconPreview.' '.$words['edit']['buttonPreview'].'</a>
+						<a href="./info/manual/removePage/'.base64_encode( $file ).'" class="btn btn-small btn-danger" onclick="return confirm(\''. addslashes( $words['edit']['buttonRemoveConfirm'] ).'\');">'.$iconRemove.' '.$words['edit']['buttonRemove'].'</a>
+						&nbsp;&nbsp;|&nbsp;&nbsp;
+						<a href="./info/manual/movePageUp/'.base64_encode( $file ).'" class="btn btn-small">'.$iconUp.' '.$words['edit']['buttonMoveUp'].'</a>
+						<a href="./info/manual/movePageDown/'.base64_encode( $file ).'" class="btn btn-small">'.$iconDown.' '.$words['edit']['buttonMoveDown'].'</a>
+					</div>
+				</form>
 			</div>
-			<div class="row-fluid">
-				<div class="span4">
-					<label for="input_title">'.$words['edit']['labelTitle'].'</label>
-					<input type="text" name="title" id="input_title" class="span12" value="'.htmlentities( $page->title, ENT_QUOTES, 'UTF-8' ).'"/>
-				</div>
-<!--				<div class="span2">
-					<label for="input_version">'.$words['add']['labelVersion'].'</label>
-					<input type="number" min="1" name="version" id="input_version" class="span12" value="'.htmlentities( $page->version, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
-				</div>
-				<div class="span2">
-					<label for="input_rank">'.$words['add']['labelRank'].'</label>
-					<input type="number" min="1" name="rank" id="input_rank" class="span12" value="'.htmlentities( $page->rank, ENT_QUOTES, 'UTF-8' ).'" required="required"/>
-				</div>-->
-<!--				<div class="span4">
-					<label>&nbsp;</label>
-					<label class="checkbox">
-						<input type="checkbox" name="backup"/>
-						'.$words['edit']['labelBackup'].'
-					</label>
-				</div>-->
-			</div>
-			<div class="buttonbar">
-				<a href="./info/manual/page/'.$pageId.'-'.$view->urlencode( $file ).'" class="btn">'.$iconCancel.' '.$words['edit']['buttonCancel'].'</a>
-				<button type="submit" name="save" class="btn btn-success">'.$iconSave.' '.$words['edit']['buttonSave'].'</button>
-				<a href="./info/manual/page/'.$pageId.'-'.$view->urlencode( $file ).'" class="btn">'.$iconPreview.' '.$words['edit']['buttonPreview'].'</a>
-				<a href="./info/manual/removePage/'.base64_encode( $file ).'" class="btn btn-small btn-danger" onclick="return confirm(\''. addslashes( $words['edit']['buttonRemoveConfirm'] ).'\');">'.$iconRemove.' '.$words['edit']['buttonRemove'].'</a>
-				&nbsp;&nbsp;|&nbsp;&nbsp;
-				<a href="./info/manual/movePageUp/'.base64_encode( $file ).'" class="btn btn-small">'.$iconUp.' '.$words['edit']['buttonMoveUp'].'</a>
-				<a href="./info/manual/movePageDown/'.base64_encode( $file ).'" class="btn btn-small">'.$iconDown.' '.$words['edit']['buttonMoveDown'].'</a>
-			</div>
-		</form>
+		</div>
 	</div>
 </div>';
 
