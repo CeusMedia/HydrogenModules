e70b1022cd33e94c5e9d02241aaef0ff2198f621
Christian Würker <kriss@ceusmedia.de>
2018-09-07 02:42:36 +0200

    Improve localization.

--- Manage/Pages/templates/manage/page/edit.meta.php
+++ Manage/Pages/templates/manage/page/edit.meta.php
@@ -1,8 +1,11 @@
 <?php
 $w				= (object) $words['edit'];
+$wMeta			= (object) $words['edit-meta'];
+
+print_m( $meta );die;
 
 if( $page->type == 1 ){
-	return '<div class="alert alert-info"><em>'.$w->no_meta.'</em></div>';
+	return '<div class="alert alert-info"><em>'.$wMeta->no_meta.'</em></div>';
 }
 
 $optChangefreq	= UI_HTML_Elements::Options( $words['changefreqs'], $page->changefreq );
@@ -11,59 +14,77 @@ $optPriority	= UI_HTML_Elements::Options( $words['priorities'], $page->priority 
 $buttonSuggest		= '';
 $buttonBlacklist	= '';
 if( $page->type == 0 ){
-	$buttonSuggest		= '<button type="button" class="btn btn-mini" onclick="ModuleManagePages.PageEditor.suggestKeyWords('.$page->pageId.', \'#input_page_keywords\');">vorschlagen</button>';
-	$buttonBlacklist	= '<button type="button" class="btn btn-mini" onclick="ModuleManagePages.PageEditor.blacklistSuggestedWords('.$page->pageId.', \'#input_page_keywords\');">Wörter ausschließen</button>';
+	$buttonSuggest		= '<button type="button" class="btn btn-mini" onclick="ModuleManagePages.PageEditor.suggestKeyWords('.$page->pageId.', \'#input_page_keywords\');">'.$wMeta->buttonSuggest.'</button>';
+	$buttonBlacklist	= '<button type="button" class="btn btn-mini" onclick="ModuleManagePages.PageEditor.blacklistSuggestedWords('.$page->pageId.', \'#input_page_keywords\');">'.$wMeta->buttonBlacklist.'</button>';
 }
 
 return '
 <div class="content-panel content-panel-form">
 	<div class="content-panel-inner">
 		<form action="./manage/page/edit/'.$page->pageId.'/'.$version.'" method="post" class="cmFormChange-auto form-changes-auto">
-			<h3>Beschreibung und Schlagwörter</h3>
+			<!--<h3>'.$wMeta->headingSettings.'</h3>-->
 			<div class="row-fluid">
 				<div class="span6">
 					<h4>Werte für diese Seite</h4>
 					<p><small class="muted">Wenn keine Werte gespeichert wurden, werden die Standartwerte benutzt.</small></p>
-					<div class="row-fluid">
-						<div class="span12">
-							<label for="input_page_description">Beschreibung</label>
-							<textarea class="span12" rows="4" name="page_description" id="input_page_description">'.htmlentities( $page->description, ENT_QUOTES, 'UTF-8' ).'</textarea>
-						</div>
-					</div>
-					<div class="row-fluid">
-						<div class="span12">
-							<label for="input_page_keywords">Schlagwörter <small class="muted">(kommagetrennt)</small></label>
-							<textarea class="span12" rows="6" name="page_keywords" id="input_page_keywords">'.htmlentities( $page->keywords, ENT_QUOTES, 'UTF-8' ).'</textarea>
-							'.$buttonSuggest.' '.$buttonBlacklist.'
-						</div>
-					</div>
-			<!--		<div class="row-fluid">
-						<div class="span6">
-							<label for="input_page_author">Autor</label>
-							<input class="span12" type="text" name="page_author" id="input_page_author" value="'.htmlentities( ""/*$page->author*/, ENT_QUOTES, 'UTF-8' ).'"/>
-						</div>
-						<div class="span6">
-							<label for="input_page_publisher">Herausgeber</label>
-							<input class="span12" type="text" name="page_publisher" id="input_page_publisher" value="'.htmlentities( ""/*$page->publisher*/, ENT_QUOTES, 'UTF-8' ).'"/>
-						</div>
-					</div>-->
 				</div>
 				<div class="span6">
 					<h4>Standardwerte</h4>
 					<div><small class="muted">Diese Werte wurden im Meta-Modul der Website definiert.</small></div>
-					<dl class="dl-horizontal" id="meta-defaults">
-						<dt class="meta-default" data-key="description">Beschreibung</dt>
-						<dd style="min-height: 8em;">'.$meta['default.description'].'</dd>
-						<dt class="meta-default" data-key="keywords">Schlagwörter</dt>
-						<dd style="min-height: 11em;">'.$meta['default.keywords'].'</dd>
-			<!--			<dt class="meta-default" data-key="author">Autor</dt>
-						<dd>'.$meta['default.author'].'</dd>
-						<dt class="meta-default" data-key="publisher">Herausgeber</dt>
-						<dd>'.$meta['default.publisher'].'</dd>-->
-					</dl>
 				</div>
 			</div>
-			<h3>Angaben für Suchmaschinen</h3>
+			<div id="meta-defaults" data-todo="refactor this element and its jQuery bindings">
+				<div class="row-fluid">
+					<div class="span6">
+						<label for="input_page_description">'.$wMeta->labelDescription.'</label>
+						<textarea class="span12" rows="4" name="page_description" id="input_page_description">'.htmlentities( $page->description, ENT_QUOTES, 'UTF-8' ).'</textarea>
+					</div>
+					<div class="span6">
+						<label for="input_page_description">'.$wMeta->labelDefaultDescription.'</label>
+						<textarea class="span12" rows="3" name="page_default_description" id="input_page_default_description">'.htmlentities( $meta['default.description']->value, ENT_QUOTES, 'UTF-8' ).'</textarea>
+						<button type="button" class="btn btn-small">save</button>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span6">
+						<label for="input_page_keywords">'.$wMeta->labelKeywords.'</label>
+						<textarea class="span12" rows="6" name="page_keywords" id="input_page_keywords">'.htmlentities( $page->keywords, ENT_QUOTES, 'UTF-8' ).'</textarea>
+						'.$buttonSuggest.' '.$buttonBlacklist.'
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span6">
+						<label for="input_page_description">'.$wMeta->labelDefaultKeywords.'</label>
+						<textarea class="span12" rows="3" name="page_default_keywords" id="input_page_default_keywords">'.htmlentities( $meta['default.keywords'], ENT_QUOTES, 'UTF-8' ).'</textarea>
+						<button type="button" class="btn btn-small">save</button>
+					</div>
+				</div>
+	<!--
+				<div class="row-fluid">
+					<div class="span6">
+						<label for="input_page_author">'.$wMeta->labelAuthor.'</label>
+						<input class="span12" type="text" name="page_author" id="input_page_author" value="'.htmlentities( ""/*$page->author*/, ENT_QUOTES, 'UTF-8' ).'"/>
+					</div>
+					<div class="span6">
+						<label for="input_page_author">'.$wMeta->labelDefaultAuthor.'</label>
+						<input class="span12" type="text" name="page_default_author" id="input_page_default_author" value="'.htmlentities( $meta['default.author'], ENT_QUOTES, 'UTF-8' ).'"/>
+						<button type="button" class="btn btn-small">save</button>
+					</div>
+				</div>
+
+				<div class="row-fluid">
+					<div class="span6">
+						<label for="input_page_publisher">'.$wMeta->labelPublisher.'</label>
+						<input class="span12" type="text" name="page_publisher" id="input_page_publisher" value="'.htmlentities( ""/*$page->publisher*/, ENT_QUOTES, 'UTF-8' ).'"/>
+					</div>
+					<div class="span6">
+						<label for="input_page_author">'.$wMeta->labelDefaultPublisher.'</label>
+						<input class="span12" type="text" name="page_default_publisher" id="input_page_default_publisher" value="'.htmlentities( $meta['default.publisher'], ENT_QUOTES, 'UTF-8' ).'"/>
+						<button type="button" class="btn btn-small">save</button>
+					</div>
+				</div>
+			</div>
+			<h3>'.$wMeta->headingSeo.'</h3>
 			<p>
 				<small class="muted">
 					Diese Angaben sind für die <a href="https://de.wikipedia.org/wiki/Webcrawler" target="_blank">Crawler</a> der Suchmaschinen bestimmt
@@ -74,16 +95,16 @@ return '
 			<br/>
 			<div class="row-fluid">
 				<div class="span3">
-					<label for="input_page_changefreq">'.$w->labelChangefreq.'</label>
+					<label for="input_page_changefreq">'.$wMeta->labelChangefreq.'</label>
 					<select name="page_changefreq" id="input_page_changefreq" class="span12">'.$optChangefreq.'</select>
 				</div>
 				<div class="span3">
-					<label for="input_page_priority">'.$w->labelPriority.'</label>
+					<label for="input_page_priority">'.$wMeta->labelPriority.'</label>
 					<select name="page_priority" id="input_page_priority" class="span12">'.$optPriority.'</select>
 				</div>
 			</div>
 			<div class="buttonbar">
-				<button type="submit" name="save" class="btn btn-primary"><i class="icon-ok icon-white"></i> '.$w->buttonSave.'</button>
+				<button type="submit" name="save" class="btn btn-primary"><i class="icon-ok icon-white"></i> '.$wMeta->buttonSave.'</button>
 			</div>
 		</form>
 	</div>
