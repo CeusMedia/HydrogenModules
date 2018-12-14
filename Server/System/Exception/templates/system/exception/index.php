553c54e6bc2f29e5cc1c5a562fd9c64b7f3d0f0c
Christian Würker <kriss@ceusmedia.de>
2018-12-14 11:31:17 +0100

    Add backlink and HTML text blocks.

--- Server/System/Exception/templates/system/exception/index.php
+++ Server/System/Exception/templates/system/exception/index.php
@@ -8,9 +8,12 @@ foreach( $words['facts'] as $key => $label ){
 		if( $key === "code" && (int) $exception->code === 0 )
 			continue;
 		if( $key === "trace" )
-			$exception->trace	= UI_HTML_Tag::create( 'kbd', nl2br( $exception->trace ) );
-		$facts[]	= UI_HTML_Tag::create( 'dt', $label );
-		$facts[]	= UI_HTML_Tag::create( 'dd', $exception->{$key} );
+			continue;
+		if( $key === "file" )
+			$exception->file	= UI_HTML_Tag::create( 'small', $exception->file );
+//			$exception->trace	= UI_HTML_Tag::create( 'kbd', nl2br( $exception->trace ) );
+		$facts[]	= UI_HTML_Tag::create( 'dt', $label, array( 'class' => 'fact-'.$key ) );
+		$facts[]	= UI_HTML_Tag::create( 'dd', $exception->{$key}, array( 'class' => 'fact-'.$key ) );
 	}
 }
 $facts	= UI_HTML_Tag::create( 'dl', $facts, array( 'class' => 'dl-horizontal' ) );
@@ -23,6 +26,8 @@ if( isset( $exceptionUrl ) ){
 	) );
 }
 
+extract( $view->populateTexts( array( 'top', 'bottom' ), 'html/system/exception' ) );
+
 return '<h3>'.$words['index']['heading'].'</h3>
 '.$textTop.'
 <!--<p>'.$words['index']['heading'].'</p>-->
@@ -31,7 +36,10 @@ return '<h3>'.$words['index']['heading'].'</h3>
 </div>
 <hr/>
 <div id="exception-facts" style="display: none">
+	<h4>Fehlermeldung</h4>
 	'.$facts.'
+	<h4>Aufrufstapel</h4>
+	'.UI_HTML_Tag::create( 'kbd', nl2br( $exception->trace ), array( 'style' => 'font-size: 10px; letter-spacing: -0.25px; line-height: 12px;' ) ).'
 </div>
 <button type="button" id="exception-facts-trigger" onclick="showExceptionFacts();" class="btn btn-mini"><i class="fa fa-info-circle"></i> '.$words['index']['buttonMore'].'</button>
 <script>
@@ -39,5 +47,20 @@ function showExceptionFacts(){
 	jQuery("#exception-facts").show();
 	jQuery("#exception-facts-trigger").hide();
 }
-</script>'.$textBottom;
+</script>
+<style>
+#exception-facts dl dd {
+	margin-left: 120px;
+}
+#exception-facts dl dt {
+	width: 110px;
+}
+#exception-facts dl dd {
+	margin-left: 120px;
+}
+#exception-facts dl .fact-file,
+#exception-facts dl .fact-line {
+	font-size: 0.85em;
+}
+s</style>'.$textBottom;
 ?>
