59a36f79037f24298547397134540d5f930064ff
kriss0r <kriss@ceusmedia.de>
2022-08-06 00:30:49 +0200

    Add filters.

---
+++ Admin/Log/Exception/templates/admin/log/exception/index.list.php
@@ -0,0 +1,128 @@
+<?php
+
+$w	= (object) $words['index.list'];
+
+$iconView	= UI_HTML_Tag::create( 'i', '', array( 'class' => 'fa fa-eye' ) );
+$iconRemove	= UI_HTML_Tag::create( 'i', '', array( 'class' => 'fa fa-remove' ) );
+
+$from		= 'admin/log/exception'.($page ? '/'.$page : '' );
+
+$selectInstance	= '';
+if( count( $instances ) > 1 ){
+	$optInstance	= [];
+	foreach( $instances as $instanceKey => $instanceData )
+		$optInstance[$instanceKey]	= $instanceData->title;
+	$optInstance	= UI_HTML_Elements::Options( $optInstance, $currentInstance );
+	$selectInstance	= UI_HTML_Tag::create( 'select', $optInstance, array(
+		'oninput'	=> 'document.location.href = "./admin/log/exception/setInstance/" + jQuery(this).val();',
+		'class'		=> '',
+		'style'		=> 'width: 100%',
+	) );
+
+}
+
+$dropdown	= '';
+$table		= '<div class="muted"><em><small>No exceptions logged.</small></em></div>';
+if( $exceptions ){
+	$list	= [];
+	foreach( $exceptions as $nr => $exception ){
+//print_m($exception);die;
+		$exceptionEnv		= unserialize( $exception->env );
+		$exceptionRequest	= unserialize( $exception->request );
+
+		$link	= UI_HTML_Tag::create( 'a', $exception->message, array( 'href' => './admin/log/exception/view/'.$exception->exceptionId ) );
+		$date	= date( 'Y.m.d', $exception->createdAt );
+		$time	= date( 'H:i:s', $exception->createdAt );
+
+		$buttons	= UI_HTML_Tag::create( 'div', array(
+			UI_HTML_Tag::create( 'a', $iconView, array(
+				'class'	=> 'btn not-btn-mini btn-small not-btn-info',
+				'href'	=> './admin/log/exception/view/'.$exception->exceptionId
+			) ),
+			UI_HTML_Tag::create( 'a', $iconRemove, array(
+				'class'	=> 'btn not-btn-mini btn-small btn-danger',
+				'href'	=> './admin/log/exception/remove/'.$exception->exceptionId
+			) ),
+		), array( 'class' => 'btn-group' ) );
+
+		$checkbox		= UI_HTML_Tag::create( 'input', NULL, [
+			'type'		=> 'checkbox',
+			'class'		=> 'checkbox-item',
+			'data-id'	=> $exception->exceptionId,
+		] );
+
+		$requestPath	= '<small class="muted">'.htmlentities( $exceptionRequest->get( '__path' ), ENT_QUOTES, 'utf-8' ).'</small>';
+		$method			= 'CLI';
+		if( preg_match( '/Web/', $exceptionEnv['class'] ) )
+			$method		= $exceptionRequest->getMethod()->get();
+		$envClass		= preg_replace( '/^(CMF_Hydrogen_Environment_)/', '<small class="muted">\\1</small>', $exceptionEnv['class'] );
+		$exceptionClass	= preg_replace( '/Exception$/', '', $exception->type );
+		$typeClass		= '<small class="muted">'.$exceptionClass.'</small>';
+		$list[]			= UI_HTML_Tag::create( 'tr', array(
+			UI_HTML_Tag::create( 'td', $checkbox ),
+			UI_HTML_Tag::create( 'td', $link.'<br/>'.$method.' '.$requestPath, array( 'class' => 'autocut' ) ),
+//			UI_HTML_Tag::create( 'td', $envClass ),
+//			UI_HTML_Tag::create( 'td', '<small class="muted">'.$exceptionClass.'</small>' ),
+			UI_HTML_Tag::create( 'td', $typeClass.'<br/>'.$date.'&nbsp;<small class="muted">'.$time.'</small>' ),
+			UI_HTML_Tag::create( 'td', $buttons ),
+		) );
+	}
+
+
+	$checkboxAll	= UI_HTML_Tag::create( 'input', NULL, [
+		'type'	=> 'checkbox',
+		'id'	=> 'admin-log-exception-list-all-items-toggle',
+	] );
+
+	$heads	= UI_HTML_Elements::TableHeads( [
+		$checkboxAll,
+//		'',
+		'',
+		'',
+	] );
+
+	$colgroup	= UI_HTML_Elements::ColumnGroup( '20px', ''/*, '180px'*//*, '180px'*/, '150px', '100px' );
+	$thead		= UI_HTML_Tag::create( 'thead', $heads );
+	$tbody		= UI_HTML_Tag::create( 'tbody', $list );
+	$table		= UI_HTML_Tag::create( 'table', [$colgroup, $thead, $tbody], [
+		'class'	=> 'table table-striped table-condensed',
+		'style'	=> 'table-layout: fixed'
+	] );
+
+	if( count( $exceptions ) > 1 ){
+		$dropdownMenu	= UI_HTML_Tag::create( 'ul', [
+			UI_HTML_Tag::create( 'li',
+				UI_HTML_Tag::create( 'a', '<i class="fa fa-trash"></i> '.$w->buttonRemove, ['class' => '#', 'id' => 'action-button-remove'] )
+			),
+		], ['class' => 'dropdown-menu not-pull-right'] );
+
+		$dropdownToggle	= UI_HTML_Tag::create( 'button', $w->buttonAction.' <span class="caret"></span>', [
+			'type'		=> 'button',
+			'class'		=> 'btn dropdown-toggle',
+		], ['toggle' => 'dropdown'] );
+		$dropdown		= UI_HTML_Tag::create( 'div', [$dropdownToggle, $dropdownMenu], ['class' => 'btn-group dropup'] );
+	}
+}
+
+$pagination	= new \CeusMedia\Bootstrap\Nav\PageControl( './admin/log/exception', $page, ceil( $total / $limit ) );
+$pagination	= $pagination->render();
+
+return '
+<div class="content-panel" style="position: relative">
+	<div style="position: absolute; right: 1em; top: 0.65em; width: 150px;">
+		'.$selectInstance.'
+	</div>
+	<h3>Exceptions</h3>
+	<div class="content-panel-inner">
+		<form action="admin/log/exception/bulk" method="post" id="form-admin-log-exception">
+			<input type="hidden" name="type" id="input_type"/>
+			<input type="hidden" name="ids" id="input_ids"/>
+			<input type="hidden" name="from" value="'.$from.'"/>
+			'.$table.'
+			<div class="buttonbar">
+				'.$pagination.'
+				'.$dropdown.'
+			</div>
+		</form>
+	</div>
+</div>';
--- Admin/Log/Exception/templates/admin/log/exception/view.php
+++ Admin/Log/Exception/templates/admin/log/exception/view.php
@@ -3,9 +3,10 @@
 $iconCancel		= UI_HTML_Tag::create( 'i', '', array( 'class' => 'fa fa-fw fa-arrow-left' ) );
 $iconRemove		= UI_HTML_Tag::create( 'i', '', array( 'class' => 'fa fa-fw fa-remove' ) );
 
-print_m($exception);die;
+//print_m($exception);die;
 
-$file		= preg_replace( "/^".preg_quote( realpath( $env->uri ), '/' )."/", '.', $exception->file );
+$file		= preg_replace( "/^".preg_quote( realpath( $env->uri ), '/' )."/", './', $exception->file );
+$file		= preg_replace( "/^".preg_quote( $env->uri, '/' )."/", './', $exception->file );
 $date		= date( 'Y.m.d', $exception->createdAt );
 $time		= date( 'H:i:s', $exception->createdAt );
 
@@ -13,15 +14,26 @@ $xmpStyle	= 'overflow: auto; border: 1px solid gray; background-color: #EFEFEF; 
 
 if( isset( $exception->traceAsHtml ) )
 	$trace	= $exception->traceAsHtml;
-else if( isset( $exception->traceAsString ) )
-	$trace	= '<xmp style="'.$xmpStyle.'">'.$exception->traceAsString.'</xmp>';
-else
-	$trace	= '<xmp style="'.$xmpStyle.'">'.$exception->trace.'</xmp>';
+else if( isset( $exception->traceAsString ) ){
+	$trace	= $exception->traceAsString;
+	$trace	= preg_replace( "/ ".preg_quote( realpath( $env->uri ), '/' )."/s", ' ./', $trace );
+	$trace	= preg_replace( "/ ".preg_quote( $env->uri, '/' )."/s", ' ./', $trace );
+	$trace	= '<xmp style="'.$xmpStyle.'">'.$trace.'</xmp>';
+}
+else{
+	$trace	= $exception->trace;
+	$trace	= preg_replace( "/ ".preg_quote( realpath( $env->uri ), '/' )."/s", ' ./', $trace );
+	$trace	= preg_replace( "/ ".preg_quote( $env->uri, '/' )."/s", ' ./', $trace );
+	$trace	= '<xmp style="'.$xmpStyle.'">'.$trace.'</xmp>';
+}
 $sectionTrace	= UI_HTML_Tag::create( 'h4', 'Stack Trace' ).$trace;
 
 $exceptionEnv		= unserialize( $exception->env );
 $exceptionRequest	= unserialize( $exception->request );
 $exceptionSession	= unserialize( $exception->session );
+
+//print_m($exceptionRequest->getAll());die;
+
 
 $facts	= [];
 $facts['Message']	= '<big><strong>'.$exception->message.'</strong></big>';
@@ -29,7 +41,7 @@ if( (int) $exception->code != 0 )
 	$facts['Code']	= $exception->code;
 $facts['File (Line)']	= $file.' ('.$exception->line.')';
 $facts['Date (Time)']	= $date.' <small class="muted">('.$time.')</small>';
-$facts['Reqest Path']	= $exceptionRequest->get( '__path' );
+$facts['Reqest Path']	= $exceptionRequest->get( '__path' ).'&nbsp;';
 $facts['App Name']		= $exceptionEnv['appName'];
 $facts['Base URL']		= $exceptionEnv['url'];
 $facts['Environment']	= $exceptionEnv['class'];
@@ -45,7 +57,9 @@ $sectionRequest	= '';
 $sectionRequestHeaders	= '';
 if( $exceptionRequest ){
 	if( get_class( $exceptionRequest ) !== 'CLI_ArgumentParser' ){
-		$requestHeaders			= UI_HTML_Tag::create( 'xmp', $exceptionRequest->getHeaders()->render(), array( 'style' => $xmpStyle ) );
+		$methodLine				= 'Method: '.$exceptionRequest->getMethod()->get().PHP_EOL;
+		$lines					= $exceptionRequest->getHeaders()->render();
+		$requestHeaders			= UI_HTML_Tag::create( 'xmp', $methodLine.$lines, array( 'style' => $xmpStyle ) );
 		$sectionRequestHeaders	= UI_HTML_Tag::create( 'h4', 'Request Headers' ).$requestHeaders;
 	}
 	$sectionRequestData		= UI_HTML_Tag::create( 'h4', 'Request Data' ).renderMapTable( $exceptionRequest->getAll() );
@@ -62,14 +76,15 @@ if( $exceptionSession ){
 $sectionFile	= '';
 if( file_exists( $exception->file ) ){
 	$fileLines	= FS_File_Reader::loadArray( $exception->file );
+	$fileLines	= file( $exception->file );
 	$firstLine	= max( 0, $exception->line - 5 );
 	$fileLines	= array_slice( $fileLines, $firstLine, 11 );
 	$lines		= [];
 	foreach( $fileLines as $nr => $line ){
 		$lines[]	= UI_HTML_Tag::create( 'tr', array(
 			UI_HTML_Tag::create( 'th', $firstLine + $nr + 1 ),
-			UI_HTML_Tag::create( 'td', '<tt>'.$line.'</tt>' ),
-		) );
+			UI_HTML_Tag::create( 'td', '<tt>'.str_replace( "\t", "&nbsp;&nbsp;&nbsp;&nbsp;", $line ).'</tt>' ),
+		), ['class' => $nr === 5 ? 'warning' : ''] );
 	}
 	$tbody		= UI_HTML_Tag::create( 'tbody', $lines );
 	$lines		= UI_HTML_Tag::create( 'table', $tbody, array(
@@ -126,7 +141,7 @@ function renderMapTable( $map, $sort = TRUE ){
 			UI_HTML_Tag::create( 'td', $key ),
 			UI_HTML_Tag::create( 'td', $type, array( 'style' => 'text-align: right' ) ),
 //			UI_HTML_Tag::create( 'td', json_encode( $value ) ),
-			UI_HTML_Tag::create( 'td', stripslashes( trim( json_encode( $value ), '"' ) ) ),
+			UI_HTML_Tag::create( 'td', htmlentities( stripslashes( trim( json_encode( $value ), '"' ) ), ENT_QUOTES, 'utf-8' ) ),
 		) );
 	}
 	$colgroup		= UI_HTML_Elements::ColumnGroup( '40px', '35%', '7%', '' );
