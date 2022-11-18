5bca966c8cb617b9046bfa7b04b399f82bf1a848
kriss0r <kriss@ceusmedia.de>
2022-11-18 14:17:28 +0100

    Resource_Authentication_Backend_OAuth2: Migration to support CeusMedia:* 0.9.x

--- src/Resource/Authentication/Backend/OAuth2/classes/Controller/Auth/Oauth2.php
+++ src/Resource/Authentication/Backend/OAuth2/classes/Controller/Auth/Oauth2.php
@@ -1,27 +1,37 @@
 <?php
 
+use CeusMedia\Common\ADT\Collection\Dictionary;
 use CeusMedia\Common\Alg\Obj\Factory as ObjectFactory;
 use CeusMedia\Common\Net\HTTP\Cookie as HttpCookie;
+use CeusMedia\Common\Net\HTTP\Request as HttpRequest;
 use CeusMedia\Common\UI\HTML\Exception\Page as HtmlExceptionPage;
 use CeusMedia\HydrogenFramework\Controller;
+use CeusMedia\HydrogenFramework\Environment\Resource\Messenger;
 
 class Controller_Auth_Oauth2 extends Controller
 {
-	protected $config;
-	protected $session;
-	protected $request;
-	protected $cookie;
-	protected $messenger;
-	protected $modelProvider;
-	protected $modelRelation;
+	protected Dictionary $config;
+	protected HttpRequest $request;
+	protected HttpCookie $cookie;
+	protected Dictionary $session;
+	protected Messenger $messenger;
+	protected Model_Oauth_Provider $modelProvider;
+	protected Model_Oauth_User $modelRelation;
+	protected Logic_Authentication_Backend_Oauth2 $logic;
+	protected bool $useCsrf;
 
-	protected $scopes	= [
+	protected array $scopes	= [
 		'adam-paterson/oauth2-slack'	=> ['identity.basic'],
 		'stevenmaguire/oauth2-paypal'	=> ['openid', 'profile', 'email', 'phone', 'address'],
 		'omines/oauth2-gitlab'			=> ['read_user']
 	];
 
-	public function login( $providerId = NULL )
+	/**
+	 *	@param		string|NULL		$providerId
+	 *	@return		void
+	 *	@throws		ReflectionException
+	 */
+	public function login( ?string $providerId = NULL )
 	{
 		if( $this->session->has( 'auth_user_id' ) )
 			$this->redirectAfterLogin();
@@ -30,35 +40,35 @@ class Controller_Auth_Oauth2 extends Controller
 		$modelRole		= new Model_Role( $this->env );
 
 		$words		= $this->getWords();
-		$msgs		= (object) $words['login'];
+		$messages	= (object) $words['login'];
 
 
 		if( ( $error = $this->request->get( 'error' ) ) ){
 			$currentProviderId	= $this->session->get( 'oauth2_providerId' );
 			if( $currentProviderId ){
 				$provider	= $this->modelProvider->get( $currentProviderId );
-				$this->messenger->noteNotice( $msgs->msgErrorFailed, $provider->title );
+				$this->messenger->noteNotice( $messages->msgErrorFailed, $provider->title );
 			}
 			if( $from = $this->session->get( 'oauth2_from' ) )
 				$this->restart( $from );
-			$this->restart( 'auth/login', FALSE );
+			$this->restart( 'auth/login' );
 		}
 		if( ( $code = $this->request->get( 'code' ) ) ){
 			$currentProviderId	= $this->session->get( 'oauth2_providerId' );
 			$currentState		= $this->session->get( 'oauth2_state' );
 			if( !$currentProviderId || !$currentState ){
-				$this->messenger->noteFailure( $msgs->msgErrorOauthIncomplete );
+				$this->messenger->noteFailure( $messages->msgErrorOauthIncomplete );
 				if( $from = $this->session->get( 'oauth2_from' ) )
 					$this->restart( $from );
-				$this->restart( 'auth/login', FALSE );
+				$this->restart( 'auth/login' );
 			}
 			$provider	= $this->modelProvider->get( $currentProviderId );
 			if( $currentState !== $this->request->get( 'state' ) ){
 				$this->session->remove( 'oauth2_state' );
-				$this->messenger->noteFailure( $msgs->msgErrorOauthInvalid );
+				$this->messenger->noteFailure( $messages->msgErrorOauthInvalid );
 				if( $from = $this->session->get( 'oauth2_from' ) )
 					$this->restart( $from );
-				$this->restart( 'auth/login', FALSE );
+				$this->restart( 'auth/login' );
 			}
 			try{
 				$client	= $this->getProviderObject( $currentProviderId );
@@ -69,42 +79,42 @@ class Controller_Auth_Oauth2 extends Controller
 					'oauthId'			=> $user->getId(),
 				) );
 				if( !$relation ){
-					$this->messenger->noteError( $msgs->msgErrorNoUserAssigned, $provider->title );
+					$this->messenger->noteError( $messages->msgErrorNoUserAssigned, $provider->title );
 					if( $from = $this->session->get( 'oauth2_from' ) )
 						$this->restart( $from );
-					$this->restart( 'auth/login', FALSE );
+					$this->restart( 'auth/login' );
 				}
 				if( ( $user = $modelUser->get( $relation->localUserId ) ) ){
 					$payload	= [
 						'backend'	=> 'oauth2',
-						'username'	=> $user ? $user->username : $username,
+						'username'	=> $user ? $user->username : '',
 		//				'password'	=> $password,															//  disabled for security
 						'userId'	=> $user ? $user->userId : 0,
 					];
-					$result		= $this->callHook( 'Auth', 'checkBeforeLogin', $this, $payload );
+					$this->callHook( 'Auth', 'checkBeforeLogin', $this, $payload );
 					if( !$this->messenger->gotError() ){
 						$role			= $modelRole->get( $user->roleId );
 						$allowedRoles	= $this->env->getConfig()->get( 'module.resource_authentication_backend_local.login.roles' );
-						$allowedRoles	= explode( ',', $allowedRoles ? $allowedRoles : "*" );
+						$allowedRoles	= explode( ',', $allowedRoles ?: "*" );
 
 						if( !$role->access )
-							$this->messenger->noteError( $msgs->msgRoleLocked, $role->title );
+							$this->messenger->noteError( $messages->msgRoleLocked, $role->title );
 						else if( $allowedRoles !== ["*"] && !in_array( $user->roleId, $allowedRoles ) )
-							$this->messenger->noteError( $msgs->msgInvalidRole, $role->title );
+							$this->messenger->noteError( $messages->msgInvalidRole, $role->title );
 						else if( $user->status == 0 )
-							$this->messenger->noteError( $msgs->msgUserUnconfirmed );
+							$this->messenger->noteError( $messages->msgUserUnconfirmed );
 						else if( $user->status == -1 )
-							$this->messenger->noteError( $msgs->msgUserLocked );
+							$this->messenger->noteError( $messages->msgUserLocked );
 						else if( $user->status == -2 )
-							$this->messenger->noteError( $msgs->msgUserDisabled );
+							$this->messenger->noteError( $messages->msgUserDisabled );
 					}
 					if( $this->messenger->gotError() ){
 						if( $from = $this->session->get( 'oauth2_from' ) )
 							$this->restart( $from );
-						$this->restart( 'auth/login', FALSE );
+						$this->restart( 'auth/login' );
 					}
 
-					$this->messenger->noteSuccess( $msgs->msgSuccess, $provider->title );
+					$this->messenger->noteSuccess( $messages->msgSuccess, $provider->title );
 					$this->session->set( 'oauth2_token', $token );
 					$this->session->set( 'auth_user_id', $user->userId );
 					$this->session->set( 'auth_role_id', $user->roleId );
@@ -119,40 +129,43 @@ class Controller_Auth_Oauth2 extends Controller
 				}
 			}
 			catch( Exception $e ){
-				$this->messenger->noteError( $msgs->msgErrorException, $provider->title, $e->getMessage() );
+				$this->messenger->noteError( $messages->msgErrorException, $provider->title, $e->getMessage() );
 				if( $this->env->getLog()->logException( $e, $this ) )
-					$this->restart( 'auth/local/login', FALSE );
+					$this->restart( 'auth/local/login' );
 				HtmlExceptionPage::display( $e );
 				exit;
 			}
 		}
-		if( $providerId ){
+		if( NULL !== $providerId ){
 			if( $this->moduleConfig->get( 'loginMode' ) === 'tab' )
 				$this->session->set( 'auth_backend', 'Oauth2' );
 			$provider	= $this->modelProvider->get( $providerId );
 			if( !$provider ){
 				$this->messenger->noteError( 'Invalid OAuth2 provider ID.' );
-				$this->restart( 'auth/local/login', FALSE );
+				$this->restart( 'auth/local/login' );
 			}
 			$this->session->set( 'oauth2_providerId', $providerId );
 			$providerObject	= $this->getProviderObject( $providerId );
 			$scopes	= [$providerObject->getDefaultScopes];
-			if( trim( $provider->scopes ) )
+/*			if( trim( $provider->scopes ) )
 				foreach( preg_split( '/\s*,\s/', $provider->scopes ) as $scope )
 					if( strlen( trim( $scope ) ) )
 						if( !in_array( $scope, $scopes ) )
-							$scopes[]	= $scope;
-			$authUrl = $providerObject->getAuthorizationUrl( $scopes );
+							$scopes[]	= $scope;*/
+			$scopes		= [];
+			if( 0 !== strlen( trim( $provider->scopes ) ) )
+				$scopes	= preg_split( '/\s*,\s*/', $provider->scopes );
+			$authUrl	= $providerObject->getAuthorizationUrl( $scopes );
 			$this->session->set( 'oauth2_state', $providerObject->getState() );
 			$this->session->set( 'oauth2_from', $this->request->get( 'from' ) );
-			$this->restart( $authUrl, NULL, NULL, TRUE );
+			$this->restart( $authUrl, FALSE, NULL, TRUE );
 		}
 		$providers	= $this->modelProvider->getAll( [], ['rank' => 'ASC'] );
 		$this->addData( 'providers', $providers );
 		return;
 	}
 
-	public function logout()
+	public function logout( $redirectController = NULL, $redirectAction = NULL )
 	{
 		$this->session->remove( 'oauth2_token' );
 
@@ -181,29 +194,34 @@ class Controller_Auth_Oauth2 extends Controller
 		$this->redirectAfterLogout( $redirectController, $redirectAction );
 	}
 
-	public function register( $providerId = NULL )
+	/**
+	 *	@param		string|NULL		$providerId
+	 *	@return		void
+	 *	@throws		ReflectionException
+	 */
+	public function register( ?string $providerId = NULL )
 	{
 		$modelUser		= new Model_User( $this->env );
 
 		$words		= $this->getWords();
-		$msgs		= (object) $words['register'];
+		$messages	= (object) $words['register'];
 
 		if( ( $error = $this->request->get( 'error' ) ) ){
 			$this->messenger->noteError( $error );
-			$this->restart( 'auth/local/register', FALSE );
+			$this->restart( 'auth/local/register' );
 		}
 		if( ( $code = $this->request->get( 'code' ) ) ){
 			$currentProviderId	= $this->session->get( 'oauth2_providerId' );
 			$currentState		= $this->session->get( 'oauth2_state' );
 			if( !$currentProviderId || !$currentState ){
-				$this->messenger->noteFailure( $msgs->msgErrorOauthIncomplete );
-				$this->restart( 'auth/local/register', FALSE );
+				$this->messenger->noteFailure( $messages->msgErrorOauthIncomplete );
+				$this->restart( 'auth/local/register' );
 			}
 			$provider	= $this->modelProvider->get( $currentProviderId );
 			if( $currentState !== $this->request->get( 'state' ) ){
 				$this->session->remove( 'oauth2_state' );
-				$this->messenger->noteFailure( $msgs->msgErrorOauthInvalid );
-				$this->restart( 'auth/local/register', FALSE );
+				$this->messenger->noteFailure( $messages->msgErrorOauthInvalid );
+				$this->restart( 'auth/local/register' );
 			}
 			try{
 				$client		= $this->getProviderObject( $currentProviderId, 'auth/oauth2/register' );
@@ -216,8 +234,8 @@ class Controller_Auth_Oauth2 extends Controller
 					'oauthId'			=> $user->getId(),
 				);
 				if( $this->modelRelation->getByIndices( $indices ) ){
-					$this->messenger->noteError( $msgs->msgErrorUserAssigned, $provider->title );
-					$this->restart( 'auth/local/register', FALSE );
+					$this->messenger->noteError( $messages->msgErrorUserAssigned, $provider->title );
+					$this->restart( 'auth/local/register' );
 				}
 				$this->session->set( 'auth_register_oauth_provider_id', $currentProviderId );
 				$this->session->set( 'auth_register_oauth_provider', $provider );
@@ -227,10 +245,10 @@ class Controller_Auth_Oauth2 extends Controller
 				$data	= $this->retrieveOwnerDate( $provider, $user );
 				foreach( $data as $key => $value )
 					$this->session->set( 'auth_register_oauth_'.$key, $value );
-				$this->restart( 'auth/local/register', FALSE );
+				$this->restart( 'auth/local/register' );
 			}
 			catch( Exception $e ){
-				$this->messenger->noteError( $msgs->msgErrorException, $provider->title, $e->getMessage() );
+				$this->messenger->noteError( $messages->msgErrorException, $provider->title, $e->getMessage() );
 				$this->messenger->noteError( $e->getMessage() );
 				if( $this->env->getLog()->logException( $e, $this ) )
 					$this->restart( 'register', TRUE );
@@ -238,7 +256,7 @@ class Controller_Auth_Oauth2 extends Controller
 				exit;
 			}
 		}
-		if( $providerId ){
+		if( NULL !== $providerId ){
 			$provider	= $this->modelProvider->get( $providerId );
 			if( !$provider ){
 				$this->messenger->noteError( 'Invalid OAuth2 provider ID.' );
@@ -252,7 +270,7 @@ class Controller_Auth_Oauth2 extends Controller
 			$authUrl = $providerObject->getAuthorizationUrl( $scopes );
 
 			$this->session->set( 'oauth2_state', $providerObject->getState() );
-			$this->restart( $authUrl, NULL, NULL, TRUE );
+			$this->restart( $authUrl, FALSE, NULL, TRUE );
 		}
 		$providers	= $this->modelProvider->getAll( [], ['rank' => 'ASC'] );
 		$this->addData( 'providers', $providers );
@@ -278,12 +296,17 @@ class Controller_Auth_Oauth2 extends Controller
 
 	/*  --  PROTECTED --  */
 
+	/**
+	 *	@return		void
+	 *	@throws		ReflectionException
+	 */
 	protected function __onInit(): void
 	{
 		$this->config		= $this->env->getConfig();
 		$this->request		= $this->env->getRequest();
 		$this->session		= $this->env->getSession();
 		$this->messenger	= $this->env->getMessenger();
+		$this->logic		= Logic_Authentication_Backend_Oauth2::getInstance( $this->env );
 		$this->cookie		= new HttpCookie( parse_url( $this->env->url, PHP_URL_PATH ) );
 		if( isset( $this->env->version ) )
 			if( version_compare( $this->env->version, '0.8.6.5', '>=' ) )
@@ -309,7 +332,13 @@ class Controller_Auth_Oauth2 extends Controller
 		return $provider;
 	}
 
-	protected function getProviderObject( $providerId, $redirectPath = 'auth/oauth2/login' )
+	/**
+	 *	@param		string		$providerId
+	 *	@param		string		$redirectPath
+	 *	@return		object
+	 *	@throws		ReflectionException
+	 */
+	protected function getProviderObject( string $providerId, string $redirectPath = 'auth/oauth2/login' ): object
 	{
 		$provider		= $this->modelProvider->get( $providerId );
 		if( !$provider )
@@ -340,23 +369,23 @@ class Controller_Auth_Oauth2 extends Controller
 	 */
 	protected function redirectAfterLogin( $controller = NULL, $action = NULL )
 	{
-		if( $controller )																			//  a redirect contoller has been argumented
-			$this->restart( $controller.( $action ? '/'.$action : '' ) );							//  redirect to controller and action if given
-		$from	= $this->request->get( 'from' );													//  get redirect URL from request if set
+		if( $controller )																			//  a redirect controller has been argumented
+			$this->restart( $controller.( $action ? '/'.$action : '' ) );						//  redirect to controller and action if given
+		$from	= $this->request->get( 'from' );												//  get redirect URL from request if set
 //		if( !$from )
 //			$from	= $this->session->get( 'oauth2_from' ) );
 //		$this->session->remove( 'oauth2_from' ) );
-		$from	= !preg_match( "/auth\/logout/", $from ) ? $from : '';								//  exclude logout from redirect request
-		$from	= preg_replace( "/^index\/index\/?/", "", $from );									//  remove full index path from redirect request
-		$forwardPath	= $this->moduleConfig->get( 'login.forward.path' );							//  get forward path for this module
-		$forwardForce	= $this->moduleConfig->get( 'login.forward.force' );						//  check if forwarding is forced
+		$from	= !preg_match( "/auth\/logout/", $from ) ? $from : '';						//  exclude logout from redirect request
+		$from	= preg_replace( "/^index\/index\/?/", "", $from );				//  remove full index path from redirect request
+		$forwardPath	= $this->moduleConfig->get( 'login.forward.path' );					//  get forward path for this module
+		$forwardForce	= $this->moduleConfig->get( 'login.forward.force' );					//  check if forwarding is forced
 		if( $forwardPath && $forwardForce )															//  forward path given and forced
-			$this->restart( $forwardPath.( $from ? '?from='.$from : '' ) );							//  redirect to forced forward path of this auth module
+			$this->restart( $forwardPath.( $from ? '?from='.$from : '' ) );						//  redirect to forced forward path of this auth module
 		if( $from )																					//  redirect target is given
-			$this->restart( 'auth?from='.$from );													//  carry redirect to base auth module dispatcher
+			$this->restart( 'auth?from='.$from );												//  carry redirect to base auth module dispatcher
 		if( $forwardPath )																			//  fallback: forward path given
 			$this->restart( $forwardPath );															//  redirect to forward path of this auth module
-		$this->restart( 'auth' );																	//  fallback: redirect to base auth module dispatcher
+		$this->restart( 'auth' );																//  fallback: redirect to base auth module dispatcher
 	}
 
 	/**
@@ -373,23 +402,23 @@ class Controller_Auth_Oauth2 extends Controller
 	 */
 	protected function redirectAfterLogout( $controller = NULL, $action = NULL )
 	{
-		if( $controller )																			//  a redirect contoller has been argumented
-			$this->restart( $controller.( $action ? '/'.$action : '' ) );							//  redirect to controller and action if given
-		$from	= $this->request->get( 'from' );													//  get redirect URL from request if set
+		if( $controller )																			//  a redirect controller has been argumented
+			$this->restart( $controller.( $action ? '/'.$action : '' ) );						//  redirect to controller and action if given
+		$from	= $this->request->get( 'from' );												//  get redirect URL from request if set
 //		$from	= !preg_match( "/auth\/logout/", $from ) ? $from : '';								//  exclude logout from redirect request
-		$from	= preg_replace( "/^index\/index\/?/", "", $from );									//  remove full index path from redirect request
-		$forwardPath	= $this->moduleConfig->get( 'logout.forward.path' );						//  get forward path for this module
-		$forwardForce	= $this->moduleConfig->get( 'logout.forward.force' );						//  check if forwarding is forced
+		$from	= preg_replace( "/^index\/index\/?/", "", $from );				//  remove full index path from redirect request
+		$forwardPath	= $this->moduleConfig->get( 'logout.forward.path' );					//  get forward path for this module
+		$forwardForce	= $this->moduleConfig->get( 'logout.forward.force' );					//  check if forwarding is forced
 		if( $forwardPath && $forwardForce )															//  forward path given and forced
-			$this->restart( $forwardPath.( $from ? '?from='.$from : '' ) );							//  redirect to forced forward path of this auth module
+			$this->restart( $forwardPath.( $from ? '?from='.$from : '' ) );						//  redirect to forced forward path of this auth module
 		if( $from )																					//  redirect target is given
-			$this->restart( 'auth?from='.$from );													//  carry redirect to base auth module dispatcher
+			$this->restart( 'auth?from='.$from );												//  carry redirect to base auth module dispatcher
 		if( $forwardPath )																			//  fallback: forward path given
 			$this->restart( $forwardPath );															//  redirect to forward path of this auth module
-		$this->restart( NULL );																		//  fallback: go to index (empty path)
+		$this->restart();																			//  fallback: go to index (empty path)
 	}
 
-	protected function refreshToken()
+	protected function refreshToken(): ?bool
 	{
 		$token	= $this->session->get( 'oauth2_token' );
 		if( !$token )
@@ -408,6 +437,7 @@ class Controller_Auth_Oauth2 extends Controller
 		catch( Exception $e ){
 			$this->messenger->noteFailure( $e->getMessage() );
 			$this->logout();
+			return FALSE;
 		}
 	}
 
