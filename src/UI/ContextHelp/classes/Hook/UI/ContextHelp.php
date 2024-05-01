f8feaa6a6acb495c69d3d85e2d2ed6172f0899f4
kriss0r <kriss@ceusmedia.de>
2024-05-01 12:11:46 +0200

    UI:ContextHelp: Extract hook to own class.

---
+++ src/UI/ContextHelp/classes/Hook/UI/ContextHelp.php
@@ -0,0 +1,53 @@
+<?php
+
+use CeusMedia\HydrogenFramework\Environment\Web as Environment;
+use CeusMedia\HydrogenFramework\Hook;
+use CeusMedia\HydrogenFramework\View;
+use PHPHtmlParser\Dom as DomParser;
+
+class Hook_UI_ContextHelp extends Hook
+{
+	public function onPageApplyModules(): void
+	{
+		$payload = ['path' => 'app.contexthelp'];
+		$this->env->getCaptain()->callHook( 'ContextHelp', 'register', $this->context, $payload );
+		$this->context->js->addSCriptOnReady( 'ContextHelp.prepare()' );
+	}
+
+	public function onRegisterContextHelp(): void
+	{
+		$data	= $this->getPayload() ?? [];
+		if( isset( $data['path'] ) ){
+			$this->registerFile( $this->env, $data['path'] );
+			return;
+		}
+		$controller	= $this->env->getRequest()->get( '__controller' );
+		$action		= $this->env->getRequest()->get( '__action' );
+		$this->registerFile( $this->env, $controller.'/contexthelp' );
+		$this->registerFile( $this->env, $controller.'/'.$action.'/contexthelp' );
+	}
+
+	public function registerFile( Environment $env, string $filePath ): void
+	{
+		$filePath	= 'html/'.$filePath.'.html';
+		$view		= new View( $env );
+		if( $view->hasContentFile( $filePath ) ){
+			$list	= [];
+			$html	= $view->loadContentFile( $filePath );
+			$dom	= new DomParser();
+			$dom->loadFromFile( $filePath );
+			$sections	= $dom->find( 'div.context-help' );
+			foreach( $sections as $section ){
+				$list[]	= array(
+					$section->getAttribute('id'),
+					$section->getAttribute('data-selector'),
+					$section->innerHtml
+				);
+			}
+			if( $list ){
+				$script	= 'ContextHelp.loadHelp('.json_encode( $list ).');';
+				$env->getPage()->js->addScript( $script );
+			}
+		}
+	}
+}
