4fb36496b37a8eeb2b5a21c3765c17641c0e835a
kriss0r <kriss@ceusmedia.de>
2024-08-22 10:48:20 +0200

    Moved Mangopay modules to nonfree modules.

--- src/Manage/My/Mangopay/templates/manage/my/mangopay/wallet/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$panel	= new View_Helper_Mangopay_List_Wallets_Big( $env );
-$panel->set( $wallets );
-$panel->setLink( 'manage/my/mangopay/wallet/view/%s' );
-//$panel->setFrom( $from );
-$panel->allowAdd( TRUE );
-return '<h2>Portmoney</h2>'.$panel;
