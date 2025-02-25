3f55a07d88166ab0f20f83d9244eef9bec309a12
kriss0r <kriss@ceusmedia.de>
2025-02-25 17:24:47 +0100

    Manage:Customer:Projects: Extract hook.

--- src/Manage/Customer/Projects/classes/View/Manage/Customer/Project.php
+++ src/Manage/Customer/Projects/classes/View/Manage/Customer/Project.php
@@ -8,9 +8,4 @@ class View_Manage_Customer_Project extends View
 	public function index()
 	{
 	}
-
-	public static function ___onRegisterTab( Environment $env, $context, $module, $data )
-	{
-		View_Manage_Customer::registerTab( 'project/'.$data['customerId'], '+Projekte', 5 );
-	}
 }
