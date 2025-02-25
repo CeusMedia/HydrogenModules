c5ec63b4929842bf28bd67956abb5779ed42042b
kriss0r <kriss@ceusmedia.de>
2025-02-25 18:19:56 +0100

    Work:Mail:Sync: Extract hook.

--- src/Work/Mail/Sync/classes/View/Work/Mail/Sync.php
+++ src/Work/Mail/Sync/classes/View/Work/Mail/Sync.php
@@ -5,9 +5,6 @@ use CeusMedia\HydrogenFramework\View;
 
 class View_Work_Mail_Sync extends View
 {
-	static public function ___onRegisterTab( Environment $env, $context, $module, $data = [] ){
-	}
-
 	public function add()
 	{
 	}
@@ -17,17 +14,17 @@ class View_Work_Mail_Sync extends View
 		$this->env->getPage()->js->addScriptOnReady( 'WorkMailSync.init()' );
 	}
 
-	public function editSync()
+	public function editSync(): void
 	{
 		$this->env->getPage()->js->addScriptOnReady( 'WorkMailSync.init()' );
 	}
 
-	public function addHost()
+	public function addHost(): void
 	{
 		$this->env->getPage()->js->addScriptOnReady( 'WorkMailSync.init()' );
 	}
 
-	public function index()
+	public function index(): void
 	{
 	}
 }
