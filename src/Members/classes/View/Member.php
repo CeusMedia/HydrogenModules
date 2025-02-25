818d2cacaadd5f6d68546cc405335340f75dfc68
kriss0r <kriss@ceusmedia.de>
2025-02-25 17:24:30 +0100

    Members: Extract hook.

--- src/Members/classes/View/Member.php
+++ src/Members/classes/View/Member.php
@@ -5,13 +5,6 @@ use CeusMedia\HydrogenFramework\View;
 
 class View_Member extends View
 {
-	public static function ___onRegisterTab( Environment $env, $context, $module, $payload )
-	{
-		$words	= (object) $env->getLanguage()->getWords( 'member' );			//  load words
-		$context->registerTab( '', $words->tabs['index'], 0 );					//  register main tab
-		$context->registerTab( 'search', $words->tabs['search'], 1 );			//  register main tab
-	}
-
 	public static function renderTabs( Environment $env, $current = 0 )
 	{
 		$tabs	= new View_Helper_Navigation_Bootstrap_Tabs( $env );
