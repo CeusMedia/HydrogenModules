5f6c0020fbedc6bff37475b2301bf3a8b89c089f
kriss0r <kriss@ceusmedia.de>
2025-03-07 02:36:57 +0100

    Server:IP:Lock: Use model constants.

--- src/Server/IP/Lock/classes/Entity/Server/IP/Lock/Filter.php
+++ src/Server/IP/Lock/classes/Entity/Server/IP/Lock/Filter.php
@@ -8,7 +8,7 @@ class Entity_Server_IP_Lock_Filter extends Entity
 	public int|string $ipLockFilterId;
 	public int|string $reasonId;
 	public int $status;
-	public int $lockStatus;
+	public int $lockStatus			= Model_IP_Lock_Filter::LOCK_STATUS_IMMEDIATE;
 	public ?string $method			= NULL;
 	public string $pattern;
 	public int $createdAt			= 0;
--- src/Server/IP/Lock/classes/Model/IP/Lock/Filter.php
+++ src/Server/IP/Lock/classes/Model/IP/Lock/Filter.php
@@ -12,6 +12,13 @@ class Model_IP_Lock_Filter extends Model
 		self::STATUS_ENABLED,
 	];
 
+	public const LOCK_STATUS_REQUEST	= 0;
+	public const LOCK_STATUS_IMMEDIATE	= 1;
+	public const LOCK_STATUSES		= [
+		self::LOCK_STATUS_REQUEST,
+		self::LOCK_STATUS_IMMEDIATE,
+	];
+
 	protected string $name			= 'ip_lock_filters';
 
 	protected array $columns		= [
