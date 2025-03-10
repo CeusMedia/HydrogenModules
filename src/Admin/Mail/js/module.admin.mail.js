73b1fd662022762cc819295198d36ef1c0328ead
kriss0r <kriss@ceusmedia.de>
2025-03-10 13:34:41 +0100

    Fix bug.

--- src/Admin/Mail/js/module.admin.mail.js
+++ src/Admin/Mail/js/module.admin.mail.js
@@ -1,17 +1,17 @@
-var ModuleAdminMail	= {};
+let ModuleAdminMail	= {};
 ModuleAdminMail.Queue = {
 	init: function(){
-		var form = jQuery('#form-admin-mail-queue');
-		var master = form.find('#admin-mail-queue-list-all-items-toggle');
-		var boxes = form.find('.checkbox-mail');
+		let form = jQuery('#form-admin-mail-queue');
+		let master = form.find('#admin-mail-queue-list-all-items-toggle');
+		let boxes = form.find('.checkbox-mail');
 		master.on('input', function(){
-			var value = jQuery(this).prop('checked');
+			let value = jQuery(this).prop('checked');
 			boxes.prop('checked', value);
 		});
 		form.find('#action-button-remove').on('click', function(){
-			var ids = [];
+			let ids = [];
 			boxes.each(function(){
-				var box = jQuery(this);
+				let box = jQuery(this);
 				if(box.prop('checked'))
 					ids.push(box.data('id'));
 			});
@@ -26,23 +26,23 @@ ModuleAdminMail.TemplateEditor	= {
 	init: function(templateId){
 		this.templateId = templateId;
 		ModuleAce.verbose = false;
-		var reloadFrameBySelector = function(selector){
+		let reloadFrameBySelector = function(selector){
 			jQuery(selector).each(function(){
 				if(this.nodeName.toLowerCase() === 'iframe' )
 					this.contentWindow.location.reload();
 			});
 		};
-		var onHtmlUpdate	= function(chance){
+		let onHtmlUpdate	= function(chance){
 			reloadFrameBySelector('.template-preview-iframe-container iframe');
 			reloadFrameBySelector("#modal-admin-mail-template-preview-html iframe");
 			reloadFrameBySelector("#frame-template-preview");
 		};
-		var onTextUpdate	= function(chance){
+		let onTextUpdate	= function(chance){
 			reloadFrameBySelector('.template-preview-iframe-container iframe');
 			reloadFrameBySelector("#modal-admin-mail-template-preview-text iframe");
 			reloadFrameBySelector("#frame-template-preview");
 		};
-		var onStyleUpdate	= function(chance){
+		let onStyleUpdate	= function(chance){
 			reloadFrameBySelector('.template-preview-iframe-container iframe');
 			reloadFrameBySelector("#modal-admin-mail-template-preview-html iframe");
 			reloadFrameBySelector("#frame-template-preview");
@@ -63,7 +63,7 @@ ModuleAdminMail.TemplateEditor	= {
 			{callbacks: {update: onStyleUpdate}}
 		);
 		jQuery("#admin-mail-template-edit li a").on("click", function(){
-			var targetUrl	= jQuery(this).attr("href").substr(1);
+			let targetUrl	= jQuery(this).attr("href").substr(1);
 			jQuery.ajax({
 				url: "./ajax/admin/mail/template/setTab/"+targetUrl
 			});
