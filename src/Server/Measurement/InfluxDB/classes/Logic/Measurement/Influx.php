1e6abcb1a69b22a4837a0b0457c35ff662e27fbc
kriss0r <kriss@ceusmedia.de>
2025-04-04 02:57:18 +0200

    Server:Measurement:InfluxDB: Add default tags.

--- src/Server/Measurement/InfluxDB/classes/Logic/Measurement/Influx.php
+++ src/Server/Measurement/InfluxDB/classes/Logic/Measurement/Influx.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
+use CeusMedia\Common\Env as CommonEnv;
 use CeusMedia\HydrogenFramework\Environment;
 use CeusMedia\HydrogenFramework\Environment\Resource\Module\Definition as ModuleDefinition;
+use CeusMedia\HydrogenFramework\Environment\Web as WebEnvironment;
 use CeusMedia\HydrogenFramework\Logic\Shared as SharedLogic;
 use InfluxDB2\Client as InfluxClient;
 use InfluxDB2\WriteApi as InfluxWriteApi;
 use InfluxDB2\Model\WritePrecision as InfluxClientWritePrecision;
-use InfluxDB2\Point as InfluxPoint;
 
 class Logic_Measurement_Influx extends SharedLogic
 {
@@ -15,6 +16,7 @@ class Logic_Measurement_Influx extends SharedLogic
 	protected ?InfluxWriteApi $writeApi	= NULL;
 	protected string $bucket;
 	protected string $org;
+	protected array $tags		= [];
 
 	public function __onInit(): void
 	{
@@ -37,6 +39,7 @@ class Logic_Measurement_Influx extends SharedLogic
 		];
 
 		$this->client	= new InfluxClient( $connectionData );
+		$this->setDefaultTags();
 	}
 
 	public function write( $measurement, array $tags = [], array $fields = [] ): bool
@@ -45,7 +48,7 @@ class Logic_Measurement_Influx extends SharedLogic
 			$this->writeApi = $this->client->createWriteApi();
 
 		$line	= $measurement;
-		foreach( $tags as $key => $value )
+		foreach( array_merge( $this->tags, $tags ) as $key => $value )
 			$line	.= ','.$key.'='.$value;
 
 		$line	.= ' ';
@@ -54,7 +57,57 @@ class Logic_Measurement_Influx extends SharedLogic
 			$list[]	= $key.'='.$value;
 		$line	.= ' '.join( ',', $list );
 
-		$this->writeApi->write( $line, InfluxClientWritePrecision::S, $this->bucket, $this->org);
+		$this->writeApi->write( $line, InfluxClientWritePrecision::S, $this->bucket, $this->org );
 		return TRUE;
+	}
+
+	/**
+	 *	Collects a map of default tags.
+	 *	Contains:
+	 *		- host: Host name used by web application
+	 *		- name: App name
+	 *		- version: App version, if set in config.ini
+	 *		- hydrogen: Framework version
+	 *		- mode: Environment run mode (live,stage,test,dev,unknown)
+	 * 		- method: CLI,GET,POST,PUT,DELETE,...
+	 *
+	 *	This map is used to be set on logic construction.
+	 *	@return		array<string,?string>
+	 */
+	protected function detectDefaultTags(): array
+	{
+		$tags	= [
+			'host'		=> parse_url( $this->env->url, PHP_URL_HOST ),
+			'name'		=> $this->env->getConfig()->get( 'app.name' ),
+			'version'	=> $this->env->getConfig()->get( 'app.version' ),
+			'hydrogen'	=> $this->env->version,
+			'mode'		=> match( $this->env->getMode() ){
+				Environment::MODE_UNKNOWN	=> 'unknown',
+				Environment::MODE_DEV		=> 'dev',
+				Environment::MODE_TEST		=> 'test',
+				Environment::MODE_STAGE		=> 'stage',
+				Environment::MODE_LIVE		=> 'live',
+			},
+		];
+		if( CommonEnv::isCli() )
+			$tags['method']	= 'CLI';
+		else if( $this->env instanceof WebEnvironment )
+			$tags['method']	= $this->env->getRequest()->getMethod()->get();
+		return $tags;
+	}
+
+	/**
+	 *	Sets several tags to be attached to every measurement package.
+	 *	@param		bool		$reset		Flag: clear tags, default: yes
+	 *	@return		void
+	 */
+	protected function setDefaultTags( bool $reset = TRUE ): void
+	{
+		$defaultTags = $this->detectDefaultTags();
+		if( $reset )
+			$this->tags	= $defaultTags;
+		else
+			foreach( $defaultTags as $key => $value )
+				$this->tags[$key]	= $value;
 	}
 }
