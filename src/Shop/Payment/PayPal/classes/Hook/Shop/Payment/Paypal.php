9d7c075d67fe75a8e559d869f5edcf2d2693fa6a
kriss0r <kriss@ceusmedia.de>
2025-05-26 04:54:34 +0200

    Shop:Payment:*: Update hooks.

--- src/Shop/Payment/PayPal/classes/Hook/Shop/Payment/Paypal.php
+++ src/Shop/Payment/PayPal/classes/Hook/Shop/Payment/Paypal.php
@@ -1,35 +1,31 @@
 <?php
 
+use CeusMedia\Common\ADT\Collection\Dictionary;
 use CeusMedia\Common\Alg\Obj\Factory as ObjectFactory;
-use CeusMedia\HydrogenFramework\Environment;
 use CeusMedia\HydrogenFramework\Hook;
 
 class Hook_Shop_Payment_Paypal extends Hook
 {
 	/**
 	 *	...
-	 *	@static
 	 *	@access		public
-	 *	@param		Environment		$env		Environment instance
-	 *	@param		object			$context	Hook context object
-	 *	@param		object			$module		Module object
-	 *	@param		array			$payload	Map of hook arguments
 	 *	@return		void
 	 */
-	public static function onRegisterShopPaymentBackends( Environment $env, object $context, object $module, array & $payload ): void
+	public function onRegisterShopPaymentBackends(): void
 	{
-		$methods	= $env->getConfig()->getAll( 'module.shop_payment_paypal.method.', TRUE );
-		$words		= $env->getLanguage()->getWords( 'shop/payment/paypal' );
+		$payload	= $this->getPayload() ?? [];
+
+		$methods	= $this->env->getConfig()->getAll( 'module.shop_payment_paypal.method.', TRUE );
+		$words		= $this->env->getLanguage()->getWords( 'shop/payment/paypal' );
 		$labels		= (object) $words['payment-methods'];
 		$descs		= (object) ( $words['payment-method-descriptions'] ?? [] );
 		/** @var Model_Shop_Payment_BackendRegister $register */
-		$register	= $payload['register'] ?? new Model_Shop_Payment_BackendRegister( $env );
+		$register	= $payload['register'] ?? new Model_Shop_Payment_BackendRegister( $this->env );
 
 		if( $methods->get( 'Express.active', FALSE ) ){
 			$priority	= $methods->get( 'Express.priority', 0 );
 			if( 0 !== $priority ){
 				$method		= $methods->getAll( 'Express.', TRUE );
-
 				$register->addEntity( Entity_Shop_Payment_Backend::fromArray( [
 					'backend'		=> 'Paypal',								//  backend class name
 					'key'			=> 'PayPal:Express',						//  payment method key
@@ -44,26 +40,45 @@ class Hook_Shop_Payment_Paypal extends Hook
 			}
 		}
 		$payload['register']	= $register;
+		$this->setPayload( $payload );
 	}
 
 	/**
-	 *	...
+	 *	Hook to register panel for final shop screen.
 	 *	@access		public
 	 *	@return		void
+	 *	@throws		RuntimeException		if payload is missing orderId
+	 *	@throws		RuntimeException		if payload is missing paymentBackends
+	 *	@throws		RuntimeException		if paymentBackends is not of Model_Shop_Payment_BackendRegister
+	 *	@throws		RuntimeException		if orderId is invalid
+	 *	@throws		ReflectionException
+	 *	@throws		\Psr\SimpleCache\InvalidArgumentException
 	 */
 	public function onRenderServicePanels(): void
 	{
-		$payload	= $this->getPayload();
-		if( empty( $payload['orderId'] ) || empty( $payload['paymentBackends']->getAll() ) )
+		$payload	= new Dictionary( $this->getPayload() ?? [] );
+		if( !$payload->has( 'orderId' ) )
+			throw new RuntimeException( 'No order ID set in payload' );
+		if( !$payload->has( 'paymentBackends' ) )
+			throw new RuntimeException( 'No payload backends in payload' );
+
+		$backendRegistry	= $payload->get( 'paymentBackends' );
+		if( !$backendRegistry instanceof Model_Shop_Payment_BackendRegister )
+			throw new RuntimeException( 'Payload must have paymentBackends by Model_Shop_Payment_BackendRegister' );
+
+		$paymentBackends	= $backendRegistry->getAll();
+		if( [] === $paymentBackends )
 			return;
-		$model	= new Model_Shop_Order( $this->env );
-		$order	= $model->get( $payload['orderId'] );
-		foreach( $payload['paymentBackends']->getAll() as $backend ){
+
+		$model		= new Model_Shop_Order( $this->env );
+		$orderId	= $payload->get( 'orderId' );
+		$order		= $model->get( $orderId );
+		foreach( $paymentBackends->getAll() as $backend ){
 			if( $backend->key === $order->paymentMethod ){
 				$className	= 'View_Helper_Shop_FinishPanel_'.$backend->backend;
 				if( class_exists( $className ) ){
 					$object	= ObjectFactory::createObject( $className, [$this->env] );
-					$object->setOrderId( $payload['orderId'] );
+					$object->setOrderId( $orderId );
 					$object->setOutputFormat( $className::OUTPUT_FORMAT_HTML );
 					$panelPayment	= $object->render();
 					$this->context->registerServicePanel( 'ShopPaymentPaypal', $panelPayment, 2 );
