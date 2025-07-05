ea852d5560fc59fbb0d95d27203a4edafa237e45
kriss0r <kriss@ceusmedia.de>
2025-07-05 04:37:26 +0200

    Work:Mail:Group: Update.

--- src/Work/Mail/Group/classes/Hook/Work/Mail/Group.php
+++ src/Work/Mail/Group/classes/Hook/Work/Mail/Group.php
@@ -5,13 +5,13 @@ use CeusMedia\HydrogenFramework\Hook;
 
 class Hook_Work_Mail_Group extends Hook
 {
-	public static function onRegisterTab( Environment $env, object $context, object $module, array & $payload )
+	public function onRegisterTab(): void
 	{
-		$words	= (object) $env->getLanguage()->getWords( 'work/mail/group' );			//  load words
-		$context->registerTab( '', $words->tabs['group'], 0 );									//  register main tab
-	//	$context->registerTab( 'member', $words->tabs['members'], 1 );							//  register members tab
-		$context->registerTab( 'server', $words->tabs['servers'], 2 );							//  register servers tab
-		$context->registerTab( 'role', $words->tabs['roles'], 3 );								//  register roles tab
-		$context->registerTab( 'message', $words->tabs['messages'], 4 );						//  register messages tab
+		$words	= (object) $this->env->getLanguage()->getWords( 'work/mail/group' );			//  load words
+		$this->context->registerTab( '', $words->tabs['group'], 0 );								//  register main tab
+	//	$this->context->registerTab( 'member', $words->tabs['members'], 1 );						//  register members tab
+		$this->context->registerTab( 'server', $words->tabs['servers'], 2 );						//  register servers tab
+		$this->context->registerTab( 'role', $words->tabs['roles'], 3 );							//  register roles tab
+		$this->context->registerTab( 'message', $words->tabs['messages'], 4 );						//  register messages tab
 	}
 }
