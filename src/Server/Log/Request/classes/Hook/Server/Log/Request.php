bb3b3d16c89b373dfe0457ae7547bf26b1b53685
kriss0r <kriss@ceusmedia.de>
2025-07-07 02:48:50 +0200

    Server:Log:Request: 0.2.2: Add logic class.

--- src/Server/Log/Request/classes/Hook/Server/Log/Request.php
+++ src/Server/Log/Request/classes/Hook/Server/Log/Request.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use CeusMedia\Common\Net\HTTP\Header\Field as HeaderField;
 use CeusMedia\HydrogenFramework\Environment\Resource\Module\Definition as ModuleDefinition;
 use CeusMedia\HydrogenFramework\Hook;
 
@@ -18,55 +17,11 @@ class Hook_Server_Log_Request extends Hook
 			return;
 
 		try{
-			$data	= $this->collectData();
-			$model	= new Model_Log_Request( $this->env );
-			$model->add( $data );
+			$logic	= Logic_Server_Log_Request::getInstance( $this->env );
+			$logic->logCurrentRequest();
 		}
 		catch( Throwable $e ){
 			$this->env->getLog()->logException( $e );
 		}
-	}
-
-	/**
-	 *	@return		Entity_Log_Request
-	 */
-	protected function collectData(): Entity_Log_Request
-	{
-		$ip			= NULL;
-		$sessionId	= NULL;
-		$method		= 'CLI';
-		$url		= NULL;
-		$cookie		= NULL;
-		$session	= NULL;
-		$headers	= NULL;
-
-		if( !CeusMedia\Common\Env::isCli() ){
-			$ip			= getenv( 'REMOTE_ADDR' );
-			$sessionId	= $this->env->getSession()->getSessionID();
-			$method		= getenv( 'REQUEST_METHOD' );
-			$url		= substr( getenv( 'REQUEST_URI' ), 0, 255 );
-			if( $this->env->has( 'cookie' ) )
-				$cookie		= $this->env->get( 'cookie' )->getAll();
-			$session	= $this->env->get( 'session' )->getAll();
-			$headers	= array_map( static function( HeaderField $field ){
-				return $field->toString();
-			}, $this->env->getRequest()->getHeaders()->getFields() );
-		}
-
-		$date	= DateTime::createFromFormat( 'U.u', (string) microtime( TRUE ) );
-
-		return Entity_Log_Request::fromArray( [
-			'ip'		=> $ip,
-			'sessionId'	=> $sessionId,
-			'method'	=> $method,
-			'url'		=> $url,
-			'request'	=> json_encode( $this->env->getRequest()->getAll() ),
-			'session'	=> json_encode( $session ),
-			'cookie'	=> json_encode( $cookie ),
-			'headers'	=> json_encode( $headers ),
-			'referer'	=> $_SERVER['HTTP_REFERER'] ?? '',
-			'userAgent'	=> $_SERVER['HTTP_USER_AGENT'] ?? '',
-			'timestamp'	=> $date->format( 'Y-m-d H:i:s.u' ),
-		] );
 	}
 }
