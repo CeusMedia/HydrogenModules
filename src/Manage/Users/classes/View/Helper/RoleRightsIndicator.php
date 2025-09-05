cb15e0f26f3031358b1bcea3d68d80cec413cc06
kriss0r <kriss@ceusmedia.de>
2025-09-05 03:09:07 +0200

    Manage:Users: 0.8.2: Add role rights indicator on index.

---
+++ src/Manage/Users/classes/View/Helper/RoleRightsIndicator.php
@@ -0,0 +1,42 @@
+<?php
+use CeusMedia\HydrogenFramework\Environment\Web as WebEnvironment;
+use CeusMedia\Common\UI\HTML\Indicator;
+class View_Helper_RoleRightsIndicator
+{
+	/** @var WebEnvironment $env */
+	protected WebEnvironment $env;
+	protected int $max				= 0;
+	protected int|string $roleId	= 0;
+	protected array $rights			= [];
+
+	public function __construct( WebEnvironment $env ){
+		$this->env	= $env;
+	}
+
+	public function setRoles( array $roles ): self
+	{
+		$max	= 0;
+		$this->rights	= [];
+
+		/** @var Entity_Role $role */
+		foreach( $roles as $role ){
+			$rights	= $this->env->getAcl()->index( NULL, $role->roleId );
+			$count	= array_sum( array_map( 'count', $rights ) );
+			$this->rights[$role->roleId]	= $count;
+			$max	= max( $max, $count );
+		}
+		$this->max	= $max;
+		return $this;
+	}
+
+	public function setRoleId( int|string $roleId ): self
+	{
+		$this->roleId	= $roleId;
+		return $this;
+	}
+
+	public function render(): string
+	{
+		return Indicator::render( $this->rights[$this->roleId], $this->max );
+	}
+}
