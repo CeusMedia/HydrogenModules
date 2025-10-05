024518c58b7f0199de054eaaed656495f49d5f34
kriss0r <kriss@ceusmedia.de>
2025-10-05 13:28:58 +0200

    Work:Meeting: 0.2.3: Finish implementation of prototype.

--- src/Work/Meetings/classes/Job/Work/Meeting.php
+++ src/Work/Meetings/classes/Job/Work/Meeting.php
@@ -1,38 +1,80 @@
 <?php
 class Job_Work_Meeting extends Job_Abstract
 {
-	public function remind()
+	public function close(): int
+	{
+		$model		= new Model_Work_Meeting( $this->env );
+		/** @var Entity_Work_Meeting[] $meetings */
+		$meetings	= $model->getAll( [
+			'status'	=> Model_Work_Meeting::STATUS_ACTIVE,
+			'dateEnd'	=> '<= '.date( 'Y-m-d H:i:s' ),
+		] );
+		foreach( $meetings as $meeting ){
+			$model->edit( $meeting->meetingId, [
+				'jobScheduleId'	=> 0,
+				'status'		=> Model_Work_Meeting::STATUS_DONE,
+				'modifiedAt'	=> time(),
+			] );
+			if( 0 !== $meeting->jobScheduleId ){
+				$model	= Model_Job_Schedule::getInstance( $this->env );
+				$model->remove( $meeting->jobScheduleId );
+			}
+		}
+		return count( $meetings );
+	}
+
+	public function remind(): void
 	{
 		$model	= new Model_Work_Meeting( $this->env );
 
-		//  STRATEGY 1: PDO query mode with date operations in query.
-		/** @var Resource_Database $dbc */
-		$dbc	= $this->env->getDatabase();
-		$date	= date( 'Y-m-d H:i:s' );
-		$query	= 'SELECT * FROM '.$model->getName()." WHERE at BETWEEN '".$date."' AND DATE_ADD('".$date."', INTERVAL 1 HOUR)";
-		$dbc->getConnection()->exec( $query );
+		$id		= (int) $this->parameters->get( 'id', 0 );
+		if( 0 !== $id ){
+			/** @var ?Entity_Work_Meeting $meeting */
+			$meeting	= $model->get( $id );
+			if( NULL === $meeting ){
+				$this->out( 'Error: Invalid ID' );
+				$this->logError( 'Error: Invalid ID' );
+				return;
+			}
+			$this->out( 'Sending reminder for: '.$meeting->title );
+			$this->sendMeetingReminder( $meeting );
+		}
+		else{
+			$datetime	= new Datetime( 'now' );
+			$target		= $datetime->add( new DateInterval( 'PT1H' ) );
+			if( $this->verbose )
+				$this->out( 'Target DateTime: '.$target->format( 'Y-m-d H:i' ).':00' );
+			$conditions	= [
+				'status'	=> Model_Work_Meeting::STATUS_ACTIVE,
+				'dateStart'	=> $target->format('Y-m-d H:i' ).':00'
+			];
+			$meetings	= $model->getAll( $conditions );
+			foreach ( $meetings as $meeting ){
+				$this->out( 'Sending reminder for: '.$meeting->title );
+				$this->sendMeetingReminder( $meeting );
+			}
+		}
+	}
 
+	protected function sendMeetingReminder( Entity_Work_Meeting $meeting ): int
+	{
+		$model		= new Model_Work_Meeting_Participant( $this->env );
+		$logicMail	= Logic_Mail::getInstance( $this->env );
+		$logicUser	= Logic_User::getInstance( $this->env );
 
-
-		$orders	= [];
-		$limits	= [];
-
-		//  STRATEGY 2: Table model query mode with exact target time, MUST run every minute.
-		$date	= DateTime::createFromFormat( 'Y-m-d H:i:s', $date );
-		$target	= $date->add( new DateInterval( 'PT1H' ) );
-		$conditions	= [
-			'date'	=> $target->format('Y-m-d H:i' ).':00'
-		];
-		$model->getAll( $conditions, $orders, $limits );
-
-		//  STRATEGY 3: Table model query mode with AND (EXPERIMENTAL)
-		$date	= DateTime::createFromFormat( 'Y-m-d H:i:s', $date );
-		$before	= $date->add( new DateInterval( 'PT1H' ) )->format('Y-m-d H:i:s' );
-		$conditions	= [
-			'date'	=> ['> '.$before, '< '.$date]
-		];
-		$items	= $model->getAll( $conditions, $orders, $limits );
-		$this->out( 'Found '.count( $items ) );
-
+		/** @var Entity_Work_Meeting_Participant[] $participants */
+		$participants	= $model->getAllByIndices( [
+			'meetingId'	=> $meeting->meetingId,
+			'type'		=> '!= '.Model_Work_Meeting_Participant::TYPE_INFORMED,
+		] );
+		foreach( $participants as $participant ){
+			$user	= $logicUser->getUser( $participant->userId );
+			$mail		= new Mail_Work_Meeting_Reminder( $this->env, [
+				'meeting'	=> $meeting,
+				'user'		=> $user,
+			] );
+			$logicMail->handleMail( $mail, $user, $this->env->getLanguage()->getLanguage() );
+		}
+		return count( $participants );
 	}
 }
