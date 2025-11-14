226df9a7628c1d9235b59815409377ce3180b24d
kriss0r <kriss@ceusmedia.de>
2025-11-14 03:57:30 +0100

    Security:CSRF: 0.3.5: Add refresh and hints in view helper.

--- src/Security/CSRF/classes/View/Helper/CSRF.php
+++ src/Security/CSRF/classes/View/Helper/CSRF.php
@@ -7,16 +7,24 @@ class View_Helper_CSRF
 {
 	protected Environment $env;
 	protected ?string $formName		= NULL;
+	protected ?bool $useHint		= NULL;
+	protected ?bool $useRefresh		= NULL;
 
 	/**
 	 *	@param		Environment	$env
 	 *	@param		string		$formName
+	 *	@param		bool|NULL	$useRefresh		Flag: also render JavaScript to reload after token duration
+	 *	@param		bool|NULL	$useHint		Flag: show hint if not refreshing
 	 *	@return		string
 	 */
-	public static function renderStatic( Environment $env, string $formName ): string
+	public static function renderStatic( Environment $env, string $formName, ?bool $useRefresh = NULL, ?bool $useHint = NULL ): string
 	{
 		$helper	= new self( $env );
 		$helper->setFormName( $formName );
+		if( NULL !== $useRefresh )
+			$helper->setUseRefresh( $useRefresh );
+		if( NULL !== $useHint )
+			$helper->setUseHint( $useHint );
 		return $helper->render();
 	}
 
@@ -33,6 +41,8 @@ class View_Helper_CSRF
 	 */
 	public function render( ?string $formName = NULL ): string
 	{
+		if( !( $this->env->getModules()->get( 'Security_CSRF' )?->isActive ?? FALSE ) )
+			return '';
 		$formName	= $formName ?: $this->formName;
 		if( !$formName )
 			throw new RuntimeException( 'No form name set' );
@@ -49,7 +59,34 @@ class View_Helper_CSRF
 			'name'	=> 'csrf_form_name',
 			'value'	=> $formName
 		] );
-		return $input1.$input2;
+		$moduleConfig	= $this->env->getModules()->get( 'Security_CSRF' )->getConfigAsDictionary()->getAll( '', TRUE );
+		$useHint		= TRUE === $this->useHint || ( NULL === $this->useHint && $moduleConfig->get( 'auto.hint' ) );
+		$useRefresh		= TRUE === $this->useRefresh || ( NULL === $this->useRefresh && $moduleConfig->get( 'auto.refresh' ) );
+		$useLifetime	= 0 !== $logic->getTokenDuration();
+		$hasDatetimeHelper	= $this->env->getModules()->has( 'UI_Helper_Datetime' );
+
+		$hint		= '';
+		$refresh	= '';
+		if( $useLifetime && $useRefresh ){
+			$script	= sprintf( 'window.setTimeout(function(){window.location.reload()}, %d)', $logic->getTokenDuration() * 1000 );
+			$this->env->getPage()->js->addScriptOnReady( $script );
+		}
+		else if( $useHint && $useLifetime ){
+			$message	= '';
+			if( $hasDatetimeHelper ){
+				$helperTime		= new View_Helper_Datetime( $this->env );
+				$tokenLifetime	= $helperTime->getDurationPhraseFromSeconds( $logic->getTokenDuration() );
+				$message		= sprintf( 'Dieses gesicherte Formular ist nur %s gültig.', $tokenLifetime );
+			}
+			else{
+				$tokenLifetime	= floor( $logic->getTokenDuration() / 60 );
+				$message	= sprintf( 'Dieses gesicherte Formular ist nur %d Minuten gültig.', $tokenLifetime );
+			}
+			if( '' !== $message )
+				$hint	= HtmlTag::create( 'div', $message, ['class' => 'alert alert-info'] );
+
+		}
+		return $input1.$input2.$hint.$refresh;
 	}
 
 	/**
@@ -59,6 +96,26 @@ class View_Helper_CSRF
 	public function setFormName( string $formName ): self
 	{
 		$this->formName	= $formName;
+		return $this;
+	}
+
+	/**
+	 *	@param		bool		$switch
+	 *	@return		self
+	 */
+	public function setUseHint( bool $switch ): self
+	{
+		$this->useHint	= $switch;
+		return $this;
+	}
+
+	/**
+	 *	@param		bool		$switch
+	 *	@return		self
+	 */
+	public function setUseRefresh( bool $switch ): self
+	{
+		$this->useRefresh	= $switch;
 		return $this;
 	}
 }
