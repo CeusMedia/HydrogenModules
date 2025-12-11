faeb89d89b338e92ad3ed7c0f07e3d29b4c2a542
kriss0r <kriss@ceusmedia.de>
2025-12-11 04:31:35 +0100

    Info:Relocation: Fix bug.

--- src/Info/Relocation/classes/Hook/Info/Relocation.php
+++ src/Info/Relocation/classes/Hook/Info/Relocation.php
@@ -11,21 +11,24 @@ class Hook_Info_Relocation extends Hook
 	 */
 	public function onPageApplyModulesCheckShortcutRoute(): void
 	{
+		if( !$this->env->getModules()->has( 'Server_Router' ) )							//  router module is NOT installed
+			return;
+
 		$config	= $this->env->getConfig()->getAll( 'module.info_relocation.', TRUE );	//  shortcut config
-		if( $this->env->getModules()->has( 'Server_Router' ) ){				//  router module is installed
-			if( $config->get( 'shortcut' ) ){										//  shortcut is enabled
-				$model	= new Model_Route( $this->env );								//  get router module
-				if( !$model->getByIndex( 'target', 'info/relocation/$1' ) ){	//  shortcut route is not set up yet
-					$model->add( [														//  add shortcut route
-						'status'	=> 1,												//  ... as active
-						'regex'		=> 1,												//  ... as regular expression
-						'code'		=> $config->get( 'shortcut.code' ),			//  ... with HTTP code
-						'source'	=> $config->get( 'shortcut.source' ),			//  ... with source pattern
-						'target'	=> $config->get( 'shortcut.target' ),			//  ... with target pattern
-						'createdAt'	=> time(),											//  ... and note creation time
-					] );
-				}
-			}
-		}
+		if( !$config->get( 'shortcut' ) )														//  shortcut is NOT enabled
+			return;
+
+		$model	= new Model_Route( $this->env );													//  get router module
+		if( $model->getByIndex( 'target', 'info/relocation/$1' ) )						//  shortcut route is already set up
+			return;
+
+		$model->add( [																				//  add shortcut route
+			'status'	=> 1,																		//  ... as active
+			'regex'		=> 1,																		//  ... as regular expression
+			'code'		=> $config->get( 'shortcut.code' ),									//  ... with HTTP code
+			'source'	=> $config->get( 'shortcut.source' ),									//  ... with source pattern
+			'target'	=> $config->get( 'shortcut.target' ),									//  ... with target pattern
+			'createdAt'	=> time(),																	//  ... and note creation time
+		] );
 	}
 }
--- src/Info/Relocation/classes/View/Info/Relocation.php
+++ src/Info/Relocation/classes/View/Info/Relocation.php
@@ -4,10 +4,18 @@ use CeusMedia\HydrogenFramework\View;
 
 class View_Info_Relocation extends View
 {
-	public function fail(): void
+	/**
+	 *	@return		string
+	 *	@throws		ReflectionException
+	 */
+	public function fail(): string
 	{
+		return $this->loadContentFile( 'html/info/relocation/fail.html' );
 	}
 
+	/**
+	 *	@return		void
+	 */
 	public function index(): void
 	{
 	}
