bd4868fe78e981818fd64dfacd5ed505908e38cb
kriss0r <kriss@ceusmedia.de>
2026-06-20 04:33:16 +0200

    Manage:Jobs: Improve language support (tabs).

--- src/Manage/Jobs/classes/View/Manage/Job.php
+++ src/Manage/Jobs/classes/View/Manage/Job.php
@@ -18,16 +18,11 @@ class View_Manage_Job extends View
 	{
 		$tabs	= new View_Helper_Navigation_Bootstrap_Tabs( $env );
 		$tabs->setBasePath( './manage/job/' );
-/*		$words	= (object) $env->getLanguage()->getWords( 'manage/job' );							//  load words
-		$tabs->registerTab( '', $words->tabs['index'], 0 );											//  register job dashboard as main tab
-		$tabs->registerTab( 'run', $words->tabs['run'], 1 );										//  register job runs tab
-		$tabs->registerTab( 'schedule', $words->tabs['schedule'], 2 );								//  register job schedule tab
-		$tabs->registerTab( 'definition', $words->tabs['definition'], 3 );							//  register job definitions tab
-*/
-		$tabs->registerTab( '', 'Dashboard', 0 );													//  register job dashboard as main tab
-		$tabs->registerTab( 'run', 'Ausführungen', 1 );												//  register job runs tab
-		$tabs->registerTab( 'schedule', 'Zeitplan', 2 );											//  register job schedule tab
-		$tabs->registerTab( 'definition', 'Jobs', 3 );												//  register job definitions tab
+		$words	= (object) $env->getLanguage()->getWords( 'manage/job' );							//  load words
+//		$tabs->registerTab( '', $words->tabs['index'], 0 );									//  register job dashboard as main tab
+		$tabs->registerTab( 'run', $words->tabs['run'], 1 );									//  register job runs tab
+		$tabs->registerTab( 'schedule', $words->tabs['schedule'], 2 );						//  register job schedule tab
+		$tabs->registerTab( 'definition', $words->tabs['definition'], 3 );					//  register job definitions tab
 
 		return $tabs->renderTabs( $current );
 	}
