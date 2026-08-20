f1654c08750cd52109263ca629fbd6681eed10bc
kriss0r <kriss@ceusmedia.de>
2026-08-20 03:33:32 +0200

    Security:Altcha: Add JS and logic class.

---
+++ src/Security/Altcha/classes/Logic/Altcha.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use CeusMedia\HydrogenFramework\Logic\Shared as SharedLogic;
+
+class Logic_Altcha extends SharedLogic
+{
+	public function doA()
+	{
+	}
+
+	protected function __onInit(): void
+	{
+
+	}
+}
